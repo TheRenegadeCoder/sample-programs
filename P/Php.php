019f91e75f0c3c4fb38bf857ee1b97ae8a452102
Tolga <toturkmen@gmail.com>
2017-10-02 01:14:07 +0300

    Update 'PHP.php'.

--- P/Php.php
+++ P/Php.php
@@ -1,12 +1,9 @@
 <?php
 
- 
-    for($i=0;$i<=10;$i++)
-        echo str_repeat(' ', 10 - $i).str_repeat('*', $i * 2 + 1)."\n";
+    for ($i=0; $i < 10; $i++)
+        echo str_repeat (' ', 10 - $i).str_repeat ('*', $i * 2 + 1) . "\n";
     
-    for($i=10;$i>=0;$i--)
-        echo str_repeat(' ', 10 - $i).str_repeat('*', $i * 2 + 1)."\n";
-
-    echo "\n";
+    for ($i=10; -1 < $i; $i--)
+        echo str_repeat (' ', 10 - $i).str_repeat ('*', $i * 2 + 1) . "\n";
 
 ?>
