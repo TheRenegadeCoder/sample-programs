bd1e2940cf1c9628b515cd9187d0aeec059dcda4
Jeremy Griffith <jrg170@case.edu>
2018-03-25 23:51:39 -0400

    Messing around with string formatters

--- log_updater.py
+++ log_updater.py
@@ -1,5 +1,14 @@
+TEMPLATE = """## Day {0}: March {1}, 2018
+
+**Today's Progress**: Implemented Hello World in {2}
+
+**Link(s) to work**
+1. [Hello World in {2}](https://therenegadecoder.com/code/python/hello-world-in-{2}/)
+"""
+
+
 def main():
-    pass
+    print(TEMPLATE.format(5, 18, 'Python'))
 
 
 if __name__ == '__main__':
