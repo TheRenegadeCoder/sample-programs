51e92758574dfd43cd4d0502ca56e51d0bfd736d
Jeremy Griffith <jrg170@case.edu>
2018-03-25 23:57:18 -0400

    Updated to retrieve user input

--- log_updater.py
+++ log_updater.py
@@ -1,14 +1,19 @@
-TEMPLATE = """## Day {0}: March {1}, 2018
+TEMPLATE = """
+## Day {0}: March {1}, 2018
 
 **Today's Progress**: Implemented Hello World in {2}
 
 **Link(s) to work**
-1. [Hello World in {2}](https://therenegadecoder.com/code/python/hello-world-in-{2}/)
+1. [Hello World in {2}](https://therenegadecoder.com/code/python/hello-world-in-{3}/)
 """
 
 
 def main():
-    print(TEMPLATE.format(5, 18, 'Python'))
+    day = input('Day out of 100: ')
+    date = input('Day of Month: ')
+    language = input('Language: ')
+    log = open('log.md', 'a')
+    log.write(TEMPLATE.format(day, date, language, language.lower()))
 
 
 if __name__ == '__main__':
