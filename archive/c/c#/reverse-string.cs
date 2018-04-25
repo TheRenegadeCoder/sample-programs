62a8f30ac5348871c4e7cf7634ba46ce6f1f91ae
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-04-25 14:58:00 -0400

    Added reverse functionality

--- archive/c/c#/reverse-string.cs
+++ archive/c/c#/reverse-string.cs
@@ -1,6 +1,12 @@
 class ReverseString
 {
-  
+  public static string Reverse(string input)
+  {
+      char[] characters = input.ToCharArray();
+      Array.Reverse(characters);
+      return new string(characters);
+  }
+
   static void Main()
   {
 
