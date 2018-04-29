4de3a8598a7c7770364ef93ab8820553042bffc9
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-04-29 12:56:00 -0400

    Added number of living neighbors calculation

--- archive/p/python/game-of-life.py
+++ archive/p/python/game-of-life.py
@@ -1,6 +1,14 @@
 class Cell:
     def __init__(is_alive: bool):
         self.isAlive = isAlive
+        self.neighbors = list()
+
+    def num_of_living_neighbors():
+        total_living_neighbors = 0
+        for neighbor in self.neighbors:
+            if neighbor.is_alive:
+                total_living_neighbors += 1
+        return total_living_neighbors
 
 class Grid:
     def __init__(width: int, spawn_rate: float):
