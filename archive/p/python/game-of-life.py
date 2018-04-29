f38dbd0e46477d00c6ef768854156080fd671f63
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-04-29 12:51:28 -0400

    Created grid class

--- archive/p/python/game-of-life.py
+++ archive/p/python/game-of-life.py
@@ -1,3 +1,8 @@
 class Cell:
-    def __init__(isAlive: bool):
+    def __init__(is_alive: bool):
         self.isAlive = isAlive
+
+class Grid:
+    def __init__(width: int, spawn_rate: float):
+        self.width = width
+        self.spawn_rate = spawn_rate
