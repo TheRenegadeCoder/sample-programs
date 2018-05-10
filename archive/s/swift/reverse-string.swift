814f45f6bb74721fd8a50ff2bf8b48ecb9f92c4b
Marty A/V <martyavedon@gmail.com>
2018-05-10 10:37:34 -0700

    Create reverse-string.swift
    note that Foundation is imported simply so we can exit...

---
+++ archive/s/swift/reverse-string.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+guard CommandLine.arguments.count > else {
+    exit(0)
+}
+
+let usersString = CommandLine.arguments[0]
+let reversedCollection = string.reversed()
+let reversedString = String(reversedCollection)
+
+print(reversedString)
