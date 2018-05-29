5dd90690fb2040f5346db43378822e20f7ad6983
Devin Leaman <dleaman212@gmail.com>
2018-05-28 19:52:57 -0500

    Added Reverse-String.ps1 sample
    I added the Reverse-String.ps1 script which shows how to use a StringBuilder within PowerShell to reverse a String that was passed to the script.
    
    I also updated the README.md to include links to the script files.

--- archive/p/powershell/README.md
+++ archive/p/powershell/README.md
@@ -4,7 +4,8 @@ Welcome to Sample Programs in PowerShell!
 
 ## Sample Programs
 
-- Hello World in PowerShell
+- [Hello World in PowerShell][1]
+- [Reverse String in PowerShell][2]
 
 ## Fun Facts
 
@@ -15,3 +16,6 @@ Welcome to Sample Programs in PowerShell!
 
 - [PowerShell Documentation](https://docs.microsoft.com/en-us/powershell/)
 - [Try It Online: PowerShell](https://tio.run/#powershell)
+
+[1]: .\hello-world.ps1
+[2]: .\Reverse-String.ps1
---
+++ archive/p/powershell/Reverse-String.ps1
@@ -0,0 +1,39 @@
+﻿<#
+.SYNOPSIS
+  A simple script for reversing a String.
+
+.DESCRIPTION
+  A simple script for reversing a String in PowerShell in order to show some
+features of the language.
+
+.PARAMETER Input
+  The string to reverse.
+
+.EXAMPLE
+  .\Reverse-String.ps1 -Input 'Hello, World'
+
+.EXAMPLE
+  .\Reverse-String.ps1 "Les Misérables"
+
+.EXAMPLE
+  .\Reverse-String.ps1 "字樣樣品"
+
+.NOTES
+  This script does *not* support emoji as PowerShell only has full support for
+emoji within the ISE.
+#>
+param
+(
+  [Parameter(Mandatory = $true,
+             Position = 0)]
+  [ValidateNotNullOrEmpty()]
+  [string]$Input
+)
+
+$StringBuilder = New-Object -TypeName System.Text.StringBuilder($Input.Length)
+
+for ($x = ($Input.Length - 1); $x -ge 0; $x--) {
+  [void]$StringBuilder.Append($Input.Chars($x))
+}
+
+Write-Host $StringBuilder.ToString()
