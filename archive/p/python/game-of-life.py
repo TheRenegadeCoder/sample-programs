a1b4fb1cd65f103ee797f0289042008371a1fd1e
Maximillian Naza <maximillianaza@gmail.com>
2018-06-29 16:24:01 -0400

    Use canvas to define the width and height of our frame

--- archive/p/python/game-of-life.py
+++ archive/p/python/game-of-life.py
@@ -1,1 +1,8 @@
 from Tkinter import *
+
+root = Tk()
+root.title("The Renegade Coder's Game of Life")
+frame = Frame(root, width=720, height=720)
+frame.pack()
+canvas = Canvas(frame, width=720, height=720)
+canvas.pack()
