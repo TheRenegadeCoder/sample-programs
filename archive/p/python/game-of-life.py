f8ef6edd4f0dbb3a8f11cf6451a0fe5ca9238561
Maximillian Naza <maximillianaza@gmail.com>
2018-06-29 16:28:03 -0400

    Implement grid function

--- archive/p/python/game-of-life.py
+++ archive/p/python/game-of-life.py
@@ -6,3 +6,21 @@ frame = Frame(root, width=720, height=720)
 frame.pack()
 canvas = Canvas(frame, width=720, height=720)
 canvas.pack()
+
+def grid():
+    x = 10
+    y = 10
+    global grid # Variable to store the Cell objects
+    global rectangles # Variable to store rectangles
+    rectangles = []
+    grid = []
+    for i in range(70):
+        grid.append([])
+        rectangles.append([])
+        for j in range(70):
+            rect = canvas.create_rectangle(x, y, x+10, y+10, fill="white")
+            rectangles[i].append(rect)
+            grid[i].append(Cell(x, y, i, j))
+            x += 10
+        x = 10
+        y += 10
