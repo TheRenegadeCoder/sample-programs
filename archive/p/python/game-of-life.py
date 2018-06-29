8170b8eef626a950078a456e5668bca816a2cf59
Maximillian Naza <maximillianaza@gmail.com>
2018-06-29 17:05:49 -0400

    Update indentation to improve readability

--- archive/p/python/game-of-life.py
+++ archive/p/python/game-of-life.py
@@ -8,91 +8,117 @@ canvas = Canvas(frame, width=720, height=720)
 canvas.pack()
 
 def create_grid():
-    x = 10
+	x = 10
     y = 10
     global grid # Variable to store the Cell objects
     global rectangles # Variable to store rectangles
     rectangles = []
     grid = []
+    
     for i in range(70):
-        grid.append([])
+    	grid.append([])
         rectangles.append([])
+        
         for j in range(70):
             rect = canvas.create_rectangle(x, y, x+10, y+10, fill="white")
             rectangles[i].append(rect)
             grid[i].append(Cell(x, y, i, j))
             x += 10
+        
         x = 10
         y += 10
 
-  def rectangle_coordinates(x, y):
-  	return (x- x%10, y - y%10)
+def rectangle_coordinates(x, y):
+	return (x- x%10, y - y%10)
 
-  def click_colour_change(event):
-    print event.x, event.y
+def click_colour_change(event):
+	print event.x, event.y
     x, y = rectangle_coordinates(event.x, event.y)
+    
     try:
-        iy = x / 10 - 1
+    	iy = x / 10 - 1
         ix = y / 10 - 1
+
         if ix == -1 or iy == -1:
-            raise IndexError
-        if grid[ix][iy].isAlive:
+        	raise IndexError
+
+        	if grid[ix][iy].isAlive:
             canvas.itemconfig(rectangles[ix][iy], fill="black")
+
         else:
-            canvas.itemconfig(rectangles[ix][iy], fill="white")
-        grid[ix][iy].switchStatus()
-        print grid[ix][iy].pos_matrix, grid[ix][iy].pos_screen
-    except IndexError:
+        	canvas.itemconfig(rectangles[ix][iy], fill="white")
+        	grid[ix][iy].switchStatus()
+        	print grid[ix][iy].pos_matrix, grid[ix][iy].pos_screen
+
+        except IndexError:
         return
 
-  def paint_grid:
-  	for i in grid:
-        for j in i:
-            if j.nextStatus != j.isAlive:
-                x, y = j.pos_matrix
+def paint_grid:
+	for i in grid:
+
+		for j in i:
+
+			if j.nextStatus != j.isAlive:
+				x, y = j.pos_matrix
                 print x, y
+                
                 if j.nextStatus:
-                    canvas.itemconfig(rectangles[x][y], fill="white")
+                	canvas.itemconfig(rectangles[x][y], fill="white")
                     print "changed", j.pos_matrix, "from dead to alive"
+
                 else:
                     canvas.itemconfig(rectangles[x][y], fill="black")
                     print "changed", j.pos_matrix, "from alive to dead"
-                j.switchStatus()
-                print "Current status of", j.pos_matrix, j.isAlive
+                    j.switchStatus()
+                    print "Current status of", j.pos_matrix, j.isAlive
 
-  def status_update(cell):
-    num_alive = 0
+def status_update(cell):
+	num_alive = 0
     x, y = cell.pos_matrix
+
     for i in (x-1, x, x+1):
-        for j in (y-1, y, y+1):
-            if i == x and j == y:
-                continue
-            if i == -1 or j == -1:
-                continue
-            try:
-                if grid[i][j].isAlive:
-                    num_alive += 1
-            except IndexError:
-                pass
-    if cell.isAlive:
-        return not( num_alive == 2 or num_alive == 3 )
-    else:
-        return num_alive == 3
 
-  def start_game():
-    for i in grid:
-        for j in i:
-            if status_update(j):
-                j.nextStatus = not j.isAlive
-                print "change in", j.pos_matrix, "from", j.isAlive, "to", j.nextStatus
-            else:
-                j.nextStatus = j.isAlive
-    paint_grid()
-    global game_id
-    game_id = root.after(200, start_game)
+    	for j in (y-1, y, y+1):
 
-  def end_game():
-    root.after_cancel(begin_id)
+    		if i == x and j == y:
+    			continue
+
+    			if i == -1 or j == -1:
+    				continue
+
+    				try:
+
+    					if grid[i][j].isAlive:
+    						num_alive += 1
+
+    					except IndexError:
+    						pass
+
+    						if cell.isAlive:
+    							return not( num_alive == 2 or num_alive == 3 )
+
+    						else:
+    							return num_alive == 3
+
+def start_game():
+
+	for i in grid:
+
+		for j in i:
+
+			if status_update(j):
+				j.nextStatus = not j.isAlive
+				print "change in", j.pos_matrix, "from", j.isAlive, "to", j.nextStatus 
+
+			else:
+				j.nextStatus = j.isAlive
+				paint_grid()
+				global game_id
+				game_id = root.after(200, start_game)
+
+def end_game():
+	root.after_cancel(begin_id)
+
 
 
 
