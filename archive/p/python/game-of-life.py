28389099f5c3d16009cd3a19278eeeaae130e8a4
Maximillian Naza <maximillianaza@gmail.com>
2018-06-29 17:26:02 -0400

    Update to get the code to run. Code is still not done, but I am done for the day. So I am pushing this for the time being

--- archive/p/python/game-of-life.py
+++ archive/p/python/game-of-life.py
@@ -9,63 +9,51 @@ canvas.pack()
 
 def create_grid():
 	x = 10
-    y = 10
-    global grid # Variable to store the Cell objects
-    global rectangles # Variable to store rectangles
-    rectangles = []
-    grid = []
-    
-    for i in range(70):
-    	grid.append([])
-        rectangles.append([])
-        
-        for j in range(70):
-            rect = canvas.create_rectangle(x, y, x+10, y+10, fill="white")
-            rectangles[i].append(rect)
-            grid[i].append(Cell(x, y, i, j))
-            x += 10
-        
-        x = 10
-        y += 10
+	y = 10
+	global grid # Variable to store the Cell objects
+	global rectangles # Variable to store rectangles
+	rectangles = []
+	grid = []
+	for i in range(70):
+		grid.append([])
+		rectangles.append([])
+		for j in range(70):
+			rect = canvas.create_rectangle(x, y, x+10, y+10, fill="white")
+			rectangles[i].append(rect)
+			grid[i].append(Cell(x, y, i, j))
+			x += 10
+		x = 10
+		y += 10
 
 def rectangle_coordinates(x, y):
 	return (x- x%10, y - y%10)
 
 def click_colour_change(event):
-	print event.x, event.y
-    x, y = rectangle_coordinates(event.x, event.y)
-    
-    try:
-    	iy = x / 10 - 1
-        ix = y / 10 - 1
-
-        if ix == -1 or iy == -1:
-        	raise IndexError
-
-        	if grid[ix][iy].isAlive:
-            canvas.itemconfig(rectangles[ix][iy], fill="black")
-
-        else:
-        	canvas.itemconfig(rectangles[ix][iy], fill="white")
-        	grid[ix][iy].switchStatus()
-        	print grid[ix][iy].pos_matrix, grid[ix][iy].pos_screen
-
-        except IndexError:
-        return
+	print (event.x, event.y)
+	x, y = rectangle_coordinates(event.x, event.y)
+	try:
+		iy = x / 10 - 1
+		ix = y / 10 - 1
+		if ix == -1 or iy == -1:
+			raise IndexError
+			if grid[ix][iy].isAlive:
+				canvas.itemconfig(rectangles[ix][iy], fill="black")
+			else:
+				canvas.itemconfig(rectangles[ix][iy], fill="white")
+				grid[ix][iy].switchStatus()
+				print (grid[ix][iy].pos_matrix, grid[ix][iy].pos_screen)
+			except IndexError:
+				return
 
 def paint_grid:
 	for i in grid:
-
 		for j in i:
-
 			if j.nextStatus != j.isAlive:
 				x, y = j.pos_matrix
                 print x, y
-                
                 if j.nextStatus:
                 	canvas.itemconfig(rectangles[x][y], fill="white")
                     print "changed", j.pos_matrix, "from dead to alive"
-
                 else:
                     canvas.itemconfig(rectangles[x][y], fill="black")
                     print "changed", j.pos_matrix, "from alive to dead"
@@ -75,41 +63,28 @@ def paint_grid:
 def status_update(cell):
 	num_alive = 0
     x, y = cell.pos_matrix
-
     for i in (x-1, x, x+1):
-
     	for j in (y-1, y, y+1):
-
     		if i == x and j == y:
     			continue
-
     			if i == -1 or j == -1:
     				continue
-
     				try:
-
     					if grid[i][j].isAlive:
     						num_alive += 1
-
     					except IndexError:
     						pass
-
     						if cell.isAlive:
     							return not( num_alive == 2 or num_alive == 3 )
-
     						else:
     							return num_alive == 3
 
 def start_game():
-
 	for i in grid:
-
 		for j in i:
-
 			if status_update(j):
 				j.nextStatus = not j.isAlive
 				print "change in", j.pos_matrix, "from", j.isAlive, "to", j.nextStatus 
-
 			else:
 				j.nextStatus = j.isAlive
 				paint_grid()
@@ -126,11 +101,3 @@ stop = Button(root, text="Stop!", command = end_game)
 stop.pack(side = RIGHT)
 canvas.bind("<Button-1>", change_colour_on_click)
 root.mainloop()
-
-
-
-
-
-
-
-
