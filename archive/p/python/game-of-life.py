8a2fef70e6a8f9f6c1cba9d2f79096fd770c3e6c
Maximillian Naza <maximillianaza@gmail.com>
2018-07-02 16:27:25 -0400

    Implement Cell class

--- archive/p/python/game-of-life.py
+++ archive/p/python/game-of-life.py
@@ -94,6 +94,23 @@ def start_game():
 def end_game():
 	root.after_cancel(begin_id)
 
+
+class Cell:
+	def __init__(self, x, y, i, j):
+		self.isAlive = False
+		self.nextStatus = None
+		self.pos_screen = (x, y)
+		self.pos_matrix = (i, j)
+
+	def __str__(self):
+		return str(self.isAlive)
+
+	def __repr__(self):
+		return str(self.isAlive)
+
+	def switchStatus(self):
+		self.isAlive = not self.isAlive
+
 create_grid()
 start = Button(root, text="Start!", command=start_game)
 start.pack(side = LEFT)
