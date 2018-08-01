7d804b298d03c5713ddc05666cbdc3c32b7f9877
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-08-01 16:43:29 -0400

    Added some type hinting

--- tools/generate-wiki.py
+++ tools/generate-wiki.py
@@ -1,18 +1,18 @@
 import os
-
+from typing import List
 
 class Repo:
     def __init__(self):
         self.source_dir = os.path.join("..", "archive")
         self.languages = list()
 
-    def generate_repo(self):
+    def analyze_repo(self):
         for root, directories, files in os.walk(self.source_dir):
             if not directories:
                 language = Language(os.path.basename(root), root, files)
                 language.analyze_language()
-                repo.languages.append(language)
-        for language in repo.languages:
+                self.languages.append(language)
+        for language in self.languages:
             print(language)
 
     def compute_total_snippets(self):
@@ -23,12 +23,12 @@ class Repo:
 
 
 class Language:
-    def __init__(self, name, path, file_list):
-        self.name = name
-        self.path = path
-        self.file_list = file_list
-        self.total_snippets = 0
-        self.total_dir_size = 0
+    def __init__(self, name: str, path: str, file_list: List[str]):
+        self.name: str = name
+        self.path: str = path
+        self.file_list: List[str] = file_list
+        self.total_snippets: int = 0
+        self.total_dir_size: int = 0
 
     def __str__(self):
         return self.name + ";" + str(self.total_snippets) + ";" + str(self.total_dir_size)
@@ -53,5 +53,14 @@ class Language:
         self.total_dir_size = size
 
 
-repo = Repo()
-repo.generate_repo()
+class Wiki:
+    def __init__(self):
+        self.repo = None
+
+    def build_wiki(self):
+        self.repo = Repo()
+        self.repo.analyze_repo()
+
+
+wiki = Wiki()
+wiki.build_wiki()
