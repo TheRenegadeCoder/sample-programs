afef888cb45e88ccd3dc0c0f67a4bb792a434972
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-08-01 16:31:16 -0400

    Added directory size calculation

--- tools/generate-wiki.py
+++ tools/generate-wiki.py
@@ -3,13 +3,13 @@ import os
 
 class Repo:
     def __init__(self):
-        self.source_dir = "../archive"
+        self.source_dir = os.path.join("..", "archive")
         self.languages = list()
 
     def generate_repo(self):
         for root, directories, files in os.walk(self.source_dir):
             if not directories:
-                language = Language(os.path.basename(root), files)
+                language = Language(os.path.basename(root), root, files)
                 language.analyze_language()
                 repo.languages.append(language)
         for language in repo.languages:
@@ -23,16 +23,19 @@ class Repo:
 
 
 class Language:
-    def __init__(self, name, file_list):
+    def __init__(self, name, path, file_list):
         self.name = name
+        self.path = path
         self.file_list = file_list
         self.total_snippets = 0
+        self.total_dir_size = 0
 
     def __str__(self):
-        return self.name + ": " + str(self.total_snippets)
+        return self.name + ";" + str(self.total_snippets) + ";" + str(self.total_dir_size)
 
     def analyze_language(self):
         self.compute_total_snippets()
+        self.computer_total_dir_size()
 
     def compute_total_snippets(self):
         count = 0
@@ -41,6 +44,13 @@ class Language:
             if file_ext not in (".md", ""):
                 count += 1
         self.total_snippets = count
+
+    def computer_total_dir_size(self):
+        size = 0
+        for file in self.file_list:
+            relative_path = os.path.join(self.path, file)
+            size += os.path.getsize(relative_path)
+        self.total_dir_size = size
 
 
 repo = Repo()
