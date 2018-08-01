bca47eab71858c274294749a40a0fa1b8e295bbe
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-08-01 16:15:07 -0400

    Converted everything to classes

--- tools/generate-wiki.py
+++ tools/generate-wiki.py
@@ -1,33 +1,43 @@
 import os
 
+
 class Repo:
     def __init__(self):
+        self.source_dir = "../archive"
         self.languages = list()
+
+    def generate_repo(self):
+        for root, directories, files in os.walk(self.source_dir):
+            if not directories:
+                language = Language()
+                language.name = os.path.basename(root)
+                language.file_list = files
+                language.num_of_snippets = language.compute_total_snippets()
+                repo.languages.append(language)
+        for language in repo.languages:
+            print(language.name + ": " + str(language.num_of_snippets))
+
+    def compute_total_snippets(self):
+        count = 0
+        for language in self.languages:
+            count += language.compute_total_snippets()
+        return count
 
 
 class Language:
     def __init__(self):
         self.name = None
-        self.num_of_snippets = 0
+        self.file_list = list()
+        self.total_snippets = 0
 
-
-def compute_num_of_snippets(file_list):
-    count = 0
-    for file in file_list:
-        file_name, file_ext = os.path.splitext(file)
-        if file_ext not in (".md", ""):
-            count += 1
-    return count
+    def compute_total_snippets(self):
+        count = 0
+        for file in self.file_list:
+            file_name, file_ext = os.path.splitext(file)
+            if file_ext not in (".md", ""):
+                count += 1
+        return count
 
 
 repo = Repo()
-
-for root, directories, files in os.walk("../archive"):
-    if not directories:
-        language = Language()
-        language.name = os.path.basename(root)
-        language.num_of_snippets = compute_num_of_snippets(files)
-        repo.languages.append(language)
-
-for language in repo.languages:
-    print(language.name + ": " + str(language.num_of_snippets))
+repo.generate_repo()
