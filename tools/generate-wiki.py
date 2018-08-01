3f18d50143c87cef9927afda88c5929900cf161f
Jeremy Grifski <jeremy.grifski@therenegadecoder.com>
2018-08-01 16:00:53 -0400

    Implemented script counting

--- tools/generate-wiki.py
+++ tools/generate-wiki.py
@@ -8,6 +8,16 @@ class Repo:
 class Language:
     def __init__(self):
         self.name = None
+        self.num_of_snippets = 0
+
+
+def compute_num_of_snippets(file_list):
+    count = 0
+    for file in file_list:
+        file_name, file_ext = os.path.splitext(file)
+        if file_ext not in (".md", ""):
+            count += 1
+    return count
 
 
 repo = Repo()
@@ -16,7 +26,8 @@ for root, directories, files in os.walk("../archive"):
     if not directories:
         language = Language()
         language.name = os.path.basename(root)
+        language.num_of_snippets = compute_num_of_snippets(files)
         repo.languages.append(language)
 
 for language in repo.languages:
-    print(language.name)
+    print(language.name + ": " + str(language.num_of_snippets))
