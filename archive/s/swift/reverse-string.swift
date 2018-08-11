bbc1527535ebb2c8c54c5c7c01149b09c64a5395
Marty A/V <martyavedon@gmail.com>
2018-08-10 23:16:18 -0700

    corrected call to wrong index
    Commandline.arguments is always initialized with at least one argument inside: the file path. This file path is always located at index 0. To access the user's first input, we must call index 1

--- archive/s/swift/reverse-string.swift
+++ archive/s/swift/reverse-string.swift
@@ -4,7 +4,7 @@ guard CommandLine.arguments.count > 0 else {
     exit(0)
 }
 
-let usersString = CommandLine.arguments[0]
+let usersString = CommandLine.arguments[1]
 let reversedCollection = usersString.reversed()
 let reversedString = String(reversedCollection)
 
