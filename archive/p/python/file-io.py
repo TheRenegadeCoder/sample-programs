dd19de981587f3f5c76064b8c3cf61496ad65352
Noah Nichols <1101qbtext@gmail.com>
2018-09-12 13:00:03 -0400

    Added python example for file IO

---
+++ archive/p/python/file-io.py
@@ -0,0 +1,30 @@
+def write_file():
+
+    try:
+        out = open("file.txt", "w")
+    except OSError as e:
+        print("Cannot open file: {}", e)
+
+
+    out.write("Hi! I'm a line of text in this file!\n")
+    out.write("Me, too!\n")
+
+    out.flush()
+    out.close()
+
+def read_file():
+    
+    try:
+        in_file = open("file.txt", "r")
+    except OSError as e:
+        print("Cannot open file to read: {}", e)
+    
+    line = in_file.readline()
+    while line:
+        print(line.rstrip('\n'))
+        line = in_file.readline()
+    
+    in_file.close()
+
+write_file()
+read_file()
