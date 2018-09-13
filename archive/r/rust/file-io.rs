8ecb2499640fad9db7667b17996ce05a05e8dfb4
Noah Nichols <1101qbtext@gmail.com>
2018-09-13 09:37:22 -0400

    Fixed warnings

--- archive/r/rust/file-io.rs
+++ archive/r/rust/file-io.rs
@@ -5,10 +5,14 @@ use std::io::Result;
 fn write_file() -> Result<()> {
     let mut out = File::create("output.txt")?;
 
-    out.write(b"A line of text written to this file in Rust!\n");
-    out.write(b"Another line also written by a Rust program!\n");
+    let bytes_written = out.write(b"A line of text written to this file in Rust!\n").unwrap_or(0);
+    let bytes_written2 = out.write(b"Another line also written by a Rust program!\n").unwrap_or(0);
 
-    out.flush();
+    if bytes_written == 0 || bytes_written2 == 0 {
+        ()
+    }
+
+    out.flush()?;
 
     Ok(())
 }
@@ -17,14 +21,15 @@ fn read_file() -> Result<()> {
     let mut in_file = File::open("output.txt")?;
 
     let mut line = String::new();
-    in_file.read_to_string(&mut line);
-
-    println!("{}", line);
+    match in_file.read_to_string(&mut line) {
+        Ok(_)   =>  println!("{}", line),
+        Err(_)  =>  panic!("Could not read contents into string!")
+    }
 
     Ok(())
 }
 
-fn main() {
-    write_file();
-    read_file();
+fn main() -> Result<()> {
+    write_file()?;
+    Ok(read_file()?)
 }
