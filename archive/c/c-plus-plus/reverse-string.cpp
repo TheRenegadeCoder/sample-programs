4c1b045989bc3f15440b5589d73860afcf7fd081
Noah Nichols <1101qbtext@gmail.com>
2018-09-14 10:04:43 -0400

    Get string from the command line

--- archive/c/c-plus-plus/reverse-string.cpp
+++ archive/c/c-plus-plus/reverse-string.cpp
@@ -2,9 +2,12 @@
 #include <string>
 #include <algorithm>
 
-int main()
+int main(int argc, char *argv[])
 {
-    std::string s = "Some arbitrary text";
+    if(argc < 2)
+        return 0;
+    
+    std::string s = argv[1];
     std::reverse(s.begin(), s.end());
 
     std::cout << s << "\n";
