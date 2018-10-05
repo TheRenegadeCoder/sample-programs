384d1b9d1c84877a70d2072bed85bda8de524191
Noah Nichols <1101qbtext@gmail.com>
2018-10-05 10:31:35 -0400

    Change i64 variables to u64

--- archive/r/rust/fibonacci.rs
+++ archive/r/rust/fibonacci.rs
@@ -1,15 +1,15 @@
-const LIMIT: i64 = 92;
+const LIMIT: u64 = 93;
 
-fn fibonacci(terms: i64) {
+fn fibonacci(terms: u64) {
     if terms > LIMIT {
         println!("The number of terms you want to calculate is too big!");
-        println!("The limit is 92.");
+        println!("The limit is {}.", LIMIT);
     } else {
         println!("\n");
-        let mut i = 0i64;
-        let mut a = 0i64;
-        let mut b = 1i64;
-        let mut c = 0i64;
+        let mut i = 0u64;
+        let mut a = 0u64;
+        let mut b = 1u64;
+        let mut c = 0u64;
         while i < terms {
             println!("{}", c);
 
@@ -24,7 +24,7 @@ fn fibonacci(terms: i64) {
 
 fn main() {
     let mut terms = String::new();
-    let mut terms_n: i64 = 0;
+    let mut terms_n: u64 = 0;
 
     if std::io::stdin().read_line(&mut terms).is_err() {
         println!("Could not read from keyboard!");
