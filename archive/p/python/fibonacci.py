e96e60a9c38af9ec65cf5dcb3b8b6476e1327aa6
Marius <mariusgarbea@ymail.com>
2018-10-06 15:34:45 -0400

    wrapped fibonacci in python in main

--- archive/p/python/fibonacci.py
+++ archive/p/python/fibonacci.py
@@ -1,13 +1,14 @@
 import sys
 
-def fibonacci(n):
-  first = 0
-  second = 1
-  result = 0
-  for i in range(1, int(n)+1):
-    result = first + second
-    first = second
-    second = result
-    print(i, ": ", first)
+if __name__ == "__main__":
+    def fibonacci(n):
+      first = 0
+      second = 1
+      result = 0
+      for i in range(1, int(n)+1):
+        result = first + second
+        first = second
+        second = result
+        print(i, ": ", first)
 
-fibonacci(sys.argv[1])
+    fibonacci(sys.argv[1])
