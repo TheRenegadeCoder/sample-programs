2046780aee93659a18567fbe260fe15f7da78200
Panagiotis Georgiadis <panagiwtis.geor@gmail.com>
2018-10-15 21:32:36 +0100

    Changed the implementation in order to accept the number of fibonacci elements required as a command line argument.

--- archive/h/haskell/fibonacci.hs
+++ archive/h/haskell/fibonacci.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import System.Environment
+
 fibonacciSequence :: Int -> [Int]
 fibonacciSequence 0            = []
 fibonacciSequence elementCount = go 0 [0,1]
@@ -8,8 +10,15 @@ fibonacciSequence elementCount = go 0 [0,1]
           | otherwise                = sequence
 
 
--- Prints out the first 10 numbers from the fibonacci sequence.
+-- Prints out the first N numbers from the fibonacci sequence
+-- where N equals to the first command line argument.
 main :: IO ()
 main = do
-  putStrLn "The first 10 fibonacci numbers are:"
-  putStrLn $ show $ fibonacciSequence 10
+  args <- getArgs
+  let n = head args
+  if null args then
+    error "You need to pass the number of the fibonacci elements to calculate through the command line\n"
+  else
+    let message  = "\nThe first " ++ n ++ " fibonacci numbers are: \n"
+        sequence = fibonacciSequence $ read n
+    in putStrLn $ message ++ show sequence
