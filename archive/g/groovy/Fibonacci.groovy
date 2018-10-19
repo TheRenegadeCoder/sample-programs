abd90d417de9ce458a1ffb38e91ab803574359a0
Loic Beylot <loic.beylot@gmail.com>
2018-10-19 18:47:49 +0100

    Add support for up to 100
    Now uses BigInteger in order to be able to calculate higher values.

--- archive/g/groovy/Fibonacci.groovy
+++ archive/g/groovy/Fibonacci.groovy
@@ -1,11 +1,12 @@
 class Fibonacci {
     static void main(String[] args) {
         if (args.length < 1 || !args[0].isInteger()) {
-            println 'please provide an integer between 1 and 10'
+            println 'please provide an integer between 1 and 100'
         } else {
             def n = args[0] as Integer
-            def first = 0
-            def second = 1
+            def first = 0G
+            def second = 1G
+            println first.class
             (1..n).each {
                 second += first
                 first = second - first
