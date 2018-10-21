06a9406f941e6a4664464e15485f0844527e464f
Mark Magahis <mark.magahis@gmail.com>
2018-10-20 22:55:09 -0500

    first attempt, needs optimization and trimming

--- archive/s/swift/baklava.swift
+++ archive/s/swift/baklava.swift
@@ -1,3 +1,27 @@
-func baklava(<#parameters#>) -> Void {
-    
+func baklava(maxWidth: Int = 10) -> Void {
+    let width = 0...maxWidth
+    for number in width {
+        let repeatedSpaces = repeatElement(" ", count:maxWidth-number)
+        for space in repeatedSpaces {
+            print(space,terminator:"")
+        }
+        
+        let repeatedAsterisks = repeatElement("*", count:number*2+1)
+        for asterisk in repeatedAsterisks {
+            print(asterisk,terminator:"")
+        }
+        print("")
+    }
+    for number in width.reversed() {
+        let repeatedSpaces = repeatElement(" ", count:maxWidth-number)
+        for space in repeatedSpaces {
+            print(space,terminator:"")
+        }
+        let repeatedAsterisks = repeatElement("*", count:number*2+1)
+        for asterisk in repeatedAsterisks {
+            print(asterisk,terminator:"")
+        }
+        print("")
+    }
 }
+baklava()
