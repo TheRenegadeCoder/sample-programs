f80c23af1224666cb327f1a7bf4cc634d64faeeb
Parker Johansen <johansen.parker@gmail.com>
2018-10-24 10:42:45 -0600

    Changed File IO in Haskell to use filename output.txt

--- archive/h/haskell/file-io.hs
+++ archive/h/haskell/file-io.hs
@@ -5,11 +5,11 @@ import System.IO
 main :: IO ()
 main = do
   -- write
-  handle <- openFile "file.txt" ReadWriteMode
+  handle <- openFile "output.txt" ReadWriteMode
   hPutStr handle "file contents"
   hClose handle
   -- read
-  handle <- openFile "file.txt" ReadWriteMode
+  handle <- openFile "output.txt" ReadWriteMode
   value <- hGetContents handle
   -- Print the contents of the file that was read.
   -- Haskell reads the file lazily, so the value must be printed before the handle is closed
