a8f83cef1c3ba2d74613688d2accbd64359d48fa
Sagar Nikam <sagarnikam123@gmail.com>
2018-10-26 12:52:27 +0530

    prints 2 triangles separated by line using stars

---
+++ archive/c/c/damru.c
@@ -0,0 +1,25 @@
+/*
+
+	DAMRU
+
+*/
+
+#include<stdio.h>
+
+int main(){
+	
+	int i,j;
+    for(i=1;i<=11;i++){
+        
+		for(j=1;j<=11;j++){
+            printf(" ");
+            if(j==1 || i==j ||  j==11 || (j==6 && i>5 )|| (i+j)==12){
+            printf("*");
+            }else{ printf(" ");}
+        } //inner for
+    printf("\n");
+    } //outer for
+    
+return 0;
+
+} //main
