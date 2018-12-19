6bb21989efee50cf79b7a7c1180801ac4eb13356
Parker Johansen <johansen.parker@gmail.com>
2018-12-19 15:34:50 -0700

    Haskell- game of life: cleaned up unused functions and added documentation

--- archive/h/haskell/game-of-life.hs
+++ archive/h/haskell/game-of-life.hs
@@ -1,14 +1,16 @@
+-- This implementation requires ArgParser. It can be installed using cabal install argparser
+
+-- Any live cell with fewer than two live neighbors dies, as if caused by underpopulation.
+-- Any live cell with two or three live neighbors lives on to the next generation.
+-- Any live cell with more than three live neighbors dies, as if by overpopulation.
+-- Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
 module Main where
+
 import System.Console.ArgParser
 import System.Environment
---import Text.Read
 import Control.Concurrent
 import Control.Applicative
-
---Any live cell with fewer than two live neighbors dies, as if caused by underpopulation.
---Any live cell with two or three live neighbors lives on to the next generation.
---Any live cell with more than three live neighbors dies, as if by overpopulation.
---Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
 
 
 type Position = (Int, Int)
@@ -20,8 +22,11 @@ type Grid = [Node]
 type Game = [Grid]
 
 class GridMember a where
+  -- | 'circle' provides the eight positions that surround a GridMember
   circle :: a -> [Position]
+  -- | 'getX' returns the x coordinate of a GridMember
   getX :: a -> Int
+  -- | 'getY' returns the y coordinate of a GridMember
   getY :: a -> Int
 
 instance GridMember Node where
@@ -33,12 +38,19 @@ instance GridMember Node where
   getY (Dead (_, y)) = y
 
 class Cell a where
+  -- | 'isAlive' determines whether a cell is alive
   isAlive :: a -> Bool
+  -- | 'kill' kills a cell
   kill :: a -> a
+  -- | 'birth' brings a cell to life
   birth :: a -> a
+  -- | 'atPosition' determines whether a cell is alive and at a given position
   atPosition :: a -> Position ->  Bool
+  -- | 'inGrid' determines if a cell is alive and located within a given list of positions
   inGrid :: a -> [Position] -> Bool
+  -- | 'neighbors' determines all living neighbors of a cell
   neighbors :: a -> [a] ->  [a]
+  -- | 'cellStep' determines whether a cell will be alive or dead in the next iteration
   cellStep :: a -> [a] -> a
 
 instance Cell Node where
@@ -69,14 +81,15 @@ instance Cell Node where
               | otherwise                            = cell
 
 
+-- | 'makeGrid' takes a width and a spawn rate and greats a grid with nodes spawned
 makeGrid :: Int -> Float -> Grid
 makeGrid width spawnRate = spawn [Dead (x, y) | x <- [0..(width-1)], y <- [0..(width-1)]] $ getSpawnIndexes width spawnRate
+  where getSpawnIndexes :: Int -> Float -> [Int]
+        getSpawnIndexes width spawnRate = let max = width ^ 2
+                                          in map floor $ tail [0,(100 / (100 * spawnRate))..(fromIntegral max)]
 
-getSpawnIndexes :: Int -> Float -> [Int]
-getSpawnIndexes width spawnRate =
-  let max = width ^ 2
-  in map floor $ tail [0,(100 / (100 * spawnRate))..(fromIntegral max)]
-
+-- | 'spawn' takes a Grid and a list of indexes in the grid at which Nodes should spawn.
+-- It returns a Grid with Nodes spawned at those indexes.
 spawn :: Grid -> [Int] -> Grid
 spawn grid indexes = spawn' grid indexes 0
   where spawn' [] _ _ = []
@@ -85,42 +98,45 @@ spawn grid indexes = spawn' grid indexes 0
           | i == count = (birth c):spawn' cs indexes     (count + 1)
           | otherwise  =         c:spawn' cs (i:indexes) (count + 1)
 
-step :: Grid -> Grid
-step grid = map (flip cellStep grid) grid
-
-
+-- | 'simulate' takes a number of frames and a grid and iterates that grid frame times
 simulate :: Int -> Grid -> Game
 simulate 0 grid        = []
 simulate frameNum grid = grid:simulate (frameNum - 1) (step grid)
+  where step :: Grid -> Grid
+        step grid = map (flip cellStep grid) grid
 
-indexMaybe xs index
-  | length xs < index = Just (xs !! index)
-  | otherwise         = Nothing
-
+-- | 'GoL' is a record for parsing the command line arguments.
+-- It takes 3 Int: width, framerate, and frames
+-- and one float: spawn percentage
 data GoL = GoL Int Int Int Float deriving (Show)
 
+-- | 'golParser' is the ArgParser parser spec for the GoL record
 golParser :: ParserSpec GoL
 golParser = GoL
-  `parsedBy` reqPos "width"  `Descr` "Grid width (assume square grid)"
-  `andBy`    reqPos "framerate"   `Descr` "Frame rate (frames/second)"
-  `andBy`    reqPos "frames" `Descr` "Total number of frames"
-  `andBy`    reqPos "spawn"  `Descr` "Spawn rate (% of living vs. dead as decimal between 0 and 1)"
+  `parsedBy` reqPos "width"     `Descr` "Grid width (assume square grid)"
+  `andBy`    reqPos "framerate" `Descr` "Frame rate (frames/second)"
+  `andBy`    reqPos "frames"    `Descr` "Total number of frames"
+  `andBy`    reqPos "spawn"     `Descr` "Spawn rate (% of living vs. dead as decimal between 0 and 1)"
 
+-- | 'golInterface' is the entrypoint for ArpParse's CLI parsing
 golInterface :: IO (CmdLnInterface GoL)
 golInterface =
   (`setAppDescr` "Game of Life in Haskell")
   <$> mkApp golParser
 
-runGame :: GoL -> Game
-runGame (GoL width framerate frames spawn) =
-  let grid = makeGrid width spawn
-  in simulate frames grid
-
+-- | 'runGameIO' is the entrypoint for running the game itself
+-- It takes all the CLI parameters from 'GoL' and uses them to
+-- instantiate a grid, simulate a game, and print the simulation
+-- out to the console
 runGameIO :: GoL -> IO ()
-runGameIO gol@(GoL width framerate frames spawn) = do
-  let game = runGame gol
+runGameIO (GoL width framerate frames spawn) = do
+  let grid = makeGrid width spawn
+  let game = simulate frames grid
   printGame framerate game
 
+-- | 'printGame' takes a delay (in seconds) and a game and prints 
+-- out each iteration of the game with the specified delay
+-- in between each print
 printGame :: Int -> Game -> IO ()
 printGame _ [] = putStrLn ""
 printGame delaySeconds (g:gs) = do
@@ -129,55 +145,37 @@ printGame delaySeconds (g:gs) = do
   threadDelay (delaySeconds * 1000000)
   printGame delaySeconds gs
 
-gridString :: Grid -> Int -> String
-gridString grid width = gridString' grid width 0
-  where gridString' [] _ _ = ""
-        gridString' (g:gs) width column
-          | column + 1 >= width =         show g ++ gridString' gs width 1
-          | otherwise           = "\n" ++ show g ++ gridString' gs width 0
-
-
+-- | 'printGrid' prints a single grid to the console
 printGrid :: Grid -> IO ()
-printGrid grd = mapM_ putStrLn $ toMultidimensional grd
+printGrid grd = mapM_ putStrLn $ rowStrings grd
 
-
-toMultidimensional :: Grid -> [String]
-toMultidimensional grd = reverse $ snd $ foldl go (0,[]) grd
+-- | 'rowStrings' converts a 'Grid' to a list of strings
+-- where each element in the list is a single row
+-- of the grid
+rowStrings :: Grid -> [String]
+rowStrings grd = reverse $ snd $ foldl go (0,[]) grd
   where go (row, []) n = (row, [strNode n])
         go (row, acc@(a:as)) n
           | getX n == row = (row, (a ++ "." ++ strNode n):as)
           | otherwise     = (row+1, (strNode n):acc)
 
-
+-- | 'strNode' converts a single node to a single character string
+-- "0" means dead "x" means alive
 strNode :: Node -> String
 strNode (Dead _)  = "0"
 strNode (Alive _) = "x"
 
 
+-- | 'clearScreen' prints 1000 empty lines to the console, effictively
+-- clearing the console window. There are good alternatives to this method
+-- but they are either platform specific or require a library
 clearScreen :: IO ()
 clearScreen = mapM_ putStrLn $ replicate 1000 " "
 
 
+-- | 'main' is the entrypoint of the program
 main :: IO ()
 main = do
   interface <- golInterface
   runApp interface runGameIO
 
---main :: IO ()
---main = do
---  args <- getArgs
---  let width = indexMaybe 0 args
---  let frameRate = indexMaybe 1 args
---  let frameNum = indexMaybe 2 args
---  let spawnRate = indexMaybe 3 args
---  case n >>= readMaybe of
---    Nothing -> putStrLn "Usage: please input a non-negative integer"
---    Just n -> putStrLn n
-
---threadDelay 1000000
-
-
---Grid width (assume square grid)
---Frame rate (frames/second)
---Total number of frames
---Spawn rate (% of living vs. dead as decimal between 0 and 1)
