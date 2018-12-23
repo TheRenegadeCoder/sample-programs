4eeafc36ea226d50b731ac6f63114137a6eff474
Parker Johansen <johansen.parker@gmail.com>
2018-12-23 01:26:11 -0700

    Added ROT-13 in Python

---
+++ archive/p/python/rot-13.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+import sys
+from string import ascii_uppercase, ascii_lowercase
+
+
+def rot_13(string):
+    return ''.join([encrypt_char(c) for c in string])
+
+
+def encrypt_char(c):
+    ltrs = ascii_uppercase if c in ascii_uppercase else ascii_lowercase
+    new_index = (ltrs.index(c) + 13) % 26
+    return ltrs[new_index]
+
+
+def exit_with_error():
+    print('Usage: please provide a string to encrypt')
+    sys.exit(1)
+
+
+def main(args):
+    try:
+        string = args[0]
+        if len(string) <= 0:
+            exit_with_error()
+        print(rot_13(string))
+    except (IndexError,ValueError):
+        exit_with_error()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
+
