9e33f0753071db8fc5a1962144e3737660bd2788
Parker Johansen <johansen.parker@gmail.com>
2018-12-31 03:37:54 -0700

    Java: Even Odd: Added more specific exception handling in main

--- archive/j/java/EvenOdd.java
+++ archive/j/java/EvenOdd.java
@@ -1,13 +1,19 @@
 public class EvenOdd
 {
-    public static void verifyNumber(String n) throws Exception
+    public static void verifyNumber(String n) throws NumberFormatException
     {
         char[] nArray = n.toCharArray();
         for (char c : nArray)
         {
             if (!Character.isDigit(c))
-                throw new Exception();
+                throw new NumberFormatException();
         }
+    }
+
+    public static void ErrorAndExit()
+    {
+        System.out.println("Usage: please input a number");
+        System.exit(1);
     }
 
     public static void main(String[] args)
@@ -21,10 +27,13 @@ public class EvenOdd
             String result = n % 2 == 0 ? "Even" : "Odd";
             System.out.println(result);
         }
-        catch (Exception e)
+        catch (NumberFormatException e)
         {
-            System.out.println("Usage: please input a number");
-            System.exit(1);
+            ErrorAndExit();
+        }
+        catch (ArrayIndexOutOfBoundsException e)
+        {
+            ErrorAndExit();
         }
     }
 }
