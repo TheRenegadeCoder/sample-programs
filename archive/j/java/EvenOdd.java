514580ed720b8ff663560e7f72755d5c64a39da2
Parker Johansen <johansen.parker@gmail.com>
2018-12-30 19:06:58 -0700

    Java: Even Odd: Added handling for invalid numbers

--- archive/j/java/EvenOdd.java
+++ archive/j/java/EvenOdd.java
@@ -1,10 +1,21 @@
 public class EvenOdd
 {
+    public static void verifyNumber(String n) throws Exception
+    {
+        char[] nArray = n.toCharArray();
+        for (char c : nArray)
+        {
+            if (!Character.isDigit(c))
+                throw new Exception();
+        }
+    }
+
     public static void main(String[] args)
     {
         try
         {
             String nstr = args[0].trim();
+            verifyNumber(nstr);
             String lastDigit = nstr.substring(nstr.length() - 1);
             int n = Integer.parseInt(lastDigit);
             String result = n % 2 == 0 ? "Even" : "Odd";
