a852d580ed68531e83960a7f851db91ec359bebe
Haseeb Majid <hmajid2301@gmail.com>
2019-01-16 21:59:56 +0000

    :sparkles: Prime Numbers in Python
    Added prime number article in Python, also updated
    the `prime-number.py` it wouldn't detect 2 as prime
     but would detect 1 as prime.
    
    Fixes #861

--- archive/p/python/prime-number.py
+++ archive/p/python/prime-number.py
@@ -4,9 +4,7 @@ from math import sqrt, ceil
 
 
 def is_prime(x):
-    if x <= 1:
-        return True
-    if x % 2 == 0:
+    if (x % 2 == 0 and x is not 2) or (x == 1):
         return False
     return not bool([n for n in range(3, int(ceil(sqrt(x))+1)) if x % n == 0])
 
@@ -19,7 +17,7 @@ def exit_with_error():
 def main(args):
     try:
         x = int(args[0])
-        if x < 0:
+        if x <= 0:
             exit_with_error()
         print(is_prime(x))
     except (IndexError,ValueError):
