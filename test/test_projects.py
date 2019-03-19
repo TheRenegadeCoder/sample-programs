5f973933f0d5ba9a3cd36ef27a391ae03f008da2
Parker Johansen <johansen.parker@gmail.com>
2019-03-19 01:49:20 -0600

    Add sample input tests for bubble-sort

--- test/test_projects.py
+++ test/test_projects.py
@@ -14,9 +14,15 @@ def docker_client():
     return docker.from_env()
 
 
-@pytest.fixture(params=source.get_sources('../archive')['baklava'],
+@pytest.fixture(params=sources['baklava'],
                 ids=['baklava' + source.extension for source in sources['baklava']])
 def baklava(request):
+    return request.param
+
+
+@pytest.fixture(params=sources['bubble-sort'],
+                ids=['bubble-sort' + source.extension for source in sources['bubble-sort']])
+def bubble(request):
     return request.param
 
 
@@ -47,3 +53,47 @@ def test_baklava(docker_client, baklava):
     actual = baklava.run(docker_client)
     actual_lines = actual.split(os.linesep)
     assert actual_lines == expected_lines
+
+
+sorting_permutations = (
+    'description,in_params,expected', [
+        (
+            'no input',
+            None,
+            'Usage: please provide a list of at least two integers to sort in the format "1, 2, 3, 4, 5"'
+        ), (
+            'empty input',
+            '""',
+            'Usage: please provide a list of at least two integers to sort in the format "1, 2, 3, 4, 5"'
+        ), (
+            'invalid input: not a list',
+            '"1"',
+            'Usage: please provide a list of at least two integers to sort in the format "1, 2, 3, 4, 5"'
+        ), (
+            'invalid input: wrong format',
+            '"4 5 3"',
+            'Usage: please provide a list of at least two integers to sort in the format "1, 2, 3, 4, 5"'
+        ), (
+            'sample input',
+            '"4, 5, 3, 1, 2"',
+            '1, 2, 3, 4, 5'
+        ), (
+            'sample input: with duplicate',
+            '"4, 5, 3, 1, 4, 2"',
+            '1, 2, 3, 4, 4, 5'
+        ), (
+            'sample input: already sorted',
+            '"1, 2, 3, 4, 5"',
+            '1, 2, 3, 4, 5'
+        ), (
+            'sample input: reverse sorted',
+            '"9, 8, 7, 6, 5, 4, 3, 2, 1"',
+            '1, 2, 3, 4, 5, 6, 7, 8, 9'
+        ),
+    ])
+
+
+@pytest.mark.parametrize(sorting_permutations[0], sorting_permutations[1], ids=[p[0] for p in sorting_permutations[1]])
+def test_bubble_sort(description, in_params, expected, docker_client, bubble):
+    actual = bubble.run(docker_client, params=in_params)
+    assert actual.replace('[', '').replace(']', '').strip() == expected
