8e71c2fb83f415cd25ef9d0ac2f9bc37edf27790
Vee Ng <vee.vidify@gmail.com>
2019-03-25 20:04:52 +1100

    Scala: fix: change bubble sort I/O according to spec

--- archive/s/scala/bubble-sort.scala
+++ archive/s/scala/bubble-sort.scala
@@ -1,25 +1,25 @@
 import scala.io.StdIn.readLine
 import scala.reflect.ClassTag
 
-object BubbleSortSamples {
+object BubbleSortSample {
   def main(args: Array[String]) {
-    // example of sorting integers
-    val inputInts = readLine().split(" ").map(_.toInt)
-    val sortedInts = bubblesort(inputInts)
-    sortedInts.map(println)
-
-    // example of sorting words (Strings)
-    val inputWords = readLine().split(" ")
-    val sortedWords = bubblesort(inputWords)
-    sortedWords.map(println)
-
-    // example of sorting using custom Ordering
-    object longerString extends Ordering[String] {
-      def compare(lhs: String, rhs: String): Int = lhs.length - rhs.length
+    // verify inputs are being provided
+    parseInput(args) match {
+      case None => println("Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\"")
+      case Some(inputArr) => {
+        val output = bubbleSort(inputArr).mkString(", ")
+        println(output)
+      }
     }
+  }
 
-    val sortedWordsByLength = bsCustom(inputWords)(longerString)
-    sortedWordsByLength.map(println)
+  def parseInput(args: Array[String]): Option[Array[Int]] = args.length match {
+    case 0 => None
+    case _ => try {
+      Some(args(0).split(",").map(_.trim).map(_.toInt))
+    } catch {
+      case e: Throwable => None
+    }
   }
 
   // bubble the current element
@@ -28,32 +28,12 @@ object BubbleSortSamples {
   // note on signature:
   // ClassTag elements help construct Array quen using ++ (instead of falling back to ArraySeq)
   // Elements of array implement Ordered, so we can compare 2 instances of T using ==, <, >, etc.
-  def bubblesort[T <% Ordered[T]: ClassTag](arr: Array[T]): Array[T] = {
+  def bubbleSort[T <% Ordered[T]: ClassTag](arr: Array[T]): Array[T] = {
     def bubble(a: Array[T]): Array[T] = a.length match {
       case 0 => a
       case 1 => a
       case _ => {
         if (a(0) > a(1)) {
-          a.slice(1, 2) ++ bubble(a.slice(0, 1) ++ a.slice(2, a.length))
-        }
-        else {
-          a.slice(0, 1) ++ bubble(a.slice(1, a.length))
-        }
-      }
-    }
-    arr.foldLeft(arr)((xs: Array[T], cur: T) => bubble(xs))
-  }
-
-  // in case we want to sort using custom comparator,
-  // we implement sort that accepts an Ordering object to match scala.util.Sorting.bubblesort signature
-  // Ordering[T] object implement compare(a: T, b: T), returning integers
-  // this helps comparing more complicated structures, with a custom-defined Ordering
-  def bsCustom[T: ClassTag](arr: Array[T])(order: Ordering[T]): Array[T] = {
-    def bubble(a: Array[T]): Array[T] = a.length match {
-      case 0 => a
-      case 1 => a
-      case _ => {
-        if (order.compare(a(0), a(1)) > 0) {
           a.slice(1, 2) ++ bubble(a.slice(0, 1) ++ a.slice(2, a.length))
         }
         else {
