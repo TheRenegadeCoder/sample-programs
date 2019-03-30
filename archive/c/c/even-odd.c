849838f955fe62464c06b6a0141de4c810b2c9ac
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-03-30 16:03:42 -0400

    Added error case

--- archive/c/c/even-odd.c
+++ archive/c/c/even-odd.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 
 int main(int argc, char **argv)
-{
-    int input = atoi(argv[1]);
-
-    if (input % 2 == 0) {
-        printf("Even\n");
-    }
-    else {
-        printf("Odd\n");
+{    
+    if (argc == 1 || sizeof(argv[1]) == 0 || (atoi(argv[1]) == 0 && !strcomp(argv[1], "0")) {
+        printf("Usage: please input a number\n");
+    } else {
+        int input = atoi(argv[1]);
+        if (input % 2 == 0) {
+            printf("Even\n");
+        }
+        else {
+            printf("Odd\n");
+        }
     }
 
     return 0;
