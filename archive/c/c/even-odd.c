f152e2593448a014c996895a52c68805fd155088
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-03-30 16:30:59 -0400

    Changed bad input conditions

--- archive/c/c/even-odd.c
+++ archive/c/c/even-odd.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char **argv)
 {    
-    if (argc == 1 || sizeof(argv[1]) == 0 || (atoi(argv[1]) == 0 && !strcmp(argv[1], "0"))) {
+    if (argc == 1 || argv[1][0] == '\0' || (atoi(argv[1]) == 0 && strcmp(argv[1], "0") != 0)) {
         printf("Usage: please input a number\n");
     } else {
         int input = atoi(argv[1]);
