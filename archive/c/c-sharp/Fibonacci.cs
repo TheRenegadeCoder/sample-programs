3445c36d8460cd55a4504689428f04114f4d0311
Parker Johansen <johansen.parker@gmail.com>
2019-04-07 00:47:29 -0600

    Fix fibonacci tests in c-sharp

--- archive/c/c-sharp/Fibonacci.cs
+++ archive/c/c-sharp/Fibonacci.cs
@@ -6,22 +6,24 @@ namespace SamplePrograms
     {
         public static void Main(string[] args)
         {
-            if(args.Length < 1)
+            try
             {
-                Console.WriteLine("Usage: please input the count of fibonacci");
-                Environment.Exit(0);
+                int n = int.Parse(args[0]);
+                int first = 0;
+                int second = 1;
+                int result = 0;
+                for(int i = 1; i <= n; i++)
+                {
+                    result = first + second;
+                    first = second;
+                    second = result;
+                    Console.WriteLine(i + ": " + first);
+                }
             }
-
-            int n = int.Parse(args[0]);
-            int first = 0;
-            int second = 1;
-            int result = 0;
-            for(int i = 1; i <= n; i++)
+            catch(Exception)
             {
-                result = first + second;
-                first = second;
-                second = result;
-                Console.WriteLine(i + ": " + first);
+                Console.WriteLine("Usage: please input the count of fibonacci numbers to output");
+                Environment.Exit(0);
             }
         }
     }
