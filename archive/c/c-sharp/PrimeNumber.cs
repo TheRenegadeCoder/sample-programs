c2becc6e96daa283919bb0e7fc7d74e56f9975d7
Parker Johansen <johansen.parker@gmail.com>
2019-05-02 11:30:06 -0600

    Fix prine number in c-sharp

--- archive/c/c-sharp/PrimeNumber.cs
+++ archive/c/c-sharp/PrimeNumber.cs
@@ -9,7 +9,7 @@ namespace SamplePrograms
         {
             if (x <= 1)
                 return false;
-            if (x % 2 == 0)
+            if (x != 2 && x % 2 == 0)
                 return false;
 
             for (ulong i = 3; i <= Convert.ToUInt64(Math.Sqrt(x)); i += 2)
@@ -36,7 +36,7 @@ namespace SamplePrograms
             }
             catch
             {
-                Console.WriteLine("Usage: please input a non-negative number");
+                Console.WriteLine("Usage: please input a non-negative integer");
                 Environment.Exit(1);
             }
         }
