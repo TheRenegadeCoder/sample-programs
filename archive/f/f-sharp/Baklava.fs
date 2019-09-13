f478415e178bf582d44acbfbdc02ff92d8dc5e94
Parker Johansen <johansen.parker@gmail.com>
2019-09-12 22:00:23 -0600

    Simplified solution for baklava in F#

--- archive/f/f-sharp/Baklava.fs
+++ archive/f/f-sharp/Baklava.fs
@@ -1,23 +1,17 @@
 let line space asterisk =
-    String.replicate space " "  + String.replicate asterisk "*"
+    String.replicate space " "  + String.replicate asterisk "*" + "\n"
 
-let rec baklavaShrink space asterisk =
-    if asterisk <= 1 then
-        line space 1
-    else
-        line space asterisk + "\n" + baklavaShrink (space + 1) (asterisk - 2)
+let rec baklavaShrink =
+    List.fold (fun accum n -> accum + (line n (21 - n * 2))) "" [ 1 .. 10 ]
 
-let rec baklavaGrow space asterisk =
-    if space <= 0 then
-        line 0 asterisk + "\n"
-    else
-        line space asterisk + "\n" + baklavaGrow (space - 1) (asterisk + 2)
+let rec baklavaGrow =
+    List.fold (fun accum n -> accum + (line n (21 - n * 2))) "" [ 10 .. -1 .. 0 ]
 
 let baklava =
-    baklavaGrow 10 1 + baklavaShrink 1 19
+    baklavaGrow + baklavaShrink
 
 [<EntryPoint>]
 let main argv =
-    printfn "%s" <| baklava
+    printfn "%s" <| baklava.TrimEnd()
     0
 
