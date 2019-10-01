793c98855c1a03d5476f13991c9836fc69c33bfb
Rishabh Agrawal <36743087+CaffeinatedCod3r@users.noreply.github.com>
2019-10-02 00:08:43 +0530

    Added Bubble Sort in Clojure

---
+++ archive/c/clojure/BubbleSort.clj
@@ -0,0 +1,14 @@
+(defn- bubble [ys x]
+  (if-let [y (peek ys)]
+    (if (> y x)
+      (conj (pop ys) x y)
+      (conj ys x))
+    [x]))
+
+(defn bubble-sort [xs]
+  (let [ys (reduce bubble [] xs)]
+    (if (= xs ys)
+      xs
+      (recur ys))))
+      
+(bubble-sort [3 2 1]) 
