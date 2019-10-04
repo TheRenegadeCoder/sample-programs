b5e018e5f4f116fc53682aceed80f240446323d5
Mark Magahis <mark.magahis@alertlogic.com>
2019-10-04 16:16:30 -0500

    ✨ Refactor Incrementor/Decrementor to be integer instead of list

--- archive/e/erlang/baklava.erl
+++ archive/e/erlang/baklava.erl
@@ -3,34 +3,34 @@
 
 -spec start(MaxWidth :: integer()) -> ok.
 start(MaxWidth) ->
-    baklava(MaxWidth, []),
+    baklava(MaxWidth, 0),
     ok.
 
 -spec baklava(MaxWidth :: integer(),
-              Incrementor :: list()) -> ok.
+              Incrementor :: integer()) -> ok.
 %%--------------------------------------------------------------------
 %% Recursively prints the top half of the baklava 
 %%--------------------------------------------------------------------
-baklava(MaxWidth, [_H|T] = Incrementor)  when MaxWidth =:= length(Incrementor) ->
+baklava(MaxWidth, MaxWidth) ->
     print((MaxWidth*2)+1, star),
-    reverse_baklava(MaxWidth, T);
+    reverse_baklava(MaxWidth, MaxWidth-1);
 baklava(MaxWidth, Incrementor) ->
-    print(MaxWidth-length(Incrementor), space),
-    print((length(Incrementor)*2)+1, star),
-    baklava(MaxWidth, [" " | Incrementor]).
+    print(MaxWidth-Incrementor, space),
+    print((Incrementor*2)+1, star),
+    baklava(MaxWidth, Incrementor+1).
 
 -spec reverse_baklava(MaxWidth :: integer(),
-                      Decrementor :: list()) -> ok.
+                      Decrementor :: integer()) -> ok.
 %%--------------------------------------------------------------------
 %% Recursively prints the bottom half of the baklava
 %%--------------------------------------------------------------------
-reverse_baklava(MaxWidth, []) ->
+reverse_baklava(MaxWidth, 0) ->
     print(MaxWidth, space),
     print(1, star);
-reverse_baklava(MaxWidth, [_H|T] = Decrementor) ->
-    print(MaxWidth-length(Decrementor), space),
-    print((length(Decrementor)*2)+1, star),
-    reverse_baklava(MaxWidth, T).
+reverse_baklava(MaxWidth, Decrementor) ->
+    print(MaxWidth-Decrementor, space),
+    print((Decrementor*2)+1, star),
+    reverse_baklava(MaxWidth, Decrementor-1).
 
 -spec print(Number :: integer(),
             Type :: star | space) -> ok.
