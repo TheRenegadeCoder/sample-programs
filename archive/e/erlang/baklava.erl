3ded2ec9ae3c37d6bb4cf28a828015955d5efa42
Mark Magahis <mark.magahis@alertlogic.com>
2019-10-04 16:12:14 -0500

    📝 Remove comments/debugging, add documentation

--- archive/e/erlang/baklava.erl
+++ archive/e/erlang/baklava.erl
@@ -3,46 +3,40 @@
 
 -spec start(MaxWidth :: integer()) -> ok.
 start(MaxWidth) ->
-    baklava(MaxWidth),
-    ok.
-
--spec baklava(MaxWidth :: integer()) -> ok.
-baklava(MaxWidth) ->
     baklava(MaxWidth, []),
     ok.
 
--spec baklava(MaxWidth :: integer(), Incrementor :: list()) -> ok.
+-spec baklava(MaxWidth :: integer(),
+              Incrementor :: list()) -> ok.
+%%--------------------------------------------------------------------
+%% Recursively prints the top half of the baklava 
+%%--------------------------------------------------------------------
 baklava(MaxWidth, [_H|T] = Incrementor)  when MaxWidth =:= length(Incrementor) ->
-    % print (MaxWidth-Incementor) space(s), print (Incrementor*2+1) asterisk(s) 
-    % io:format(" ~w", [(MaxWidth-length(Incrementor))]),
-    print(MaxWidth-length(Incrementor), space),
-    % io:format("*~w", [(length(Incrementor)*2)+1]),
-    print((length(Incrementor)*2)+1, star),
+    print((MaxWidth*2)+1, star),
     reverse_baklava(MaxWidth, T);
 baklava(MaxWidth, Incrementor) ->
-    % print (MaxWidth-Incementor) space(s), print (Incrementor*2+1) asterisk(s) 
-    % io:format(" ~w", [(MaxWidth-length(Incrementor))]),
     print(MaxWidth-length(Incrementor), space),
-    % io:format("*~w", [(length(Incrementor)*2)+1]),
     print((length(Incrementor)*2)+1, star),
-    % call baklava(MaxWidth, Incrementor+1)
     baklava(MaxWidth, [" " | Incrementor]).
 
--spec reverse_baklava(MaxWidth :: integer(), Decrementor :: integer()) -> ok.
+-spec reverse_baklava(MaxWidth :: integer(),
+                      Decrementor :: list()) -> ok.
+%%--------------------------------------------------------------------
+%% Recursively prints the bottom half of the baklava
+%%--------------------------------------------------------------------
 reverse_baklava(MaxWidth, []) ->
-    % io:format(" ~w", [MaxWidth]),
     print(MaxWidth, space),
-    % io:format("*~w", [(0*2)+1]),
     print(1, star);
 reverse_baklava(MaxWidth, [_H|T] = Decrementor) ->
-    % print (MaxWidth-Decrementor) space(s), print (Decrementor*2+1) asterisk(s) 
-    % io:format(" ~w", [(MaxWidth-length(Decrementor))]),
     print(MaxWidth-length(Decrementor), space),
-    % io:format("*~w", [(length(Decrementor)*2)+1]),
     print((length(Decrementor)*2)+1, star),
-    % call baklava(MaxWidth, Decrementor-1)
     reverse_baklava(MaxWidth, T).
 
+-spec print(Number :: integer(),
+            Type :: star | space) -> ok.
+%%--------------------------------------------------------------------
+%% Recursively prints the specified Number of stars or spaces
+%%--------------------------------------------------------------------
 print(0,star) ->
     io:format("~n");
 print(0,_) ->
