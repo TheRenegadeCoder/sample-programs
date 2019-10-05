5d271d355f942157fad150a9fcd75fe1c326fbf0
Chris Athanas <realityexpanderdev@gmail.com>
2019-10-05 08:14:22 -0500

    Create UseOfWhenForTypeChecking.kt

---
+++ archive/k/kotlin/UseOfWhenForTypeChecking.kt
@@ -0,0 +1,16 @@
+fun describe(obj: Any): String =
+    when (obj) {
+        1          -> "One"
+        "Hello"    -> "Greeting"
+        is Long    -> "Long"
+        !is String -> "Not a string"
+        else       -> "Unknown"
+    }
+
+fun main() {
+    println(describe(1))
+    println(describe("Hello"))
+    println(describe(1000L))
+    println(describe(2))
+    println(describe("other"))
+}
