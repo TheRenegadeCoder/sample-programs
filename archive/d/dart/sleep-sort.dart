1bc62215c00f816a91ebdb0280ae905370d833bc
agilob <info@agilob.net>
2019-10-07 19:36:52 +0100

    Use completer and futures for sleep sort

--- archive/d/dart/sleep-sort.dart
+++ archive/d/dart/sleep-sort.dart
@@ -1,23 +1,26 @@
+import 'dart:async';
+
+// massivly overcomplicated sleep sort to demonstrate Completer and Future mechanisms of Dart
 void main(List<String> args) async {
-    await execFutures(
-      sleepSort(List.unmodifiable([1, 2, 3])
-      )).then((r) => print(r));
+    print(await execFutures(
+      sleepSort(
+        List.unmodifiable([1, 2, 3, 7])
+      )));
 }
 
 List<Future<int>> sleepSort(List<int> elements) {
   List<Future<int>> futures = List();
   elements.forEach((e) {
-    futures.add(Future.delayed(Duration(seconds: e)));
+    futures.add(Future.delayed(Duration(seconds: e), () => e));
   });
   return futures;
 }
 
-Future<List<int>> execFutures(List<Future<int>> futures) {
-  List<int> results = List();
-  futures.forEach((f) {
-  f.then((result) {
-      results.add(result);
-    });
+Future<List<int>> execFutures(List<Future<int>> futures) async {
+  Completer<List<int>> c = Completer();
+  Future.wait(futures).then((f) {
+    c.complete(f);
   });
-  return results;
+
+  return c.future;
 }
