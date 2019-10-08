0aef492602166223d8b8c8f9adf74dfbe9a7b4ee
Sayantan <belikesayantan12@gmail.com>
2019-10-08 20:51:40 +0530

    Longest Palindromic Substring in Python

---
+++ archive/p/python/longest_palindrome_substring.py
@@ -0,0 +1,33 @@
+"""
+Given a string S, find the longest palindromic substring in S.
+"""
+def longestPalindrome(string):
+    longest = ""
+
+    centres = [len(string) - 1]
+    for diff in range(1, len(string)):
+        centres.append(centres[0] + diff)
+        centres.append(centres[0] - diff)
+
+    for centre in centres:
+
+        if(min(centre + 1, 2 * len(string) - 1 - centre) <= len(longest)):
+            break
+        if centre % 2 == 0:
+            left, right = (centre // 2) - 1, (centre // 2) + 1
+        else:
+            left, right = centre // 2, (centre // 2) + 1
+
+        while left >= 0 and right < len(string) and string[left] == string[right]:
+            left -= 1
+            right += 1
+        
+        if right - left > len(longest):
+            longest = string[left + 1: right]
+        
+    return longest
+
+if __name__ == '__main__':
+    string = str(input("Enter String: "))
+    sub = longestPalindrome(string)
+    print("Longest Palindromic Substring is: {}".format(sub))
