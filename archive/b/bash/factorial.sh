edf8a09c6ae3ae3f6236f6d7423f45f2ef530ec5
Amanda Hager Lopes de Andrade Katz <amanda.katz@serpro.gov.br>
2019-10-09 10:00:24 -0300

    Add Factorial in bash #1219

---
+++ archive/b/bash/factorial.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+number=$1
+
+if [[ -z "$number" ]]
+then
+	echo "Usage: please input a non-negative integer"
+	exit 1
+fi
+
+if ! [[ "$number" =~ ^[0-9]+$ ]]  
+then 
+        echo "Usage: please input a non-negative integer"
+        exit 1
+fi
+
+value=1
+
+while [ $number -gt 1 ]
+do
+
+  value=$((value * number))
+ 
+  number=$((number - 1))
+
+done
+
+echo $value
