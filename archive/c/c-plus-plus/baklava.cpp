9aefbb552e9f069c6f25570e6f203fb1d2fdbed6
Behnam Ahmad khan beigi <yottanami@gmail.com>
2019-10-09 12:57:32 +0330

    Create Baklava

---
+++ archive/c/c-plus-plus/baklava.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  int i,j,k,n;
+  cin>>n;
+  for(i=1; i<=(n+1)/2; )         
+    {                  
+      for(k=1; (n+1)/2-i+1>=k; k++)
+        cout<<" ";
+      for(j=1; j<2*i; j++)
+        cout<<"*";
+      cout<<"\n";                  i++;       
+    }                  
+  if(2*i-1>=n)
+    {
+      for(i=(n+1)/2-1; i>=1; i--)
+        {
+          for(k=1; (n+1)/2-i+1>=k; k++)
+            cout<<" ";
+          for(j=1; j<2*i; j++)
+            cout<<"*";
+          cout<<"\n";
+        }
+    }
+  system("pause");
+  return(0);
+}
