59df95a8b213ff0a19f4b66c1a8d3ce0e795a1c8
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-10-10 12:07:09 -0400

    Update and rename string_reverse.php to reverse-string.php

--- archive/p/php/reverse-string.php
+++ archive/p/php/reverse-string.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * by: Ricky Putra
- * its assumed that the file is running through command line
- * inputs are read from the same.
- */
-
   $input = $argv[1];
   $length = strlen($input);
   for ($i=($length-1) ; $i >= 0 ; $i--) {  
