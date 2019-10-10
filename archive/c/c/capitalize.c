5df5c3c0f3e4550c71a52ae1e9278550f788330d
Softizo <appmaker4fun@outlook.com>
2019-10-09 18:38:19 -0700

    Fixed grammar mistake

--- archive/c/c/capitalize.c
+++ archive/c/c/capitalize.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[]) {
     } else if(argc > 2) {
         printf("Use quotes around multiple strings.\n");
     } else {
-        printf("Expected an string argument.\n");
+        printf("Expected a string argument.\n");
     }
 
     return 0;
