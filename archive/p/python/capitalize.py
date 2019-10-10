3135dc82ab8eb2a9e3f00f8363882086e7c2c2b4
Manan Gill <11139125+gmananan@users.noreply.github.com>
2019-10-09 21:57:44 -0400

    Addressed case where no input is provided
    Addressing change in PR #1543 to the specification of the problem

--- archive/p/python/capitalize.py
+++ archive/p/python/capitalize.py
@@ -7,4 +7,7 @@ def capitalize(input):
         print('Input is not a string')
 
 if(__name__ == '__main__'):
-    capitalize(sys.argv[1]) #most arguments from argv will be treated as a string
+    if(len(sys.argv) == 1):
+        print('Usage: provide a string as the first command line argument')
+    else:
+        capitalize(sys.argv[1]) #most arguments from argv will be treated as a string
