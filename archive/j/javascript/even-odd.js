090bcbcbd49e02775d52ba38f6546d827de1a70a
Manan Gill <11139125+gmananan@users.noreply.github.com>
2019-10-11 17:38:55 -0400

    Capitalized 'Even' and 'Odd'
    Co-Authored-By: Jeremy Grifski <jeremy.grifski@gmail.com>

--- archive/j/javascript/even-odd.js
+++ archive/j/javascript/even-odd.js
@@ -2,5 +2,5 @@ const input = Number(process.argv[2]); //coerce the input into a number if possi
 if(!Number.isInteger(input)){ //if there is no input, input = undefined and the statement still prints
     console.log('Usage: please input a number');
 } else {
-    console.log(input%2 === 0 ? 'even' : 'odd');
+    console.log(input%2 === 0 ? 'Even' : 'Odd');
 }
