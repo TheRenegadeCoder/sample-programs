808c7b0c1f75ad44d2a913fb88f342fab2ffc932
Sanjay Pradeep <sanjay.siddha3@gmail.com>
2019-10-11 13:09:15 +0530

    Doc addition to make it more readable and understandable

--- docs/projects/quick-sort/quick-sort.js
+++ docs/projects/quick-sort/quick-sort.js
@@ -1,9 +1,24 @@
-var items = [5,3,7,6,2,9];
+// LANGUAGE : JavaScript
+// Author: Sanjay Siddha Pradeep Kumar
+// Created Date: 11 October, 2019
+// Twitter Account : https://twitter.com/sanjaysiddha
+
+/*
+@func: swap
+@args: items, leftIndex, rightIndex
+@desc: swapping elements in a list/array
+*/
 function swap(items, leftIndex, rightIndex){
     var temp = items[leftIndex];
     items[leftIndex] = items[rightIndex];
     items[rightIndex] = temp;
 }
+
+/*
+@func: partition
+@args: items, left, right
+@desc: partitioning total item in array
+*/
 function partition(items, left, right) {
     var pivot   = items[Math.floor((right + left) / 2)], //middle element
         i       = left, //left pointer
@@ -24,6 +39,11 @@ function partition(items, left, right) {
     return i;
 }
 
+/*
+@func: quickSort
+@args: items, left and right
+@desc: returns you the output 
+*/
 function quickSort(items, left, right) {
     var index;
     if (items.length > 1) {
@@ -37,6 +57,7 @@ function quickSort(items, left, right) {
     }
     return items;
 }
+var items = [5,3,7,6,2,9];
 // first call to quick sort
 var sortedArray = quickSort(items, 0, items.length - 1);
 console.log(sortedArray); //prints [2,3,5,6,7,9]
