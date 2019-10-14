f440d5037c51ddad821f5d2d4625692a6985923c
yottanami <yottanami@gmail.com>
2019-10-14 11:04:47 +0330

    First space removed from output of baklava.cpp

--- archive/c/c-plus-plus/baklava.cpp
+++ archive/c/c-plus-plus/baklava.cpp
@@ -8,7 +8,7 @@ int main(int argc, char *argv[])
   n = 21;
   for(i=1; i<=(n+1)/2; )
     {
-      for(k=1; (n+1)/2-i+1>=k; k++)
+      for(k=1; (n+1)/2-i>=k; k++)
         cout<<" ";
       for(j=1; j<2*i; j++)
         cout<<"*";
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     {
       for(i=(n+1)/2-1; i>=1; i--)
         {
-          for(k=1; (n+1)/2-i+1>=k; k++)
+          for(k=1; (n+1)/2-i>=k; k++)
             cout<<" ";
           for(j=1; j<2*i; j++)
             cout<<"*";
