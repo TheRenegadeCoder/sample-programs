bd92de3296b603b341a7f4510e19cb9cdf0b01d0
Gabriela Kandová <gabika.kandova@gmail.com>
2019-10-14 22:44:32 +0200

    Added Factorial in Perl

---
+++ archive/p/perl/factorial.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+# accept input as argument
+my ($number) = @ARGV;
+
+# if not provided, read from standard input
+if (!defined $number) {
+	$number = <STDIN>;
+	chomp $number;
+}
+
+if (!defined $number || $number !~ /\-*\d/ || $number < 0) {
+	print "Usage: please input a non-negative integer\n";
+	exit;
+}
+
+my $factorial = 1;
+
+for (my $i = 1; $i <= $number; $i++) {
+	$factorial = $factorial * $i;
+}
+
+print "$factorial\n";
