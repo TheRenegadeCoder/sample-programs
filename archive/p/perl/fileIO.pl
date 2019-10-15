070a0f616cc8f60c8f9e0e542cf3ca3f9df2d47f
Ewerton Queiroz <ewerton.queiroz@outlook.com>
2019-10-15 01:29:31 -0300

    Subroutines Main(), Write() and Read() in fileIO.pl

--- archive/p/perl/fileIO.pl
+++ archive/p/perl/fileIO.pl
@@ -1,17 +1,27 @@
 #!/usr/bin/env perl
 
-open(my $writing, ">output.txt") || die "File could not be written.\nError: $!";
-
-print $writing "Some arbitrary data.\n";
-
-close($writing) || die "The file could not be closed on write.\nError: $!";
-
-open(my $reading, "<output.txt") || die "File could not be readed.\nError: $!";
-
-while (!eof($reading)) {
-	print <$reading>;
+sub Main {
+	Write("Some arbitrary data.");
+	Read();
+	exit(0);
 }
 
-close($reading) || die "The file could not be closed on reading.\nError: $!";
+sub Write {
+	open(my $writing, ">output.txt") || die "File could not be written.\nError: $!";
 
-exit(0);
+	print $writing "@_"."\n";
+
+	close($writing) || die "The file could not be closed on write.\nError: $!";
+}
+
+sub Read {
+	open(my $reading, "<output.txt") || die "File could not be readed.\nError: $!";
+
+	while (!eof($reading)) {
+		print <$reading>;
+	}
+
+	close($reading) || die "The file could not be closed on reading.\nError: $!";
+}
+
+Main();
