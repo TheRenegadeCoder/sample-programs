553bbd61dea1c796e6be265208dbc51875830e8b
Amasha007 <sewminiamasha77@gmail.com>
2019-10-16 21:37:11 +0530

    added c sharp program to find square root

---
+++ archive/c/c-sharp/SquareRoot.cs
@@ -0,0 +1,15 @@
+// C# program to illustrate the 
+// Math.Sqrt() method 
+using System; 
+  
+class GFG { 
+  
+    // Main Method 
+    public static void Main() 
+    { 
+        double x = 81; 
+  
+        // Input positive value, Output square root of x 
+        Console.Write(Math.Sqrt(x)); 
+    } 
+} 
