6e1c55b24276c39f9f635b9a64d65f1a74b4e00a
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-10-16 00:07:41 -0400

    Removed unit tests and renamed file to match naming scheme

--- archive/e/elixir/longest-common-subsequence.ex
+++ archive/e/elixir/longest-common-subsequence.ex
@@ -1,6 +1,3 @@
-# Run Command: elixir longest_common_subsequence.ex "1, 2, 3" "2, 3, 4"
-# created date : 14 October, 2019
-
 defmodule LongestCommonSubsequence do
   def main() do
     lcs = solve(System.argv())
@@ -51,33 +48,4 @@ defmodule LongestCommonSubsequence do
   end
 end
 
-LongestCommonSubsequence.main
-
-ExUnit.start()
-
-defmodule LongestCommonSubsequenceTests do
-  use ExUnit.Case
-
-  test "No input" do
-    assert is_bitstring(LongestCommonSubsequence.solve([]))
-  end
-
-  test "Empty input" do
-    assert is_bitstring(LongestCommonSubsequence.solve([""]))
-  end
-
-  test "Missing Input" do
-    assert is_bitstring(LongestCommonSubsequence.solve(["25, 15, 10, 5"]))
-  end
-
-  test "Sample Input: Same Length" do
-    assert LongestCommonSubsequence.solve(["1, 4, 5, 3, 15, 6", "1, 7, 4, 5, 11, 6"]) == "1, 4, 5, 6"
-  end
-
-  test "Sample Input: Different Lengths" do
-    assert LongestCommonSubsequence.solve(["1, 4, 8, 6, 9, 3, 15, 11, 6", "1, 7, 4, 5, 8, 11, 6"]) ==
-             "1, 4, 8, 11, 6"
-  end
-end
-
-
+LongestCommonSubsequence.main()
