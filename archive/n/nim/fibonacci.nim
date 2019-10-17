a063d2933ee1bb8df38ee8158b3872a20644dfcc
Michael Olson <molson@quicketsolutions.com>
2019-10-16 23:19:56 -0500

    Updating Fibonacci to take command-line arguments and handle invalid input

--- archive/n/nim/fibonacci.nim
+++ archive/n/nim/fibonacci.nim
@@ -1,19 +1,30 @@
 # Fibonacci Sample Program in Nim
+# Input the number of iterations as a command-line parameter
+import os
 import strutils
 
-echo "Input the number of iterations (n): "
-let n = stdin.readline.parseBiggestUInt
-echo "------------------------------`"
+var n: BiggestUInt
+try:
+    n = paramStr(1).parseBiggestUInt
+except IndexError:
+    echo "Usage: please input the count of fibonacci numbers to output"
+    quit(1)
+except ValueError:
+    echo "invalid input: not a number"
+    quit(1)
 
 var previouspreviousInt: BiggestUInt
 var previousInt: BiggestUInt = 0
 var currentInt: BiggestUInt = 1
 
-for i in 1..n:
+if n == 0:
+    echo ""
+    quit(0)
+
+for i in 1..<n:
     previouspreviousInt = previousInt
     previousInt = currentInt
     currentInt = previouspreviousInt + previousInt
-    echo currentInt
 
-echo "------------Done!-------------`"
+echo currentInt
 
