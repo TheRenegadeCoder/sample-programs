43f0ceb3611df7220616617e02e0adbf55e325ff
Sailok Chinta <sailokchinta2012@gmail.com>
2019-10-24 14:37:56 +0530

    Update selection-sort.cpp
    Implemented the required checks.

--- archive/c/c-plus-plus/selection-sort.cpp
+++ archive/c/c-plus-plus/selection-sort.cpp
@@ -10,33 +10,84 @@ void swap(int *x,int *y){
 	*y=t;
 }
 
+//Function to handle errors
+void handle_error(){
+	cout<<"Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\""<<endl;
+	exit(0);
+}
+
+//Function to check whether inputs satisfy given constraints
+int check(string s){
+	int x1=0,x2=s.size()-1;
+
+	//x1 gives first index position where integer occurs
+	for(int i=0;i<s.size();i++){
+		if(s[i]!=' '){
+			x1=i;
+			break;
+		}
+	}
+
+	//x2 gives last index position where integer occurs
+	for(int i=s.size()-1;i>=x1;i--){
+		if(s[i]!=' '){
+			x2=i;
+			break;
+		}
+	}
+	
+	//if any space occurs between this substring then throw error
+	for(int i=x1;i<=x2;i++){
+		if(s[i]==' '){
+			handle_error();
+		}
+	}
+
+	return stoi(s);
+}
+
+//Function for converting string input into integer vector 
+vector<int> convert(string s){
+	/*
+		Loop to convert numbers in string to integers
+	*/
+	vector<int> v;
+	string num="";
+	for(int i=0;i<s.size();i++){
+		if((int)s[i]>=48 && (int)s[i]<=57 || s[i]==' '){
+			num+=s[i];
+		}else if(s[i]==','){
+			v.push_back(check(num));
+			num="";
+		}else{
+			handle_error();	
+		}
+	}
+
+
+	if(num.size()>0){
+		v.push_back(check(num));
+	}
+
+	return v;
+}
+
 int main(int argc,char* argv[]){
 	/*
 		Condition to check for No String as Input
 	*/
 	if(argc<2){
-		cout<<"Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\""<<endl;
-		exit(0);
+		handle_error();
 	}
 
-	vector<int> v;
-	string s=argv[1];
-	
-	/*
-		Conditon to check if given input has more than 2 integers and doesnot contain any spaces
-	*/
-	if(s.size()<4 || s.find(" ")!=string::npos){
-		cout<<"Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\""<<endl;
-		exit(0);
-	}
+	vector<int> v=convert(argv[1]);
 
 	/*
-		Loop to convert numbers in string to integers
+		Conditon to check if given input has more than 2 integers
 	*/
-	for(int i=0;i<s.size();i++){
-		if(s[i]!=','){
-			v.push_back((int)(s[i]-'0'));
-		}
+	if(v.size()<1){
+		cout<<"Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\""<<endl;
+		exit(0);
 	}
 
 	int n=v.size();
