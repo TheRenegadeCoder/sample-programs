02f46f46f6c884fb8fab88a5d3a6af877bc9ea81
rizkimufti <rizkimufti@gmail.com>
2019-10-24 20:14:45 +0700

    add array cpp

---
+++ archive/c/c-plus-plus/array.cpp
@@ -0,0 +1,15 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+int i, marks[]={80, 62, 70, 90, 98}; //declaration and initialization of array
+clrscr();
+
+//traversal of array 
+for(i=0;i<5;i++)
+{
+cout<<endl<<marks[i];
+}
+getch();
+}
