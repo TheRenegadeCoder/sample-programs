dd7912e92b575753be4ae040eaa132e6112131f9
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-10-26 19:47:32 -0400

    Updated solution to pull from command line

--- archive/j/javascript/capitalize.js
+++ archive/j/javascript/capitalize.js
@@ -2,4 +2,4 @@ function capitalize(stringToCapitalize) {
     return stringToCapitalize[0].toUpperCase() + stringToCapitalize.slice(1);
 }
 
-console.log(capitalize("mynewstringcapitalize"))
+console.log(capitalize(process.argv[2]))
