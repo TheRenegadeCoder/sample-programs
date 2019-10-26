ce225d247c26b2034332380e95b183d5641621f1
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-10-26 19:36:29 -0400

    Rewrote capitalize implementation
    Unfortunately, Python's capitalize implementation isn't quite as simple as ours.

--- archive/p/python/capitalize.py
+++ archive/p/python/capitalize.py
@@ -1,13 +1,11 @@
 import sys
 
 def capitalize(input):
-    if(isinstance(input, str)):
-        print(input.capitalize())
-    else:
-        print('Input is not a string')
+    if len(input) > 0:
+        print(input[0].capitalize() + input[1:])
 
-if(__name__ == '__main__'):
+if __name__ == '__main__':
     if(len(sys.argv) == 1):
-        print('Usage: please provide a string')
+        print('Usage: provide a string as the first command line argument')
     else:
-        capitalize(sys.argv[1]) #most arguments from argv will be treated as a string
+        capitalize(sys.argv[1])
