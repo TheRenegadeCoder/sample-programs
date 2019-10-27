851447ffcb22b0578548588ad527d7544cf61730
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-10-26 21:41:24 -0400

    Reworked C++ capitalize

--- archive/c/c-plus-plus/capitalize.cpp
+++ archive/c/c-plus-plus/capitalize.cpp
@@ -7,19 +7,11 @@ int main(int argc, const char *argv[])
         std::cout << "Usage: please provide a string";
         return 1;
     }
-
-    for (int i = 1; i < argc; i++) {
-        if (i == 1) {                                              //If it is the first letter of the entire string
-            for (int j = 0; j < (int) std::strlen(argv[1]); j++) { //Then iterate through the first word passed
-                if (j == 0)                                        //If it is the first letter of the first word
-                    std::cout << (char) ((*argv[1]) - 0x20);       //Print out the current characters value - 0x20 to get the uppercase
-                else
-                    std::cout << *(argv[1] + sizeof(char)*j);      //Otherwise just print out the character
-            }
-        } else
-            std::cout << argv[i];                                  //If not the first string, then just print out the current word
-
-        if (i < argc - 1)                                          //Make sure to not output an extra space at the end
-            std::cout << " ";
+    
+    for (int j = 0; j < (int) std::strlen(argv[1]); j++) { 
+        if (j == 0)                                        
+            std::cout << (char) toupper(argv[1][j]);      
+        else
+            std::cout << *(argv[1] + sizeof(char)*j);      
     }
 }
