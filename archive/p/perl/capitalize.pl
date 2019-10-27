d2a43067962c0bab08450e4b3b64404b191514d5
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-10-26 20:37:32 -0400

    Added length condition in perl

--- archive/p/perl/capitalize.pl
+++ archive/p/perl/capitalize.pl
@@ -5,7 +5,7 @@ use warnings;
 # accept input as argument
 my ($string) = @ARGV;
 
-if (!defined $string) {
+if (!defined $string || length $string == 0) {
 	print "Usage: please provide a string\n";
 	exit;
 }
