f99448418f02473717ce7e45955a87135408ca7c
agilob <info@agilob.net>
2019-10-27 19:38:13 +0000

    Accept input from user, parse string and remove invalid values

--- archive/d/dart/sleep-sort.dart
+++ archive/d/dart/sleep-sort.dart
@@ -1,36 +1,18 @@
 import 'dart:async';
 
-// massivly overcomplicated sleep sort to demonstrate Completer and Future mechanisms of Dart
 void main(List<String> args) async {
-  // print "subscribes" to `execFutures" and awaits for returned value from `Completer.future` which will contain
-  // a sorted array 
-    print(await execFutures(
-      sleepSort(
-        List.unmodifiable([1, 2, 3, 7])
-      )));
+
+  List<int> userInput = args.map((str) => int.tryParse(str))
+    .takeWhile((test) => test != null)
+    .toList();
+
+  List<int> sorted = await sleepsort(userInput);
+
+  print(sorted);
 }
 
-// builds a list of futues, where each of them sleeps for `e` number of seconds and then returns `e` as int value
-List<Future<int>> sleepSort(List<int> elements) {
-  List<Future<int>> futures = List();
-  elements.forEach((e) {
-    futures.add(Future.delayed(Duration(seconds: e), () => e));
-  });
-  return futures;
-}
-
-// Accepts list of Futures as input
-// builds a type-safe completer which holds results of all futures and allows
-// to pass the values to another blocking method
-Future<List<int>> execFutures(List<Future<int>> futures) async {
-  Completer<List<int>> c = Completer();
-  Future.wait(futures)
-  // when is a subscriber for value of completed futures
-  // as I called Future.await
-  // this will wait for all futures to complete before returning sorted values
-  .then((f) {
-    c.complete(f);
-  });
-
-  return c.future;
+Future<List<int>> sleepsort(Iterable<int> input) async {
+  List<int> sorted = List();
+  await Future.wait(input.map((i) => Future.delayed(Duration(seconds: i), () => sorted.add(i))));
+  return Future.value(sorted);
 }
