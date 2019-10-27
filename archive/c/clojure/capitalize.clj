ec4a433290aabbf0034b2c9e96a6d342803a4302
Jeremy Grifski <jeremy.grifski@gmail.com>
2019-10-26 23:49:48 -0400

    Implemented my own capitalize method
    Uses string concatenation of substrings to reconstruct the final string

--- archive/c/clojure/capitalize.clj
+++ archive/c/clojure/capitalize.clj
@@ -9,10 +9,17 @@
 (defn- print-error []
   (println "Usage: please provide a string"))
 
+(defn- capitalize [s]
+  (str 
+    (clojure.string/upper-case 
+      (subs s 0 1)) (subs s 1)
+  ))
+
 (defn main [args]
   (if (is-valid-input args) 
-    (println(clojure.string/capitalize (first args)))
+    (println (capitalize (first args)))
     (print-error)
   ))
+
 
 (main *command-line-args*)
