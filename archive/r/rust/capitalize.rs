876f9ad1bb007daf631b761bd376dec72354708c
Mallikarjuna S J <msj2@users.noreply.github.com>
2019-10-28 10:22:17 +0530

    Capitalise Letter 1 of the Input String
    Capitalise Letter 1 of the Input String

--- archive/r/rust/capitalize.rs
+++ archive/r/rust/capitalize.rs
@@ -1,20 +1,23 @@
-// Capitalize the first letter of a string
+// Accept a string from User and Capitalize the first letter of that string
 // Rust is built to support not just ASCII, but Unicode by-default.
 // Do note, each character in string is multi-byte UTF-8 supported, which needs upto 3 bytes (to accommodate Japanese letters)
-
-// function names have to be strictly in snake case or small case
-fn to_title_case(s: &str) -> String {
-    let mut buff = s.chars();
-    match buff.next() {
-        None => String::new(),
-        Some(f) => f.to_uppercase().chain(buff).collect(),
-    }
-}
-
+// Best part of Rust compiler issues warning to remove unused variables, functions, ...
 fn main() {
-    println!("{}", to_title_case("rust"));
-    println!("{}", to_title_case("turns"));
-    println!("{}", to_title_case("dust"));
-    println!("{}", to_title_case("to"));
-    println!("{}", to_title_case("best"));
-}
+    // Use Std. Input Device
+    use std::io::{stdin};
+    // Create a new String Object
+    let mut input_value = String::new();
+    // Read a mutable input value from Std Input with newline at the end. If it fails, throw Error Message
+    stdin().read_line(&mut input_value).expect("Error: reading from keyboard failed");
+    // Chomp or Remove the new line at the end of string
+    input_value = input_value.trim_end().to_string();
+    // COnvert the String Object into Vectorr
+	let mut buff: Vec<char> = input_value.chars().collect();
+    // Convert 1'st character to Capital letter, Throw Error in case of null string
+	buff[0] = buff[0].to_uppercase().nth(0).expect("Updating letter 1 to uppper case failed");
+    // Convert vector to String 
+	let buff_hold: String = buff.into_iter().collect();
+    // Print Updated Value
+	
+	println! ("String with letter1 capitalised is {:?}", buff_hold);
+        }
