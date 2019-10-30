855672e9c2d4073460a5ee1b6fbeb70738132e35
Nate Damon <ndamon@moog.com>
2019-10-30 12:37:00 -0400

    Removed extra whitespaces from previous code

--- archive/p/python/roman_numeral.py
+++ archive/p/python/roman_numeral.py
@@ -1,4 +1,4 @@
-# Python program to convert Roman Numerals 
+# Python program to convert Roman Numerals
 # to Numbers
 
 import sys
@@ -24,34 +24,34 @@ def value(r):
 def roman_to_decimal(string):
     res = 0
     i = 0
-  
+
     while i < len(string):
-  
-        # Getting value of symbol s[i] 
+
+        # Getting value of symbol s[i]
         s1 = value(string[i])
-  
-        if i+1 < len(string):
-  
-            # Getting value of symbol s[i+1] 
+
+        if i + 1 < len(string):
+
+            # Getting value of symbol s[i+1]
             s2 = value(string[i + 1])
-  
-            # Comparing both values 
+
+            # Comparing both values
             if s1 >= s2:
-  
-                # Value of current symbol is greater 
-                # or equal to the next symbol 
-                res += s1 
+
+                # Value of current symbol is greater
+                # or equal to the next symbol
+                res += s1
                 i += 1
-            else: 
-  
-                # Value of current symbol is less than 
-                # to the next symbol 
-                res = res + s2 - s1 
+            else:
+
+                # Value of current symbol is less than
+                # to the next symbol
+                res = res + s2 - s1
                 i += 2
-        else: 
-            res += s1 
+        else:
+            res += s1
             i += 1
-  
+
     return res
 
 
