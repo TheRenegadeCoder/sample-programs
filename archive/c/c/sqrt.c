e39778b7acd4ee212de48563ad7e2beaad181a0f
HARSH <iamhp2k@gmail.com>
2019-10-31 23:47:47 +0530

    Create sqrt.c

---
+++ archive/c/c/sqrt.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+//program used To find Square Root of Any Number
+
+void main()
+
+            {
+                float a; 
+                scanf(" %f ",&a);
+                printf("Square Root Of numbers: %f",pow(a,1/2.0)); 
+		// pow is function in math.h takes parameters as pow(number,exponent);
+
+            }
