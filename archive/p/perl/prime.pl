20f6e92329f495f8eeb8343dfb392556ed31ad8c
Mallikarjuna S J <msj2@users.noreply.github.com>
2019-10-31 19:30:18 +0530

    Check if a number is prime or composite
    Accept a number on command line and print if it is Prime or Composite
    Prime Numbers will always be Odd and have only 1 Divisor, itself..  Use that to determine Composite.
    If

---
+++ archive/p/perl/prime.pl
@@ -0,0 +1,32 @@
+# Requirement https://sample-programs.therenegadecoder.com/projects/prime-number/
+# Issue  #1834
+# Accept a number on command line and print if it is Prime or Composite
+# Prime Numbers will always be Odd and have only 1 Divisor, itself..  Use that to determine Composite.
+
+use warnings;
+
+my ($prime) = @ARGV;
+
+if ( $prime <= 0 ) {
+    print("please input a non-negative integer");
+}
+else {
+    if ( $prime % 2 == 0 ) {
+        print("Composite");
+    }
+    else {
+        $i               = 0;
+        $num_of_divisors = 0;
+        for ( $i = $prime ; $i > 1 ; $i = $i - 2 ) {
+            if ( $prime % $i == 0 ) {
+                $num_of_divisors += 1;
+            }
+        }
+        if ( $num_of_divisors > 2 ) {
+            print("Composite");
+        }
+        else {
+            print("Prime");
+        }
+    }
+}
