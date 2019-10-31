097e3f28e6029ad431aee7af9424f3c958a0fc80
Mallikarjuna S J <msj2@users.noreply.github.com>
2019-10-31 17:25:13 +0530

    Return Factorial of number (less than 35)
    accept a number on command line (works till 34)
    print it's factorial.

---
+++ archive/r/rust/factorial.rs
@@ -0,0 +1,28 @@
+//  Requirement     https://sample-programs.therenegadecoder.com/projects/factorial/
+// Accept a number on command line and print it's factorial
+// Works till factorial 34, which is 39 digits, ...
+fn main() {
+    //confirm integer is passed as commandline argument
+    let mut input_value = std::env::args().nth(1).expect("please input a non-negative integer");
+    // Trim the trailing newline
+    input_value = input_value.trim_end().to_string();
+    //String to Int
+
+    let input_num: u128 = input_value
+        .trim()
+        .parse()
+        .expect("please input a non-negative integer");
+    // let mut index = input_num;
+    // let mut factorial = input_num;
+    // for index in input_num..1 {
+    //     index = index -1;
+    //     factorial = factorial * index;
+    // }
+    let mut n = input_num as u128;
+    let mut result = 1;
+    while n > 0 {
+        result = result * n;
+        n = n - 1;
+    }
+    println!("{}", result );
+}
