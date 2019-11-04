1a8b1db9490b29345847e849723f33f158c5b3e9
Mallikarjuna S J <msj2@users.noreply.github.com>
2019-11-04 16:47:51 +0530

    Send a String  and Send Empty Value
    Test Case 1: Send a String  and Test Case 2: Empty Value

--- archive/r/ruby/even-odd.rb
+++ archive/r/ruby/even-odd.rb
@@ -1,9 +1,18 @@
 # Requirement https://sample-programs.therenegadecoder.com/projects/even-odd/
 # Issue 1839
+# Test Case 1: Send a String
+# $ruby odd_even_1.rb nikhil
+# Usage: please input a number
+# Test Case 2: Empty Value
+# $ ruby odd_even_1.rb
+#Usage: please input a number
 
 if ARGV.length < 1
     puts "Usage: please input a number"
 else
+    if ARGV.empty?
+        puts "Usage: please input a number"
+    end
     string1 = ARGV[0]
     num = Integer(string1)
 
