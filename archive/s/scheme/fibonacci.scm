17612a9c4cf5f01d148ade63b3e1f961b13bd846
lbarros00 <laisa.barros26@gmail.com>
2019-11-03 21:52:15 -0800

    add error handling and update fib 0 to return nothing

--- archive/s/scheme/fibonacci.scm
+++ archive/s/scheme/fibonacci.scm
@@ -1,6 +1,9 @@
 (define (fibonacci n)
-  (cond ((= n 0) 1)
-	((= n 1) 1)
-	(else (+ (fibonacci (- n 1)) (fibonacci (- n 2))))))
+  (cond ((number? n)
+         (cond ((= n 0) "")
+               ((= n 1) 1)
+               ((= n 2) 1)
+               (else (+ (fibonacci (- n 1)) (fibonacci (- n 2))))))
+        (else (begin (display "Usage: please input the count of fibonacci numbers to output")))))
 
 (display (fibonacci (read)))
