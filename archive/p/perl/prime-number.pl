d69289653e7d49cd2e81928efe59af074f87128c
Mallikarjuna S J <msj2@users.noreply.github.com>
2019-11-04 19:38:54 +0530

    0 must be composite number
    0 must be composite number

--- archive/p/perl/prime-number.pl
+++ archive/p/perl/prime-number.pl
@@ -1,25 +1,30 @@
 # Requirement https://sample-programs.therenegadecoder.com/projects/prime-number/
 # Issue  #1834
 # Accept a number on command line and print if it is Prime or Composite
-# Prime Numbers will always be Odd and have only 1 Divisor, itself..  Use that to determine Composite.
+# Prime Numbers will have only 1 Divisor, itself..  Use that to determine Composite.
 
 use warnings;
 
 my ($prime) = @ARGV;
 
-if ($#ARGV < 2){
-    print("Usage: please input a non-negative integer");
+$num_args = $#ARGV + 1;
+
+if ($num_args <1 ) {
+    print "PHAT   Usage: please input a non-negative intege";
     exit(0);
 }
-if ( $prime <= 0 ) {
+
+if ( $prime < 0 ) {
     print("Usage: please input a non-negative integer");
     exit(0);
 }
 else {
     if ( $prime % 2 == 0 ) {
         print("Composite");
+        exit(0);
     }
     else {
+
         $i               = 0;
         $num_of_divisors = 0;
         for ( $i = $prime ; $i > 1 ; $i = $i - 2 ) {
