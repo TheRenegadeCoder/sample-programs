42df52ea7f7d6836609b941c5225e2514b10bd8b
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-03-23 23:50:04 -0400

    Cleaned up Python syntax

--- string_palindrome.py
+++ string_palindrome.py
@@ -1,12 +1,8 @@
-##This program code can be used to check whether a string is a palindrome or not
-
-string=str(input("Enter the string:"))
-L=len(string)
-for i in range(L,0,-1):
-for j in range(L):
-  if(string[i]==string[j]):
-    print("Palindrome.")
-  else:
-    print("Not  Palindrome.")
-  
-  
+string = str(input("Enter the string:"))
+L = len(string)
+for i in range(L, 0, -1):
+  for j in range(L):
+    if(string[i] == string[j]):
+      print("Palindrome.")
+    else:
+      print("Not  Palindrome.")  
