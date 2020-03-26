68d4cf861af7a742ca189aca0f3821b9f52cffd9
Maximillian Naza <maximillianaza@gmail.com>
2020-03-25 22:10:17 -0700

    Update game of life implementation

--- archive/p/python/game_of-life.py
+++ archive/p/python/game_of-life.py
@@ -1,120 +1,173 @@
-from tkinter import *
-
-root = Tk()
-root.title("The Renegade Coder's Game of Life")
-frame = Frame(root, width=720, height=720)
-frame.pack()
-canvas = Canvas(frame, width=720, height=720)
-canvas.pack()
-
-def create_grid():
-	x = 10
-	y = 10
-	global grid # Variable to store the Cell objects
-	global rectangles # Variable to store rectangles
-	rectangles = []
-	grid = []
-	for i in range(70):
-		grid.append([])
-		rectangles.append([])
-		for j in range(70):
-			rect = canvas.create_rectangle(x, y, x+10, y+10, fill="white")
-			rectangles[i].append(rect)
-			grid[i].append(Cell(x, y, i, j))
-			x += 10
-		x = 10
-		y += 10
-
-def rectangle_coordinates(x, y):
-	return (x- x%10, y - y%10)
-
-def click_colour_change(event):
-	print (event.x, event.y)
-	x, y = rectangle_coordinates(event.x, event.y)
-	try:
-		iy = x / 10 - 1
-		ix = y / 10 - 1
-		if ix == -1 or iy == -1:
-			raise IndexError
-			if grid[ix][iy].isAlive:
-				canvas.itemconfig(rectangles[ix][iy], fill="black")
-			else:
-				canvas.itemconfig(rectangles[ix][iy], fill="white")
-				grid[ix][iy].switchStatus()
-				print (grid[ix][iy].pos_matrix, grid[ix][iy].pos_screen)
-	except IndexError:
-		return
-
-def paint_grid():
-	for i in grid:
-		for j in i:
-			if j.nextStatus != j.isAlive:
-				x, y = j.pos_matrix
-				print (x, y)
-				if j.nextStatus:
-					canvas.itemconfig(rectangles[x][y], fill="white")
-					print ("changed", j.pos_matrix, "from dead to alive")
-				else:
-					canvas.itemconfig(rectangles[x][y], fill="black")
-					print ("changed", j.pos_matrix, "from alive to dead")
-					j.switchStatus()
-					print ("Current status of", j.pos_matrix, j.isAlive)
-
-def status_update(cell):
-	num_alive = 0
-	x, y = cell.pos_matrix
-	for i in (x-1, x, x+1):
-		for j in (y-1, y, y+1):
-			if i == x and j == y:
-				continue
-				if i == -1 or j == -1:
-					continue
-					try:
-						if grid[i][j].isAlive:
-							num_alive += 1
-					except IndexError:
-						pass
-						if cell.isAlive:
-							return not( num_alive == 2 or num_alive == 3 )
-						else:
-							return num_alive == 3
-
-def start_game():
-	for i in grid:
-		for j in i:
-			if status_update(j):
-				j.nextStatus = not j.isAlive
-				print ("change in", j.pos_matrix, "from", j.isAlive, "to", j.nextStatus)
-			else:
-				j.nextStatus = j.isAlive
-				paint_grid()
-				global game_id
-				game_id = root.after(200, start_game)
-
-def end_game():
-	root.after_cancel(game_id)
+from random import randint
 
 
 class Cell:
-	def __init__(self, x, y, i, j):
-		self.isAlive = False
-		self.nextStatus = None
-		self.pos_screen = (x, y)
-		self.pos_matrix = (i, j)
+    def __init__(self):
+        """
+        Class holding init status of cell (dead).
+        Ability to set- and fetch new statuses with functions
+        """
+        self._status = 'Dead'
 
-	def __str__(self):
-		return str(self.isAlive)
+    def set_dead(self):
+        """
+        method sets the cell status to DEAD
+        """
+        self._status = 'Dead'
 
-	def __repr__(self):
-		return str(self.isAlive)
+    def set_alive(self):
+        """
+        method sets the cell status to ALIVE
+        """
+        self._status = 'Alive'
 
-	def switchStatus(self):
-		self.isAlive = not self.isAlive
+    def is_alive(self):
+        """
+        method checks if the cell is ALIVE
+        returns True if it is alive, False if not.
+        """
+        if self._status == 'Alive':
+            return True
+        return False
 
-create_grid()
-start = Button(root, text="Start!", command=start_game)
-start.pack(side = LEFT)
-stop = Button(root, text="Stop!", command = end_game)
-stop.pack(side = RIGHT)
-canvas.bind("<Button-1>", click_colour_change)
-root.mainloop()
+    def get_print_character(self):
+        """
+        method returning a status character of our choice to print on the board
+        """
+        if self.is_alive():
+            return 'O'
+        return '.'
+
+
+class Board:
+    def __init__(self, rows, columns):
+        """
+        constructor holds input from user and populates the grid with cells.
+        """
+        self._rows = rows
+        self._columns = columns
+        self._grid = [[Cell() for column_cells in range(self._columns)] for row_cells in range(self._rows)]
+
+        self._generate_board()
+
+    def draw_board(self):
+        """
+        method that draws the actual board in the terminal
+        """
+        print('\n' * 10)
+        print('printing board')
+        for row in self._grid:
+            for column in row:
+                print(column.get_print_character(), end='')
+            print()  # to create a new line pr. row.
+
+    def _generate_board(self):
+        """
+        method that sets the random state of all cells.
+        """
+
+        for row in self._grid:
+            for column in row:
+                # there is a 33% chance the cells spawn alive.
+                chance_number = randint(0, 2)
+                if chance_number == 1:
+                    column.set_alive()
+
+    def update_board(self):
+        """
+        method that updates the board based on
+        the check of each cell pr. generation
+        """
+        # cells list for living cells to kill and cells to resurrect or keep alive
+        goes_alive = []
+        gets_killed = []
+
+        for row in range(len(self._grid)):
+            for column in range(len(self._grid[row])):
+                # check neighbour pr. square:
+                check_neighbour = self.check_neighbour(row, column)
+
+                living_neighbours_count = []
+
+                for neighbour_cell in check_neighbour:
+                    # check live status for neighbour_cell:
+                    if neighbour_cell.is_alive():
+                        living_neighbours_count.append(neighbour_cell)
+
+                cell_object = self._grid[row][column]
+                status_main_cell = cell_object.is_alive()
+
+                # If the cell is alive, check the neighbour status.
+                if status_main_cell == True:
+                    if len(living_neighbours_count) < 2 or len(living_neighbours_count) > 3:
+                        gets_killed.append(cell_object)
+
+                    if len(living_neighbours_count) == 3 or len(living_neighbours_count) == 2:
+                        goes_alive.append(cell_object)
+
+                else:
+                    if len(living_neighbours_count) == 3:
+                        goes_alive.append(cell_object)
+
+        # sett cell statuses
+        for cell_items in goes_alive:
+            cell_items.set_alive()
+
+        for cell_items in gets_killed:
+            cell_items.set_dead()
+
+    def check_neighbour(self, check_row, check_column):
+        """
+        method that checks all the neighbours for all the cells
+        and returns the list of the valid neighbours so the update
+        method can set the new status
+        """
+        # how deep the search is:
+        search_min = -1
+        search_max = 2
+
+        # empty list to append neighbours into.
+        neighbour_list = []
+        for row in range(search_min, search_max):
+            for column in range(search_min, search_max):
+                neighbour_row = check_row + row
+                neighbour_column = check_column + column
+
+                valid_neighbour = True
+
+                if neighbour_row == check_row and neighbour_column == check_column:
+                    valid_neighbour = False
+
+                if neighbour_row < 0 or neighbour_row >= self._rows:
+                    valid_neighbour = False
+
+                if neighbour_column < 0 or neighbour_column >= self._columns:
+                    valid_neighbour = False
+
+                if valid_neighbour:
+                    neighbour_list.append(self._grid[neighbour_row][neighbour_column])
+        return neighbour_list
+
+
+def main():
+    # assume the user types in a number
+    user_rows = int(input('how many rows? '))
+    user_columns = int(input('how many columns? '))
+
+    # create a board:
+    game_of_life_board = Board(user_rows, user_columns)
+
+    # run the first iteration of the board:
+    game_of_life_board.draw_board()
+    # game_of_life_board.update_board()
+
+    user_action = ''
+    while user_action != 'q':
+        user_action = input('Press enter to add generation or q to quit:')
+
+        if user_action == '':
+            game_of_life_board.update_board()
+            game_of_life_board.draw_board()
+
+
+main()
