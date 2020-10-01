d0801b89e93b0d606ffbca1b841e8b77025f74f5
Atharv2 <48738128+Atharv2@users.noreply.github.com>
2020-10-01 13:29:58 -0700

    Added 99_cups.py

---
+++ archive/p/python/99_cups.py
@@ -0,0 +1,8 @@
+cups = 100
+
+while cups >0:
+    print(f"{cups} cups of coffee on the wall, {cups} cups of coffee on the wall")
+    cups -= 1
+    print(f"Take one down, pass it around, {cups} of coffee on the wall")
+
+print("No More cups of coffee!")
