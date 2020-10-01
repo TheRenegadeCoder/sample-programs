60786de8341f9585eb3db4e414938aaae4246d29
Ram Bharath <710019104035@annauniv.edu.in>
2020-10-01 16:57:06 +0530

    Updated capitalize.py in Python

--- archive/p/python/capitalize.py
+++ archive/p/python/capitalize.py
@@ -1,16 +1,9 @@
 import sys
 
-def capitalize(input):
-    ''' capitalize(self, /)
- |      Return a capitalized version of the string.
- |
- |      More specifically, make the first character have upper case and the rest lower
- |      case.'''
-    if len(input) > 0:
-        print(input.capitalize())
-
 if __name__ == '__main__':
     if(len(sys.argv) == 1 or len(sys.argv[1]) == 0):
         print('Usage: please provide a string')
     else:
-        capitalize(sys.argv[1])
+        input = sys.argv[1]
+        if len(input) > 0:
+            print(input.capitalize(sys.argv[1]))
