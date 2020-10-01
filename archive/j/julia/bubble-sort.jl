37b286c006362a181e60872df5172748d257434a
sniklas142 <niklas.schnierstein@googlemail.com>
2020-10-01 16:14:02 +0200

    Corrected error message

--- archive/j/julia/bubble-sort.jl
+++ archive/j/julia/bubble-sort.jl
@@ -17,7 +17,7 @@ function BubbleSort(arr)
 end
 
 function error()
-    println("Usage: please Usage: please provide a list of at least two integers to sort in the format “1, 2, 3, 4, 5”")
+    println("Usage: please provide a list of at least two integers to sort in the format “1, 2, 3, 4, 5”")
 end
 
 function HandleInput(inp)
