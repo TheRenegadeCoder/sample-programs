c514a9ef5849a0b2fa36abdf355dc0d032274d16
Ederson Ferreira <edersondeveloper@gmail.com>
2020-10-01 12:05:02 -0400

    adding checks to factorial

--- archive/r/ruby/factorial.rb
+++ archive/r/ruby/factorial.rb
@@ -1,10 +1,28 @@
 value = ARGV[0].to_i
 
-def fatorial(v)
-  if v == 1
-    return v
+def check_empty(n)
+  return n.to_s.empty?
+end
+
+def check_string(n)
+  return n.is_a? String
+end
+
+def check_negative(n)
+  return n.negative?
+end
+
+def fatorial(n)
+  if check_string(n) == false && check_negative(n) == false && check_empty(n) == false
+    if n == 1
+      return n
+    elsif n.zero?
+      return 1
+    else
+      return n * fatorial(n - 1)
+    end
   else
-    return v*fatorial(v - 1)
+    return 'The number is a string or negative'
   end
 end
 
