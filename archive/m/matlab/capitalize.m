3fcbcdb4303466071e744e40e4c8e1e92135b117
iwishiwasaneagle <jh.ewers@gmail.com>
2020-10-02 11:51:28 +0100

    Usage string on empty or missing input

--- archive/m/matlab/capitalize.m
+++ archive/m/matlab/capitalize.m
@@ -1,4 +1,12 @@
 function capString = capitalize(string)
-    string = char(string);
-    capString = convertCharsToStrings(strcat(upper(string(1:1)),string(2:end)));
+    if(nargin==0)
+        fprintf("Usage: please provide a string\n");
+    else
+        if(strlength(string)==0)
+            fprintf("Usage: please provide a string\n");
+        else
+            string = char(string);
+            capString = convertCharsToStrings(strcat(upper(string(1:1)),string(2:end)));
+        end
+    end
 end
