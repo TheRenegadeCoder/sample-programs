1712fc4612876492005efc980bf59a63f6b8c6f9
Jerin <jerinthomas1404@gmail.com>
2020-10-02 20:14:37 +0530

    Quine code in JAVA; self generating code

---
+++ archive/j/java/Quine.java
@@ -0,0 +1,6 @@
+public class Quine { 
+    public static void main(String[] args) { 
+        char c=34; System.out.println(s+c+s+c+';'+'}'); 
+    } 
+    static String s="public class Quine { public static void main(String[] args) { char c=34; System.out.println(s+c+s+c+';'+'}'); } static String s=";
+}
