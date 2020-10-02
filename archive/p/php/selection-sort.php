79ced057ad8ab166df7656610d6844497b66a7f9
Ayush Pandey <59275479+Ayush7-BIT@users.noreply.github.com>
2020-10-02 20:54:14 +0530

    Updated code for selection sort in php....

--- archive/p/php/selection-sort.php
+++ archive/p/php/selection-sort.php
@@ -1,33 +1,18 @@
-<?php
+function SelectionSort(&$data, $count) {
+	for ($i = 0; $i < $count - 1; $i++)
+	{
+		$min = $i;
 
-$numbers = array_map('intval', explode(',', $argv[1]));
-$array_size = count($numbers);
-if ($array_size <= 1)
-{
-    exit('Usage: please provide a list of at least two integers to sort in the format "1, 2, 3, 4, 5"');
-}
-
-
-function selection_sort($data)
-{
-for($i=0; $i<count($data)-1; $i++) {
-	$min = $i;
-	for($j=$i+1; $j<count($data); $j++) {
-		if ($data[$j]<$data[$min]) {
-			$min = $j;
+		for ($j = $i + 1; $j < $count; $j++)
+		{
+			if ($data[$j] < $data[$min])
+			{
+				$min = $j;
+			}
 		}
+
+		$temp = $data[$min];
+		$data[$min] = $data[$i];
+		$data[$i] = $temp;
 	}
-    $data = swap_positions($data, $i, $min);
 }
-return $data;
-}
-
-function swap_positions($data1, $left, $right) {
-	$backup = $data1[$right];
-	$data1[$right] = $data1[$left];
-	$data1[$left] = $backup;
-	return $data1;
-}
-
-echo implode(', ',selection_sort($numbers));
-?>
