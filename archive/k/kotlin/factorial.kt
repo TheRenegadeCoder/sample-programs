d7ea87bf0666a4aacb303a6ab9d4327743ab6425
Kush_Shah <35930270+shahkv95@users.noreply.github.com>
2020-10-03 03:33:49 +0530

    Added Factorial in Kotlin
    Added/Updated Factorial in Kotlin

---
+++ archive/k/kotlin/factorial.kt
@@ -0,0 +1,30 @@
+// ***********************************************************************************************************************************************************************
+// Method 1 
+// Using For Loop
+
+fun main(args: Array<String>) {
+
+    val number = args[0].toInt()
+    var factorial: Long = 1
+    for (i in 1..number) {
+        factorial *= i.toLong()
+    }
+    println("Factorial of $number = $factorial")
+}
+
+
+// ***********************************************************************************************************************************************************************
+// Method 2
+// Using While Loop
+
+fun main(args: Array<String>) {
+
+    val number = args[0].toInt()
+    var i = 1
+    var factorial: Long = 1
+    while (i <= number) {
+        factorial *= i.toLong()
+        i++
+    }
+    println("Factorial of $number = $factorial")
+}
