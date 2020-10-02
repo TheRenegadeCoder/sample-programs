e1883cfa3de55ed0739c823689afeb872d377ae9
Kush_Shah <35930270+shahkv95@users.noreply.github.com>
2020-10-03 03:45:48 +0530

    Added Factorial in Kotlin
    Added/Updated Factorial in Kotlin

--- archive/k/kotlin/factorial.kt
+++ archive/k/kotlin/factorial.kt
@@ -3,7 +3,8 @@
 // Using For Loop
 
 fun main(args: Array<String>) {
-
+    
+    // Taking the input from the user
     val number = args[0].toInt()
     var factorial: Long = 1
     for (i in 1..number) {
@@ -19,6 +20,7 @@ fun main(args: Array<String>) {
 
 fun main(args: Array<String>) {
 
+    // Taking the input from the user
     val number = args[0].toInt()
     var i = 1
     var factorial: Long = 1
