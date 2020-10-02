3e8c8bba90f32117bb87483f3f8d09889ce652b1
Ganesh Naik <naikganeshg2n@gmail.com>
2020-10-02 12:31:07 +0530

    Performance improvement

--- archive/j/javascript/prime-number.js
+++ archive/j/javascript/prime-number.js
@@ -1,17 +1,17 @@
 const isPrime = (number) => {
-  for (let i = 2; i < number; i++) {
-    if (number % i == 0) {
-      return false;
+    for (let i = 2; i <= Math.ceil(Math.sqrt(number)); i++) {
+      if (number % i == 0) {
+        return false;
+      }
     }
+    return number > 1;
+  };
+  
+  const input = process.argv[2];
+  let number = Number(input)
+  
+  if (input !== '' && Number.isInteger(number) && number >= 0) {
+    isPrime(input) ? console.log("prime") : console.log("composite");
+  } else {
+    console.log("Usage: please input a non-negative integer")
   }
-  return number > 1;
-};
-
-const input = process.argv[2];
-let number = Number(input)
-
-if (input !== '' && Number.isInteger(number) && number >= 0) {
-  isPrime(input) ? console.log("prime") : console.log("composite");
-} else {
-  console.log("Usage: please input a non-negative integer")
-}
