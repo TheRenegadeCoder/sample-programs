b047fb104d7313f54c94ca7481ca09d89de50392
uditpatel2526 <56185794+uditpatel2526@users.noreply.github.com>
2020-10-03 22:57:41 +0530

    Create factorial.scala

---
+++ archive/s/scala/factorial.scala
@@ -0,0 +1,23 @@
+// Scala Program to calculate Factorial 
+// of a number using recursion 
+
+// Creating object 
+object compile
+{ 
+	// Function to calculate 
+	// factorial using Recursive 
+	// formula (i.e N! = N * N-1 !) 
+	def factorial(n: Int): Int =
+	{ 
+		if (n == 0) 
+			return 1
+		else
+			return n * factorial(n-1) 
+	} 
+
+	// Driver Code 
+	def main(args: Array[String]) 
+	{ 
+		println(factorial(5)) 
+	} 
+} 
