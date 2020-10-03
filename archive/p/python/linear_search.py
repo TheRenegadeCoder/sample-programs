c96f211a0bb1b3d3fec5c640d3b1f6a11c4b230a
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-10-02 20:31:23 -0400

    Rewrote program to properly return booleans

--- archive/p/python/linear_search.py
+++ archive/p/python/linear_search.py
@@ -3,7 +3,13 @@ import sys
 def sysarg_to_list(string):
     return [int(x.strip(" "), 10) for x in string.split(',')]
 
-if(len(sys.argv) != 2):
+def linear_search(array, key):
+    for item in array:
+        if item == key:
+            return True
+    return False
+
+if len(sys.argv) != 2 or not sys.argv[1]:
     print('Usage: please provide a list of sorted integers ("1, 4, 5, 11, 12") and the integer to find ("11")')
     sys.exit()
 
@@ -11,13 +17,4 @@ key = int(sys.argv[2])
 array = sysarg_to_list(sys.argv[1])
 size = len(array)
 
-flag = 0
-pos = -1
-
-for i in range(size):
-    if array[i] == key:
-        flag = 1
-        pos = i
-        break
-
-print(flag)
+print(linear_search(array, key))
