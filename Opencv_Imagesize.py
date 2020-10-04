b59e6ce2df8cd6255838b44b3243155583418067
Anshul Choudhary <34908313+achoudh5@users.noreply.github.com>
2020-10-03 21:32:47 -0400

    Update Opencv_Imagesize.py

--- Opencv_Imagesize.py
+++ Opencv_Imagesize.py
@@ -1,11 +1,10 @@
-<!--Using opencv to determine the size of an image-->
+#Using opencv to determine the size of an image
+#Run: python Opencv_Imagesize.py
 import imutils
 import cv2
 
-image=cv2.imread("/Users/anshul.choudhary@ibm.com/PycharmProjects/opencv/sample.png")
+image=cv2.imread("<dir_path>/<image_name>")
 (h,w,d) = image.shape
 print ("width={},height={},depth={}".format(w,h,d))
-
-
 cv2.imshow("Image",image)
 cv2.waitKey(0)
