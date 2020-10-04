ead40cd7086e2fe56895597bae690493e7042892
Karl Marx <51373628+karlmarx@users.noreply.github.com>
2020-10-04 16:29:52 -0400

    Create Quine.java

---
+++ archive/j/java/Quine.java
@@ -0,0 +1,1 @@
+public class Quine{public static void main(String[] args){char quote=34;String code="public class Quine{public static void main(String[] args){char quote=34;String code=;System.out.println(code.substring(0,84)+quote+code+quote+code.substring(84));}}";System.out.println(code.substring(0,84)+quote+code+quote+code.substring(84));}}
