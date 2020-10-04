14bea3a201afd48a75bb69117af7f88826e9e063
Allan Klaus <allan_klaus@globo.com>
2020-10-04 12:50:04 -0300

    Transform roman number to decimal in ruby

---
+++ archive/r/ruby/roman-numeral.rb
@@ -0,0 +1,43 @@
+ROMAN_VALUES = {
+  "I": 1,
+  "V": 5,
+  "X": 10,
+  "L": 50,
+  "C": 100,
+  "D": 500,
+  "M": 1000
+}
+
+def roman_valid?(roman_numbers)
+  return false if roman_numbers.any? { |roman_number| !ROMAN_VALUES.keys.include?(roman_number.to_sym) }  
+  return false if roman_numbers.join.include?('MMMM')
+  
+  counter_numbers = roman_numbers.tally # Only on ruby 2.7+
+  unless counter_numbers['M'].nil?
+    return false if counter_numbers['M'] > 4
+  end
+  
+  counter_numbers.reject { |k| k == 'M' }.all? { |(_, counter)| counter <= 3 }
+end
+
+def roman_to_decimal(full_roman_number)
+  return 0 if full_roman_number.empty?
+
+  roman_numbers = full_roman_number.upcase.split('')
+  raise 'Invalid Roman Number' unless roman_valid?(roman_numbers)
+
+  previous = 0
+  total = 0
+
+  roman_numbers.each do |roman_number|
+    value = ROMAN_VALUES[roman_number.to_sym]
+
+    total += value if (value >= previous)
+    total -= value unless (value >= previous)
+    previous = value
+  end
+
+  p total
+end
+
+roman_to_decimal(ARGV[0])
