82f4d1b2ef8e3224e68ec0e589e776315761a4f6
Jeremy Grifski <jeremy.grifski@gmail.com>
2020-10-05 23:46:18 -0400

    Solution now prints list directly

--- archive/p/python/sleep_sort.py
+++ archive/p/python/sleep_sort.py
@@ -7,23 +7,25 @@ def arg_to_list(string):
     return [int(x.strip(" "), 10) for x in string.split(',')]
 
 
-def sleep_sort(i):
+def sleep_sort(i, output):
     sleep(i)
-    print(i)
+    output.append(i)
 
 
 def main():
     array = arg_to_list(sys.argv[1])
     
     threads = []
+    output = []
     for i in array:
-        arg_tuple = (i,)
+        arg_tuple = (i, output)
         thread = threading.Thread(target=sleep_sort, args=arg_tuple)
         thread.start()
         threads.append(thread)
         
     for thread in threads:
         thread.join()
-
         
+    print(output)
+
 main()
