2d61bd3ff604d91732dea435090a7e7733fe16e8
Blake.Ke <smallblack9@gmail.com>
2020-10-08 22:09:00 +0100

    Add testinfo.yml and add validations for input arguments to Capitalize.kt to make sure it handles some invalid cases

--- archive/k/kotlin/Capitalize.kt
+++ archive/k/kotlin/Capitalize.kt
@@ -1,11 +1,9 @@
-fun main(args: Array<String>){
-        // Get input, or use default value
-        val targetValue = when (args.isNotEmpty() && !args[0].isBlank()) {
-            true -> args[0]
-            false -> throw Error("No String Provided. Nothing to Capitalize")
-        }
-
+fun main(args: Array<String>) {
+    if (args.isNullOrEmpty() || args[0].isBlank()) {
+        println("Usage: please provide a string")
+    } else {
         // Kotlin provides a simple `capitalize()` function in the standard
         // library for all String objects
-        println(targetValue.capitalize())
-     }
+        println(args[0].capitalize())
+    }
+}
