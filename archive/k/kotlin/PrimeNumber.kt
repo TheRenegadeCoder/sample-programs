6528ed90622a464a09dc3389c483307e13f3a96e
Blake.Ke <smallblack9@gmail.com>
2020-10-09 22:55:30 +0100

    Update PrimeNumber.kt - take input argument and add validations

--- archive/k/kotlin/PrimeNumber.kt
+++ archive/k/kotlin/PrimeNumber.kt
@@ -1,10 +1,11 @@
 fun main(args: Array<String>) 
 {
-    var num = readLine()!!.toIntOrNull()
-    if(num == null){
-        print("Usage: please input a non-negative integer")
+    if (args.isNullOrEmpty() || args[0].isBlank() || args[0].toIntOrNull()?.takeIf { it >= 0 } == null) {
+        println("Usage: please input a non-negative integer")
         return
     }
+
+    val num = args[0].toInt()
     if(num>1)
     {
         for(i in 2 until num)
