823eda8f0191d62a2450d9159f396ee2eaa6d88f
Kush_Shah <35930270+shahkv95@users.noreply.github.com>
2020-10-09 21:59:38 +0530

    Added factorial in Kotlin
    Commented the second method keeping the first function as the only method that could be directly called from the command line. This will help to remove the discrepancy caused by having two different methods in a single file.

--- archive/k/kotlin/factorial.kt
+++ archive/k/kotlin/factorial.kt
@@ -27,20 +27,24 @@ fun main(args: Array<String>) {
 // Method 2
 // Using While Loop
 
-fun main(args: Array<String>) {
+// fun main(args: Array<String>) {
 
-    // Taking the input from the user
-    val number = args[0].toInt()
-    var i = 1
-    var factorial: Long = 1
-    while (i <= number) {
-        if (i == 0){
-        	factorial = 1}
-       else{
-        factorial *= i.toLong()
-       }
+//     // Taking the input from the user
+//     val number = args[0].toInt()
+//     var i = 1
+//     var factorial: Long = 1
+//     while (i <= number) {
+//         if (i == 0){
+//         	factorial = 1}
+//        else{
+//         factorial *= i.toLong()
+//        }
         
-        i++
-    }
-    println("Factorial of $number is $factorial")
-}
+//         i++
+//     }
+//     println("Factorial of $number is $factorial")
+// }
+
+
+
+// The second method is commented so that the first method can be run directly from the command line by just passing a number as an input
