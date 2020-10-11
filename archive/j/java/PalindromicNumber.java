c48083441a98f5baadf9fce3f5f8ce2db29d1408
Blake.Ke <smallblack9@gmail.com>
2020-10-11 14:14:11 +0100

    Add PalindromicNumber.java and pass the test

---
+++ archive/j/java/PalindromicNumber.java
@@ -0,0 +1,24 @@
+public class PalindromicNumber {
+
+    public static void main(String[] args) {
+        Long num = null;
+        try {
+            num = Long.parseLong(args[0]);
+        } catch (Exception ignored) {
+        }
+        if (num == null || num.toString().length() < 2) {
+            System.out.println("Usage: please input a number with at least two digits");
+        } else {
+            System.out.println(isPalindromic(num));
+        }
+    }
+
+    public static boolean isPalindromic(Long num) {
+        char[] numChars = String.valueOf(num).toCharArray();
+        for (int i = 0; i < numChars.length / 2; i++) {
+            if (numChars[i] != numChars[numChars.length - 1 - i])
+                return false;
+        }
+        return true;
+    }
+}
