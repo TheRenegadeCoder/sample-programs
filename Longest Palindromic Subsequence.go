06523db69d4af5cd922616c7057d67dee12ac7e7
adityasingh22 <31698903+adityasingh22@users.noreply.github.com>
2020-10-14 17:05:35 +0530

    Updated Longest Palidromic substring in Golang

--- Longest Palindromic Subsequence.go
+++ Longest Palindromic Subsequence.go
@@ -1,26 +1,136 @@
-# Longest Palindromic Subsequence
+// Longest Palindromic Subsequence
+// DP solution using O(n^2) time complexity and O(n^2) space complexity
 
-func longestPalindromeSubseq(s string) int {
-    mem := make([][]int, len(s))
-    for i := range mem {
-        mem[i] = make([]int, len(s))
-    }
-    return helper(0, len(s) - 1, &s, &mem)
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func longestPalindrome(s string) string {
+	if len(s) == 0 {
+		return "Incorrect input provided. Program Terminated"
+	}
+
+	start, n, maxLen := 0, len(s), 0
+	d := make([][]bool, n)
+	for i := 0; i < n; i++ {
+		d[i] = make([]bool, n)
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		for j := i; j < n; j++ {
+			if i == j {
+				d[i][j] = truepackage main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func longestPalindrome(s string) string {
+	if len(s) == 0 {
+		return "Incorrect input provided. Program Terminated"
+	}
+
+	start, n, maxLen := 0, len(s), 0
+	d := make([][]bool, n)
+	for i := 0; i < n; i++ {
+		d[i] = make([]bool, n)
+	}
+
+	for i := n - 1; i >= 0; i-- {
+		for j := i; j < n; j++ {
+			if i == j {
+				d[i][j] = true
+			} else if i+1 == j {
+				d[i][j] = s[i] == s[j]
+			} else {
+				d[i][j] = s[i] == s[j] && d[i+1][j-1]
+			}
+
+			if d[i][j] && (j-i+1) > maxLen {
+				start = i
+				maxLen = j - i + 1
+			}
+		}
+	}
+
+	return s[start : start+maxLen]
+
+
 }
 
-func helper(l, r int, s *string, mem *[][]int) int {
-    if l == r { return 1 }
-    if l > r { return 0 }
-    if (*mem)[l][r] != 0 { return (*mem)[l][r] }
-    if (*s)[l] == (*s)[r] {
-        (*mem)[l][r] = 2 + helper(l+1, r-1, s, mem)
-    } else {
-        (*mem)[l][r] = max(helper(l+1, r, s, mem), helper(l, r-1, s, mem))
-    }
-    return (*mem)[l][r]
+func IsUpper(s string) bool {
+	for _, r := range s {
+		if !unicode.IsUpper(r) && unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
 }
 
-func max(a, b int) int {
-    if a > b { return a }
-    return b
+func IsLower(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLower(r) && unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	s:="cbbd"
+	if IsUpper(s) || IsLower(s){
+		fmt.Println(longestPalindrome(s))
+	}else{
+		fmt.Println("No Palindromic substring present.")
+	}
+	
+}
+			} else if i+1 == j {
+				d[i][j] = s[i] == s[j]
+			} else {
+				d[i][j] = s[i] == s[j] && d[i+1][j-1]
+			}
+
+			if d[i][j] && (j-i+1) > maxLen {
+				start = i
+				maxLen = j - i + 1
+			}
+		}
+	}
+
+	return s[start : start+maxLen]
+
+
+}
+
+func IsUpper(s string) bool {
+	for _, r := range s {
+		if !unicode.IsUpper(r) && unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func IsLower(s string) bool {
+	for _, r := range s {
+		if !unicode.IsLower(r) && unicode.IsLetter(r) {
+			return false
+		}
+	}
+	return true
+}
+
+func main() {
+	s:="cbbd"
+	if IsUpper(s) || IsLower(s){
+		fmt.Println(longestPalindrome(s))
+	}else{
+		fmt.Println("No Palindromic substring present.")
+	}
+	
 }
