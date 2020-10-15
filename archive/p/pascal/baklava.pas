950edc05be4c4a0f487fd5ab58c5b594030ac3cd
smjalageri <70333834+smjalageri@users.noreply.github.com>
2020-10-15 20:01:28 +0530

    Update baklava.pas

--- archive/p/pascal/baklava.pas
+++ archive/p/pascal/baklava.pas
@@ -1,43 +1,42 @@
 program baklava;
-
+(* Print 10 lines of Sweet Baklava
+   Note: Pascal Loops are inclusive of ends i.e., 
+   for i:= 0 to 10 will run 11 times .*)
 var 
 i, j, spacecount, starcount : integer;
 begin
-	(* Top Part *)
+(* Top Part *)
 for i:= 0 to 10 do
 	begin
-			    spacecount := 10 - i;
-				starcount := i*2 ;
-				for j:= spacecount downto 1 do
-				begin
-					write(' ');
-
-				end;
-
-				for j:=0 to starcount do
-				begin
-				write('*');	
-
-				end; 
-		writeln();
-	end;
-
-(*Bottom Part*)
-for i:= 10 downto 0 do
-	begin
-	    spacecount := 10 - i;
-		starcount := i * 2;
-		for j:= spacecount downto 1 do
+		spacecount := 10 - i;
+		starcount := i*2;
+    (* Print Spaces*)
+		for j:= spacecount downto 0 do
 		begin
 			write(' ');
 		end;
-
-
+    (*Print Stars*)
 		for j:=0 to starcount do
 		begin
-		write('*');		
+		  write('*');	
 		end; 
-				writeln();
+		writeln();
 	end;
-
-	end.
+(*Bottom Part*)
+for i:= 9 downto 0 do
+	begin
+	    spacecount := 10 - i;
+		starcount := i * 2;
+    (* Print Spaces*)
+		for j:= spacecount downto 0 do
+		begin
+			write(' ');
+		end;
+    (* Print Stars*)
+		for j:=0 to starcount do
+		begin
+		  write('*');		
+		end; 
+		writeln();
+	end;
+end.
