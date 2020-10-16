5fb35a43069d9eba9b8b5fcdb4a65c3788255c2f
smjalageri <70333834+smjalageri@users.noreply.github.com>
2020-10-16 18:55:00 +0530

    Updated Even Odd
    This is running fine on local machine for 
    
    no input	None	Usage: please input a number
    empty input	””	Usage: please input a number
    invalid input: not a number	a	Usage: please input a number
    sample input: even	2	Even
    sample input: odd	5	Odd
    sample input: negative even	-14	Even
    sample input: negative odd	-27	Odd
    
    Surprisingly, it is failing for 
    
    +ve Odd
    -ve Odd
    +ve Even
    -ve Even  here, ... 
    
    Issue as in travis build was numerals are seen by this pascal script as strings.....  
    
    this doesn't happen on local machine, ... :)

--- archive/p/pascal/even_odd.pas
+++ archive/p/pascal/even_odd.pas
@@ -1,34 +1,28 @@
 program even_odd;
-
 (*
-Accept number, If not number, Empty, character or no input, Display Usage: please input a numberif number, % divide by 2 Print either Even or Odd
-Learnings 9:21 PM Oct 10
-equals to Operator is =
-Strings inside write must be in single quotes
-last line of the program must be end.*)
+Accept number, If not number, Empty, character or no input, Display Usage: please input a number
+if number is % divide by 2 Print Even 
+If number is not % by 2, print Odd
+*)
 var
 { local variable declaration }
  buf: String;
  a, check:integer;
 begin    
-
     readln(buf);
     Val(buf, a, check) ;
     if check <> 0
     then
     writeln('Usage: please input a number')
-
     else
     begin
-
         if( (a mod 2) = 0) then
             begin
-            writeln('Even'); 
+            write('Even'); 
             end         
         else
             begin
-            writeln('Odd'); 
+            write('Odd'); 
             end
     end
-
 end.
