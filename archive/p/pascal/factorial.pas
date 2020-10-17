a645fb0884afb8ee78c712187912cbbd68b05a4f
smjalageri <70333834+smjalageri@users.noreply.github.com>
2020-10-17 12:15:18 +0530

    read number from command line print it's factorial
    read number from command line print it's factorial

--- archive/p/pascal/factorial.pas
+++ archive/p/pascal/factorial.pas
@@ -1,38 +1,29 @@
-program a1;
-
+(* Accept number from command line, print it's Factorial*)
+program factorial_compute(input, output, stdErr);
 var
-{ local variable declaration }
- buf: String;
-(* number, factorial, check:Cardinal;*)
-number, check: integer;
-factorial: Cardinal;
+  buf: String;
+  i, number, check_error: integer;
+  factorial: Cardinal;
 begin    
-
-    readln(buf);
-    Val(buf, number, check) ;
-    if check <> 0
-    then
-    writeln('Usage: please input a number')
-
-    else
-            if number < 0
-            then
-            writeln('Usage: please input a non-negative integer')
-
-            else
-
-            if number > 33
-                        then
-            writeln(number, ' is out of the reasonable bounds for calculation.')
-
-            else
-            begin
-                    factorial :=1;
-                    for check:= 1 to number do
-                    factorial:= factorial * check;
-                                writeln(factorial);
-
-
-            end
-
+  buf:= paramStr(1);
+  Val(buf, number, check_error);  
+  if check_error <> 0 (* Non Integer*)
+  then
+    writeln('Usage: please input a non-negative integer')
+  else
+  if number < 0 (* Negative Number*) then
+    writeln('Usage: please input a non-negative integer')
+  else
+  begin
+    if number > 33 (*exceeds unsigned integer limit in Pascal*)
+  then
+    writeln(number, ' is out of the reasonable bounds for calculation.')
+  else
+  begin
+    factorial :=1;
+    for i:= 1 to number do
+    factorial:= factorial * i;
+    writeln(factorial);
+  end;
+  end;
 end.
