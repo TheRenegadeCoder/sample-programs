9d876d9cdcde00537646447fc290db5c51c2e571
smjalageri <70333834+smjalageri@users.noreply.github.com>
2020-10-17 12:58:37 +0530

    Change Even Odd to read from commandline
    Change Even Odd to read from commandline

--- archive/p/pascal/even_odd.pas
+++ archive/p/pascal/even_odd.pas
@@ -1,15 +1,10 @@
-program even_odd;
-(*
-Accept number, If not number, Empty, character or no input, Display Usage: please input a number
-if number is % divide by 2 Print Even 
-If number is not % by 2, print Odd
-*)
+program a1(input, output, stdErr);
+(* Read Number from commandline, print  Even or Odd *)
 var
-{ local variable declaration }
  buf: String;
  a, check:integer;
 begin    
-    readln(buf);
+  buf:= paramStr(1);
     Val(buf, a, check) ;
     if check <> 0
     then
@@ -18,11 +13,11 @@ begin
     begin
         if( (a mod 2) = 0) then
             begin
-            write('Even'); 
+            writeln('Even'); 
             end         
         else
             begin
-            write('Odd'); 
+            writeln('Odd'); 
             end
     end
 end.
