07fcc13cb483a211234a2eb13101d40936610a1b
RishavHimmatramka <46376699+RishavHimmatramka@users.noreply.github.com>
2020-10-21 10:14:05 +0545

    Update no-of-digits.py

--- no-of-digits.py
+++ no-of-digits.py
@@ -1,3 +1,11 @@
+# This program can count the no of digits in a number or a string as well.
+#Example1:
+#Input: Hello
+#Output: The number of digits are: 5
+#Example2:
+#Input: 123784
+#Output: The number of digits are: 6
+
 n=input("Enter a number or a string")
 digit=len(n)
 print("The number of digits are: ",digit)
