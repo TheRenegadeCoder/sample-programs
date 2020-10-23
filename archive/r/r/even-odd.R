a4787bad7929b993d5c407b9dbe0dcd7563a95cf
manasmithamn <73154097+manasmithamn@users.noreply.github.com>
2020-10-23 09:31:55 +0530

    Change as.integer to as.numeric -Check for numbers
    Change as.integer to as.numeric -Check for numbers

--- archive/r/r/even-odd.R
+++ archive/r/r/even-odd.R
@@ -1,8 +1,9 @@
 args<-commandArgs(TRUE)
 if(length(args) > 0){
   a1 = args[1]
-  a = as.integer(a1)
-  if(!(is.na(a) )){
+  # as.integer or as.numeric both..
+  a = as.numeric(a1)
+  if(!(is.na(a))){
     if(a %% 2 == 0){
       cat("Even")
     }else{
@@ -11,7 +12,6 @@ if(length(args) > 0){
   }else{
   cat("Usage: please input a number")
 }
-
 } else{
   cat("Usage: please input a number")
 }
