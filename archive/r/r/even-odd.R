0844487cfa19cbd77e70892fe6d81b6ae91e46d3
manasmithamn <73154097+manasmithamn@users.noreply.github.com>
2020-10-24 06:06:45 +0530

    Accept input Check & Print If it's Odd or Even
    Accept input Check & Print If it's Odd or Even
    Using is.na instead of is.null, to avoid NA Coercion Warning

--- archive/r/r/even-odd.R
+++ archive/r/r/even-odd.R
@@ -1,11 +1,11 @@
-# Adding suppresswarning, based on assumption that this is leading to 
-# Errors
+# Accept input from Commandline, Check & Print If it's Odd or Even
+# Using is.na instead of is.null, to avoid NA Coercion Warning
 args<-commandArgs(TRUE)
 if(length(args) > 0){
   a1 = args[1]
   # as.integer or as.numeric both..
   a = as.numeric(a1)
-  if(!(is.na(a))){
+  if (is.na(a) != 0){
     if(a %% 2 == 0){
       cat("Even")
     }else{
