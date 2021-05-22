e77355840fd446d5bf13398b7471775785e4abad
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-05-22 14:32:54 -0400

    Update prime-number.sh

--- archive/b/bash/prime-number.sh
+++ archive/b/bash/prime-number.sh
@@ -7,7 +7,7 @@ function is_prime(){
     if [[ $1 -eq 2 ]] || [[ $1 -eq 3 ]]; then
         return 1  # prime
     fi
-    if [[ $(($1 % 2)) -eq 0 ]] || [[ $(($1 % 3)) -eq 0 ]]; then
+    if [[ $(($1 % 2)) -eq 0 ]] || [[ $(($1 % 3)) -eq 0 ]] || [[ $1 -eq 1 ]]; then
         return 0  # not a prime
     fi
     i=5; w=2
