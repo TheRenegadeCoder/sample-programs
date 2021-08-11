fced241dbb0ef9761f4c0c53c73968cf715b19a1
Jeremy Grifski <jeremy.grifski@gmail.com>
2021-08-10 20:06:41 -0400

    Added Scheme Tests (#2427)
    * Create testinfo.yml
    
    * Generated READMEs from sources using Ronbun
    
    on-behalf-of: @TheRenegadeCoder <jeremy.grifski@therenegadecoder.com>
    
    * Changed reverse string implementation
    
    I managed to get this to work in Jdoodle, but I have no clue if it works in Gambit. Also, I didn't check for length because I wasn't trying to relearn lisp/scheme.
    
    * Trying a different scheme
    
    * Generated READMEs from sources using Ronbun
    
    on-behalf-of: @TheRenegadeCoder <jeremy.grifski@therenegadecoder.com>
    
    * Delete fibonacci.scm
    
    * Generated READMEs from sources using Ronbun
    
    on-behalf-of: @TheRenegadeCoder <jeremy.grifski@therenegadecoder.com>

--- archive/s/scheme/fibonacci.scm
+++
@@ -1,9 +0,0 @@
-(define (fibonacci n)
-  (cond ((number? n)
-         (cond ((= n 0) "")
-               ((= n 1) 1)
-               ((= n 2) 1)
-               (else (+ (fibonacci (- n 1)) (fibonacci (- n 2))))))
-        (else (begin (display "Usage: please input the count of fibonacci numbers to output")))))
-
-(display (fibonacci (read)))
