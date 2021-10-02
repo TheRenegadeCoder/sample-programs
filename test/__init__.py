51560ec491b7b289fcd868cda9cebcd941eeeea0
sudoKIngpin <wd51886@gmail.com>
2021-10-02 23:46:32 +0530

    Palindrome function added to check string

---
+++ test/__init__.py
@@ -0,0 +1,1 @@
+print("This is a package file")
---
+++ test/projects/test_palindrome.py
@@ -0,0 +1,6 @@
+def pal(word):
+    w2=word[::-1]
+    if word==w2:
+        print("Its a palindrome")
+    else:
+        print('Its not as palindrome')
