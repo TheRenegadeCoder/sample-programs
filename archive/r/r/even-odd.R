7863c394043964e197ac41a384162b7c97e81f72
manasmithamn <73154097+manasmithamn@users.noreply.github.com>
2021-10-02 15:23:55 +0530

    Check if a number is even or odd
    I submitted this last year, but got rejected, trying again with modifications, .... Checking if this will pass thru Glotter without NA Coercion Error

---
+++ archive/r/r/even-odd.R
@@ -0,0 +1,21 @@
+# Accept input from Commandline, Check & Print If it's Odd or Even
+# Using is.na instead of is.null, to avoid NA Coercion Warning
+
+args<-commandArgs(TRUE)
+if(length(args) > 0){
+  a1 = args[1]
+  # as.integer or as.numeric both..
+  a = as.numeric(a1)
+  # Suppress Warnings can't be put inside 
+  if (is.na(a) == 0){
+    if(a %% 2 == 0){
+      cat("Even")
+    }else{
+      cat("Odd")
+    }
+  }else{
+  cat("Usage: please input a number")
+}
+} else{
+  cat("Usage: please input a number")
+}
