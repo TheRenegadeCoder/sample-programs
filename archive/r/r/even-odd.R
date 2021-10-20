49af776f34ea3314ecff60ccec7ab7c84f5349ca
GroobleDierne <grooble.dierne@gmail.com>
2021-10-20 17:18:02 +0200

    Fixed input checking

--- archive/r/r/even-odd.R
+++ archive/r/r/even-odd.R
@@ -1,8 +1,11 @@
 args<-commandArgs(TRUE)
 number = args[1]
-if (!is.numeric(number)) {
+numbers_only <- function(a) !grepl("\\D", number)
+
+if (length(args) < 1 || numbers_only(number) == FALSE) {
   cat("Usage: please input a number")
 }else {
+  number = as.numeric(number)
   if (number %% 2 == 0) {
     cat("Even")
   }else {
