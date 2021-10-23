edb69ef6159d9a99df8629f6716115e496655d75
manasmithamn <73154097+manasmithamn@users.noreply.github.com>
2021-10-24 01:16:50 +0530

    Updated Fibonacci Series
    Updated Fibonacci Series, print index and the Fibonacci Number

--- archive/p/pascal/fibonacci.pas
+++ archive/p/pascal/fibonacci.pas
@@ -3,32 +3,32 @@ program Fibonacci(input, output, stdErr);
 
 *)
 var
-   buf: String;
-   count, check, first,second,result, i : integer;
+  buf: String;
+  count, check, first,second,result, i : integer;
 
 begin
-   (*Variable initialisation must be inside begin-end block*)
-   first := 0;
-   second := 1;
-   result := 0;
+  (*Variable initialisation must be inside begin-end block*)
+  first := 0;
+  second := 1;
+  result := 0;
 
-  begin    
-    buf:= paramStr(1);
-    Val(buf, count, check);  
-    (*If input is valid integer, check will be 0, else will be 1*)
-    if (check <> 0)
-    then
+  begin   
+   buf:= paramStr(1);
+   Val(buf, count, check);  
+   (*If input is valid integer, check will be 0, else will be 1*)
+   if (check <> 0)
+   then
+   begin
+    writeln('Usage: please input the count of fibonacci numbers to output');
+   end
+   else
+    for i := 1 to count do
     begin
-      writeln('Usage: please input the count of fibonacci numbers to output');
-    end
-    else
-      for i := 1 to count do
-      begin
-      
-      result := first + second;
-      first := second;
-      second := result;
-      writeln(first);
-      end;
+    
+    result := first + second;
+    first := second;
+    second := result;
+    writeln(i, ': ', first);
     end;
+   end;
 end.
