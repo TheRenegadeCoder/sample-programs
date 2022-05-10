44c524a0e9f3426b4372c15ae0aadd94e6e947ae
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-05-09 20:52:34 -0400

    Fixed up bugs in selection sort

--- archive/p/php/selection-sort.php
+++ archive/p/php/selection-sort.php
@@ -7,32 +7,31 @@ if (empty($argv[1])) {
 $numbers = array_map('intval', explode(',', $argv[1]));
 $array_size = count($numbers);
 
-if ($array_size <= 1)
-{
-    echo('Usage: please provide a list of at least two integers to sort in the format "1, 2, 3, 4, 5"\n')
+if ($array_size <= 1) {
+    echo ('Usage: please provide a list of at least two integers to sort in the format "1, 2, 3, 4, 5"\n');
     exit(1);
 }
 
 function selection_sort($data)
 {
-	for($i=0; $i<count($data)-1; $i++) {
-		$min = $i;
-		for($j=$i+1; $j<count($data); $j++) {
-			if ($data[$j]<$data[$min]) {
-				$min = $j;
-			}
-		}
-		$data = swap_positions($data, $i, $min);
-	}
-return $data;
+    for ($i = 0; $i < count($data) - 1; $i++) {
+        $min = $i;
+        for ($j = $i + 1; $j < count($data); $j++) {
+            if ($data[$j] < $data[$min]) {
+                $min = $j;
+            }
+        }
+        $data = swap_positions($data, $i, $min);
+    }
+    return $data;
 }
 
-function swap_positions($data1, $left, $right) {
-	$backup = $data1[$right];
-	$data1[$right] = $data1[$left];
-	$data1[$left] = $backup;
-	return $data1;
+function swap_positions($data1, $left, $right)
+{
+    $backup = $data1[$right];
+    $data1[$right] = $data1[$left];
+    $data1[$left] = $backup;
+    return $data1;
 }
 
-echo implode(', ',selection_sort($numbers));
-
+echo implode(', ', selection_sort($numbers));
