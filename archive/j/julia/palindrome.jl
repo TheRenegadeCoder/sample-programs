7bf44f1f75d91677a7f9be2fbdeae80593106d3f
smjalageri <70333834+smjalageri@users.noreply.github.com>
2022-10-05 08:36:23 +0530

    Check Palindromic Number

---
+++ archive/j/julia/palindrome.jl
@@ -0,0 +1,26 @@
+#!/usr/bin/julia
+function err() 
+  println("Usage: please input a number")
+end
+
+function palindrome_check(n)
+  new_num = 0
+  original = n
+  while (n > 0)
+    digit = n % 10
+    new_num = new_num * 10 + digit 
+    n ÷= 10
+  end
+
+  if(new_num == original)
+      return "true"
+  else
+      return "false"
+  end
+end
+
+try
+   println(palindrome_check(parse(Int, ARGS[1])))
+catch e
+   err()
+end
