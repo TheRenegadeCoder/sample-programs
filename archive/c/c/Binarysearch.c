02ade12ce8c1e0bd781befe94a45d3a410224dca
Deepak Rana <dr3734195@gmail.com>
2022-10-08 00:13:56 +0530

    added binary search program

---
+++ archive/c/c/Binarysearch.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+//binary search program
+
+int main()
+{
+int i, low, high, mid, n, key, array[100];
+
+
+printf("Enter number of elements \n");
+scanf("%d",&n);
+
+printf("Enter %d integers \n", n);
+for(i = 0; i < n; i++)
+scanf("%d",&array[i]);
+
+
+//select you desired element
+
+printf("Enter value to findn");
+scanf("%d", &key);
+
+low = 0;
+
+high = n - 1;
+
+mid = (low+high)/2;
+
+while (low <= high)
+ {
+
+
+   if(array[mid] < key)
+  low = mid + 1;
+   else if (array[mid] == key) {
+printf("%d found at location %d.n", key, mid+1);
+break;
+}
+else
+high = mid - 1;
+mid = (low + high)/2;
+}
+if(low > high)
+printf("Not found! %d isn't present in the list.n", key);
+return 0;
+}
