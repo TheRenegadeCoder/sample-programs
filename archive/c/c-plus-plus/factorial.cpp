c54c6d8417a9fbe4076ca5971bae8d9bb1344643
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-10-10 15:23:31 -0400

    Working through c++ code

--- archive/c/c-plus-plus/factorial.cpp
+++ archive/c/c-plus-plus/factorial.cpp
@@ -4,28 +4,28 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-  if (argc < 2 || std::string(argv[1]) == "")
-  {
-    cout << "Usage: please input a non-negative integer" << endl;
-    return 1;
-  }
+    if (argc < 2 || std::string(argv[1]) == "")
+    {
+        cout << "Usage: please input a non-negative integer" << endl;
+        return 1;
+    }
 
-  int n = atoi(argv[1]);
-  if (n == 0 && std::string(argv[1]) != "0")
-  {
-    cout << "Usage: please input a non-negative integer" << endl;
-    return 1;
-  }
-  if(n<0)
-  {
-    cout << "Usage: please input a non-negative integer" << endl;
-    return 1;
-  }
-  int i,fact=1;        
-  for(i=1; i<=n; i++)
-  {    
-      fact=fact*i;    
-  }    
-  cout<<fact<<endl;  
-  return 0;
+    int n = atoi(argv[1]);
+    if (n == 0 && std::string(argv[1]) != "0")
+    {
+        cout << "Usage: please input a non-negative integer" << endl;
+        return 1;
+    }
+    if (n < 0)
+    {
+        cout << "Usage: please input a non-negative integer" << endl;
+        return 1;
+    }
+    int i, fact = 1;
+    for (i = 1; i <= n; i++)
+    {
+        fact = fact * i;
+    }
+    cout << fact << endl;
+    return 0;
 }
--- archive/c/c-plus-plus/hello-world.cpp
+++ archive/c/c-plus-plus/hello-world.cpp
@@ -3,6 +3,6 @@ using namespace std;
 
 int main()
 {
-   cout << "Hello, World!";
-   return 0;
+    cout << "Hello, World!";
+    return 0;
 }
