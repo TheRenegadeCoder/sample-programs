c435b892b7f9627d94875e2ed68e8e09536c2659
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-10-10 14:27:52 -0400

    Working on unifying the code style

--- archive/j/java/EvenOdd.java
+++ archive/j/java/EvenOdd.java
@@ -1,36 +1,28 @@
-public class EvenOdd
-{
-    public static void verifyNumber(String n) throws NumberFormatException
-    {
+public class EvenOdd {
+    public static void verifyNumber(String n) throws NumberFormatException {
         if (n.startsWith("-"))
             n = n.substring(1);
         char[] nArray = n.toCharArray();
-        for (char c : nArray)
-        {
+        for (char c : nArray) {
             if (!Character.isDigit(c))
                 throw new NumberFormatException();
         }
     }
 
-    public static void ErrorAndExit()
-    {
+    public static void ErrorAndExit() {
         System.out.println("Usage: please input a number");
         System.exit(1);
     }
 
-    public static void main(String[] args)
-    {
-        try
-        {
+    public static void main(String[] args) {
+        try {
             String nstr = args[0].trim();
             verifyNumber(nstr);
             String lastDigit = nstr.substring(nstr.length() - 1);
             int n = Integer.parseInt(lastDigit);
             String result = n % 2 == 0 ? "Even" : "Odd";
             System.out.println(result);
-        }
-        catch (NumberFormatException | ArrayIndexOutOfBoundsException | StringIndexOutOfBoundsException e)
-        {
+        } catch (NumberFormatException | ArrayIndexOutOfBoundsException | StringIndexOutOfBoundsException e) {
             ErrorAndExit();
         }
     }
