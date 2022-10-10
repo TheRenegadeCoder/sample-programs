1ada42980499a6362daba48ed727b4eb6ec0c753
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-10-10 15:26:04 -0400

    Cleaning up existing code

--- archive/c/c-plus-plus/longest-word.cpp
+++ archive/c/c-plus-plus/longest-word.cpp
@@ -1,38 +1,42 @@
-#include<iostream>
-#include<string>
-#include<cstring>
+#include <iostream>
+#include <string>
+#include <cstring>
 #include <sstream>
 
 using namespace std;
 
-int handle_error(){
+int handle_error()
+{
     printf("Usage: please provide a string");
     exit(0);
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    if(argc<2)
+    if (argc < 2)
     {
         handle_error();
     }
     string inputStr(argv[1]);
 
-    if(inputStr.size()==0){
-         handle_error();
+    if (inputStr.size() == 0)
+    {
+        handle_error();
     }
     int max = -1;
 
     stringstream ss(inputStr);
     string word;
-    while (ss >> word) {
-       
+    while (ss >> word)
+    {
+
         int size = word.size();
-        if(size>max){
+        if (size > max)
+        {
             max = size;
         }
     }
-    cout<<max;
+    cout << max;
 
     return 0;
 }
