8a535b28cffbc1e2f9dff7158fa4f6b5b2be32ef
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-03-10 17:27:55 -0600

    Use glotter2 0.5.0a1 and delete tests

--- test/utilities.py
+++
@@ -1,7 +0,0 @@
-def clean_list(string):
-    """
-    Strip optional characters from list for sake of comparing in tests
-    :param string: the list to clean represented as a string
-    :return: the clean list a as a string
-    """
-    return string.replace('[', '').replace(']', '').strip()
