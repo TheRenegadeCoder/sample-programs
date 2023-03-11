f7273abb6981bea8fac6dbbaa5197143915184af
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-03-11 08:12:44 -0600

    Simplified 'runner.py' down to just invoking 'glotter.main()', deleted all existing 'ProjectType' values, and moved 'ProjectType' definition to 'test/__init__.py' with a single placeholder item

--- runner.py
+++ runner.py
@@ -1,18 +1,3 @@
-from enum import Enum, auto
-from glotter import main as glotter_main
-
-
-class ProjectType(Enum):
-    Placeholder = auto()
-
-    @property
-    def key(self):
-        return self.name.lower()
-
-
-def main():
-    glotter_main()
-
-
 if __name__ == '__main__':
+    from glotter import main
     main()
