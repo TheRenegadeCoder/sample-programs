68cc0e650690fd85ca528ab0d92d057979318d9d
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-03-22 17:13:38 -0500

    Modify Roman Numeral in PHP

--- archive/p/php/roman-numeral.php
+++ archive/p/php/roman-numeral.php
@@ -40,38 +40,6 @@ function roman_to_decimal($romans)
     return $total;
 }
 
-// Unit Tests (needs 'zend.assertions=1' in php.ini, otherwise ignored)
-// Easy values
-assert(roman_to_decimal("") === 0);
-assert(roman_to_decimal("I") === 1);
-assert(roman_to_decimal("V") === 5);
-assert(roman_to_decimal("X") === 10);
-assert(roman_to_decimal("L") === 50);
-assert(roman_to_decimal("C") === 100);
-assert(roman_to_decimal("D") === 500);
-assert(roman_to_decimal("M") === 1000);
-
-// Bad string
-try {
-    roman_to_decimal("XT");
-    assert(false, "Expecting parsing error");
-} catch (Exception $e) { /* OK */}
-
-// Complex values
-assert(roman_to_decimal("XIV") === 14);
-assert(roman_to_decimal("XXV") === 25);
-assert(roman_to_decimal("CMI") === 901);
-assert(roman_to_decimal("MIC") === 1099);
-assert(roman_to_decimal("MDCLXVI") === 1666);
-assert(roman_to_decimal("MDCCCLXXXVIII") === 1888);
-assert(roman_to_decimal("MCMXC") === 1990);
-assert(roman_to_decimal("MCMXCIX") === 1999);
-assert(roman_to_decimal("MMVIII") === 2008);
-assert(roman_to_decimal("MMXII") === 2012);
-assert(roman_to_decimal("MMXIX") === 2019);
-assert(roman_to_decimal("MMMDCCCLXXXVIII") === 3888);
-assert(roman_to_decimal("MMMCMXCV") === 3995);
-
 try {
 
     // Check argument count
