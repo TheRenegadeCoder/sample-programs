4860caff553410b803441667b30b4b7d806efda7
rzuckerm <rzuckerm@users.noreply.github.com>
2023-03-27 17:32:52 -0500

    Modify Longest Word in PHP (#2987)

--- archive/p/php/longest-word.php
+++ archive/p/php/longest-word.php
@@ -10,19 +10,13 @@ function longest_word_length($str)
     return max(array_map("strlen", preg_split("/\s+/", $str)));
 }
 
-// Exit if too few arguments
-if (count($argv) < 2)
-{
-    usage();
-}
-
-// Exit if 1st command-line argument is empty
-$str = $argv[1];
-if (empty($str))
+// Exit if too few arguments or 1st command-line argument is empty
+if (count($argv) < 2 || empty($argv[1]))
 {
     usage();
 }
 
 // Get longest word length and display
+$str = $argv[1];
 $longest_len = longest_word_length($str);
 echo "${longest_len}\n";
