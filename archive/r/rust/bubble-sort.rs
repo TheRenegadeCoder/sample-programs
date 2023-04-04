a20c906fb7c1e5566618d7f9197281b37858c668
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-04-03 19:58:06 -0500

    First pass working integer and integer list parsing

--- archive/r/rust/bubble-sort.rs
+++ archive/r/rust/bubble-sort.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::process;
 
 fn bubble_sort(arr: &mut Vec<i32>) {
   let mut swap_occured = true;
@@ -15,21 +16,39 @@ fn bubble_sort(arr: &mut Vec<i32>) {
   }
 }
 
+fn parse_int(s: &String) -> Option<i32> {
+  match s.trim().parse::<i32>() {
+      Ok(n) => Some(n),
+      Err(e) => None,
+  }
+}
+
+fn parse_int_list(s_list: &String) -> Vec<i32> {
+  let results: Vec<Option<i32>> = s_list.split(",")
+    .map(|s| parse_int(&s.to_string()))
+    .collect();
+  if results.iter().any(|s| s.is_none()) {
+    vec![]
+  }
+  else {
+    results.iter().map(|result| result.unwrap()).collect()
+  }
+}
+
 fn main() {
   let err_msg = "Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\"";
   let args: Vec<String> = env::args().collect();
 
   if args.len() < 2 {
-    panic!(err_msg);
+    println!("{err_msg}");
+    process::exit(0);
   }
 
-  let mut arr: Vec<i32> = args[1].clone()
-    .split(",")
-    .map(|s| s.trim().parse().expect(err_msg))
-    .collect();
+  let mut arr: Vec<i32> = parse_int_list(&args[1]);
 
   if arr.len() < 2 {
-    panic!(err_msg);
+    println!("{err_msg}");
+    process::exit(0);
   }
 
   bubble_sort(&mut arr);
--- archive/r/rust/even-odd.rs
+++ archive/r/rust/even-odd.rs
@@ -1,22 +1,41 @@
 // Requirement is in https://sample-programs.therenegadecoder.com/projects/even-odd/
 // Program to accept an integer on the command line and outputs if the integer is Even or Odd.
 
+use std::env;
+
+fn usage() {
+    println!("Usage: please input a number");
+    std::process::exit(0);
+}
+
+fn parse_int(s: &String) -> Option<i32> {
+    match s.trim().parse::<i32>() {
+        Ok(n) => Some(n),
+        Err(e) => None,
+    }
+}
+
 fn main() {
-    //confirm integer is passed as commandline argument
-    let mut input_value = std::env::args().nth(1).expect("Kindly pass an integer as Command line Argument");
-    // Trim the trailing newline
-    input_value = input_value.trim_end().to_string();
-    //Convert String to Int
-    let input_num: i32 = input_value
-        .trim()
-        .parse()
-        .expect("Kindly enter an integer");
+    // Exit if not enough command-line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        usage();
+    }
+
+    // Exit if 1st command-line argument not an integer
+    let mut input_value: Option<i32> = parse_int(&args[1]);
+    if input_value.is_none() {
+        usage();
+    }
+
+    let input_num: i32 = input_value.unwrap();
+
     // Even numbers are divisible by 2
     if input_num % 2 == 0 {
         println!("Even");
     }
     // Odd numbers are not divisible by 2
     else {
-    	println!("Odd");
-    }    
+        println!("Odd");
+    }
 }
