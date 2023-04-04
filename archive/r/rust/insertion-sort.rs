6c1ee21091d00d8de3af4da6d8a4bf8e54479c3c
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-04-03 20:18:55 -0500

    Got insertion sort working

--- archive/r/rust/insertion-sort.rs
+++ archive/r/rust/insertion-sort.rs
@@ -1,34 +1,58 @@
 use std::env;
+use std::process;
+
+fn usage() {
+    println!("Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\"");
+    process::exit(0);
+}
+
+fn parse_int(s: &String) -> Option<i32> {
+    match s.trim().parse::<i32>() {
+        Ok(n) => Some(n),
+        Err(e) => None,
+    }
+}
+
+fn parse_int_list(s_list: &String) -> Vec<i32> {
+    let results: Vec<Option<i32>> = s_list.split(",")
+        .map(|s| parse_int(&s.to_string()))
+        .collect();
+    if results.iter().any(|s| s.is_none()) {
+        vec![]
+    }
+    else {
+        results.iter().map(|result| result.unwrap()).collect()
+    }
+}
+
 
 fn insertion_sort(arr: &mut Vec<i32>) {
-  for i in 0..arr.len() {
-    for j in (0..i).rev() {
-      if arr[j] >= arr[j+1] {
-        arr.swap(j, j+1);
-      } else {
-        break;
-      }
+    for i in 0..arr.len() {
+        for j in (0..i).rev() {
+            if arr[j] >= arr[j+1] {
+                arr.swap(j, j+1);
+            } else {
+                break;
+            }
+        }
     }
-  }
 }
 
 fn main() {
-  let err_msg = "Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\"";
-  let args: Vec<String> = env::args().collect();
+    // Exit if not enough command-line arguments
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        usage();
+    }
 
-  if args.len() < 2 {
-    panic!(err_msg);
-  }
+    // Convert 1st command-line argument to list of integers
+    let mut arr: Vec<i32> = parse_int_list(&args[1]);
 
-  let mut arr: Vec<i32> = args[1].clone()
-    .split(",")
-    .map(|s| s.trim().parse().expect(err_msg))
-    .collect();
+    // Exit if list too small
+    if arr.len() < 2 {
+        usage();
+    }
 
-  if arr.len() < 2 {
-    panic!(err_msg);
-  }
-  
-  insertion_sort(&mut arr);
-  println!("{:?}", arr);
+    insertion_sort(&mut arr);
+    println!("{:?}", arr);
 }
