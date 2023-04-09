62e809c8014225035becea59c4fae25ef27f8330
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-04-09 12:43:26 -0500

    Fix bad var name and simplify if

--- archive/r/rust/depth-first-search.rs
+++ archive/r/rust/depth-first-search.rs
@@ -80,16 +80,12 @@ impl Graph {
 fn create_graph(adjacency_matrix: &Vec<i32>, vertices: &Vec<i32>) -> Graph {
     // Add child nodes to each node based on non-zero values of adjacency matrix
     let mut graph = Graph::new(vertices[0]);
-    let num_adjancencies = adjacency_matrix.len();
+    let num_adjacencies = adjacency_matrix.len();
     let mut index = 0;
     for row_vertex in vertices {
         graph.add_vertex(*row_vertex);
         for col_vertex in vertices {
-            if index >= num_adjancencies {
-                break;
-            }
-
-            if adjacency_matrix[index] != 0 {
+            if index < num_adjacencies && adjacency_matrix[index] != 0 {
                 graph.add_edge(*row_vertex, *col_vertex);
             }
 
