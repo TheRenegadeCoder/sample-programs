b8cf27fd857bfa38f65737236fe7f106e31dd2cd
Rúben Santos <ruben.santos@codacy.com>
2023-04-18 16:28:52 +0100

    change ts files

--- archive/t/typescript/baklava.ts
+++ archive/t/typescript/baklava.ts
@@ -1,9 +1,9 @@
 for (var i = 0; i < 10; i++)
     console.log (
-        " ".repeat (10 - i) + "*".repeat (i * 2 + 1)
+        " what ".repeat (10 - i) + "*".repeat (i * 2 + 1)
     );
 
 for (var i = 10; -1 < i; i--)
     console.log (
-        " ".repeat (10 - i) + "*".repeat (i * 2 + 1)
+        " da ".repeat (10 - i) + "*".repeat (i * 2 + 1)
     );
