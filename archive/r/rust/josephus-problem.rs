aea97532324b40b25c7254953ec3ab0a55e14ef2
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-05-06 15:18:15 -0500

    Better parsing for Josephus Problem in Rust

--- archive/r/rust/josephus-problem.rs
+++ archive/r/rust/josephus-problem.rs
@@ -1,14 +1,14 @@
 use std::env::args;
+use std::str::FromStr;
 use std::process::exit;
-use std::num::ParseIntError;
 
 fn usage() -> ! {
     println!("Usage: please input the total number of people and number of people to skip.");
     exit(0);
 }
 
-fn parse_int(s: String) -> Result<i32, ParseIntError> {
-    s.trim().parse::<i32>()
+fn parse_int<T: FromStr>(s: &str) -> Result<T, <T as FromStr>::Err> {
+    s.trim().parse::<T>()
 }
 
 // Reference: https://en.wikipedia.org/wiki/Josephus_problem#The_general_case
@@ -29,15 +29,19 @@ fn josephus(n: i32, k: i32) -> i32 {
 }
 
 fn main() {
+    let mut args = args().skip(1);
+
     // Exit if 1st command-line argument not an integer
-    let mut n: i32 = parse_int(
-        args().nth(1).unwrap_or_else(|| usage())
-    ).unwrap_or_else(|_| usage());
+    let mut n: i32 = args
+        .next()
+        .and_then(|s| parse_int(&s).ok())
+        .unwrap_or_else(|| usage());
 
     // Exit if 2nd command-line argument not an integer
-    let mut k: i32 = parse_int(
-        args().nth(2).unwrap_or_else(|| usage())
-    ).unwrap_or_else(|_| usage());
+    let mut k: i32 = args
+        .next()
+        .and_then(|s| parse_int(&s).ok())
+        .unwrap_or_else(|| usage());
 
     // Run Josephus problem and store results
     println!("{}", josephus(n, k));
