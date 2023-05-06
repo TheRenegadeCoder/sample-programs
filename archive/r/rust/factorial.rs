3c11144414903db6c4bbcb878c0947f917bfe325
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-05-06 14:25:04 -0500

    Better parsing for Factorial in Rust

--- archive/r/rust/factorial.rs
+++ archive/r/rust/factorial.rs
@@ -2,34 +2,32 @@
 // Accept a number on command line and print it's factorial
 // Works till factorial 34, which is 39 digits, ...
 use std::env::args;
+use std::str::FromStr;
 use std::process::exit;
-use std::num::ParseIntError;
 
 fn usage() -> ! {
     println!("Usage: please input a non-negative integer");
     exit(0);
 }
 
-fn parse_int(s: String) -> Result<i32, ParseIntError> {
-    s.trim().parse::<i32>()
+fn parse_int<T: FromStr>(s: &str) -> Result<T, <T as FromStr>::Err> {
+    s.trim().parse::<T>()
 }
 
 fn main() {
-    // confirm integer is passed as commandline argument
-    let mut input_num: i32 = parse_int(
-        args().nth(1).unwrap_or_else(|| usage())
-    ).unwrap_or_else(|_| usage());
+    let mut args = args().skip(1);
 
-    // Make sure non-negative
-    if input_num < 0 {
-        usage()
-    }
+    // confirm positive integer is passed as command-line argument
+    let mut input_num: u32 = args
+        .next()
+        .and_then(|s| parse_int(&s).ok())
+        .unwrap_or_else(|| usage());
 
     let mut n = input_num as u128;
     let mut result = 1;
     while n > 0 {
-        result = result * n;
+        result *= n;
         n = n - 1;
     }
-    println!("{}", result );
+    println!("{result}");
 }
