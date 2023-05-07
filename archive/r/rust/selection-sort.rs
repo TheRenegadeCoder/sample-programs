01b82483a76c5f431804c92bbc32278bb6d4b31b
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-05-07 06:20:49 -0500

    Better parsing and use of generics for Selection Sort in Rust

--- archive/r/rust/selection-sort.rs
+++ archive/r/rust/selection-sort.rs
@@ -1,31 +1,23 @@
 use std::env::args;
 use std::process::exit;
-use std::num::ParseIntError;
+use std::str::FromStr;
 
 fn usage() -> ! {
     println!("Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\"");
     exit(0);
 }
 
-fn parse_int(s: String) -> Result<i32, ParseIntError> {
-    s.trim().parse::<i32>()
+fn parse_int<T: FromStr>(s: &str) -> Result<T, <T as FromStr>::Err> {
+    s.trim().parse::<T>()
 }
 
-fn parse_int_list(s_list: String) -> Option<Vec<i32>> {
-    let results: Vec<Result<i32, ParseIntError>> = s_list.split(",")
-        .map(|s| parse_int(s.to_string()))
-        .collect();
-    match results.iter().any(|s| s.is_err()) {
-        true => None,
-        false => Some(
-            results.iter()
-            .map(|result| result.clone().unwrap())
-            .collect()
-        )
-    }
+fn parse_int_list<T: FromStr>(s: &str) -> Result<Vec<T>, <T as FromStr>::Err> {
+    s.split(',')
+        .map(parse_int)
+        .collect::<Result<Vec<T>, <T as FromStr>::Err>>()
 }
 
-fn selection_sort(arr: &mut Vec<i32>) {
+fn selection_sort<T: PartialOrd>(arr: &mut Vec<T>) {
     // Source: https://en.wikipedia.org/wiki/Selection_sort#Implementations
     for i in 0..(arr.len() - 1) {
         let mut min_pos = i;
@@ -42,17 +34,19 @@ fn selection_sort(arr: &mut Vec<i32>) {
 }
 
 fn main() {
+    let mut args = args().skip(1);
+
     // Convert 1st command-line argument to list of integers
-    let mut arr: Vec<i32> = parse_int_list(
-        args().nth(1)
-        .unwrap_or_else(|| usage())
-    ).unwrap_or_else(|| usage());
+    let mut arr: Vec<i32> = args
+        .next()
+        .and_then(|s| parse_int_list(&s).ok())
+        .unwrap_or_else(|| usage());
 
     // Exit if list too small
     if arr.len() < 2 {
         usage();
     }
 
-    selection_sort(&mut arr);
-    println!("{:?}", arr);
+    selection_sort::<i32>(&mut arr);
+    println!("{arr:?}");
 }
