c6b4dab5885877ce544e427c2681b887787c9f59
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-05-13 07:19:02 -0500

    Fix formatting for Hello World and Factorial

--- archive/d/d/factorial.d
+++ archive/d/d/factorial.d
@@ -5,40 +5,40 @@ import core.stdc.stdlib: exit;
 
 void usage()
 {
-  writeln("Usage: please input a non-negative integer");
-  exit(0);
+    writeln("Usage: please input a non-negative integer");
+    exit(0);
 }
 
 int fac(int a)
 {
-  if (a <= 1)
-  {
-    return 1;
-  }
-  return a*fac(a-1);
+    if (a <= 1)
+    {
+        return 1;
+    }
+    return a*fac(a-1);
 }
 
 int main(string[] args)
 {
-  if (args.length < 2)
-  {
-    usage();
-  }
-
-  int depth;
-  try {
-    depth = to!int(args[1]);
-    if (depth < 0)
+    if (args.length < 2)
     {
-      usage();
+        usage();
     }
-  }
-  catch(ConvException _)
-  {
-    usage();
-  }
 
-  int result = fac(depth);
-  writeln(result);
-  return 0;
+    int depth;
+    try {
+        depth = to!int(args[1]);
+        if (depth < 0)
+        {
+            usage();
+        }
+    }
+    catch(ConvException _)
+    {
+        usage();
+    }
+
+    int result = fac(depth);
+    writeln(result);
+    return 0;
 }
