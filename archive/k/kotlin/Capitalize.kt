71ff4c1d610311ecc61439e3b634860565b0a959
Rúben Santos <ruben.santos@codacy.com>
2023-05-16 11:16:24 +0100

    remove kotlin

--- archive/k/kotlin/Capitalize.kt
+++
@@ -1,9 +0,0 @@
-fun main(args: Array<String>) {
-    if (args.isNullOrEmpty() || args[0].isBlank()) {
-        println("Usage: please provide a string")
-    } else {
-        // Kotlin provides a simple `capitalize()` function in the standard
-        // library for all String objects
-        println(args[0].capitalize())
-    }
-}
