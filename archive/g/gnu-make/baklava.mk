d07d3c11e6ec802b1377e12c8c4fa02d6a46716f
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-07-09 14:15:19 -0500

    Add comment about number encoding

--- archive/g/gnu-make/baklava.mk
+++ archive/g/gnu-make/baklava.mk
@@ -11,36 +11,36 @@ TWO := x x
 TEN := $(TWO) $(TWO) $(TWO) $(TWO) $(TWO)
 
 # Increment function
-# Arg 1: Number
-# Return: Number + 1
+# Arg 1: Number encoded as x's
+# Return: Number + 1 encoded as x's
 INC = $(1) $(ONE)
 
 # Decrement function
-# Arg 1: Number
-# Return: Number - 1 unless 0, else Number
+# Arg 1: Number encoded as x's
+# Return: max(Number - 1, 0) encoded as x's
 DEC = $(wordlist 2,$(words $(1)),$1)
 
 # Add function
-# Arg 1: Number 1
-# Arg 2: Number 2
-# Return: Number 1 + Number 2
+# Arg 1: Number 1 encoded as x's
+# Arg 2: Number 2 encoded as x's
+# Return: Number 1 + Number 2 encoded as x's
 ADD = $(1) $(2)
 
 # Subtract function
-# Arg 1: Number 1
-# Arg 2: Number 2
-# Return: Max(Number 1 - Number 2, 0)
+# Arg 1: Number 1 encoded as x's
+# Arg 2: Number 2 encoded as x's
+# Return: Max(Number 1 - Number 2, 0) encoded as x's
 SUB = $(wordlist $(words $(call INC,$(2))),$(words $(1)),$(1))
 
 # Repeat function
 # Arg 1: Character
-# Arg 2: Number of repeats
+# Arg 2: Number of repeats encoded as x's
 # Return: Character repeated specified number of times
 REPEAT = $(if $(2),$(1)$(call REPEAT,$(1),$(call DEC,$(2))))
 
 # Baklava line function
-# Arg 1: Number of spaces
-# Arg 2: Number of stars
+# Arg 1: Number of spaces encoded as x's
+# Arg 2: Number of stars encoded as x's
 # Return: Specified number of spaces followed by specified number of stars
 BAKLAVA_LINE = $(call REPEAT,$(SPACE),$(1))$(call REPEAT,$(STAR),$(2))
 
@@ -48,8 +48,8 @@ BAKLAVA_LINE = $(call REPEAT,$(SPACE),$(1))$(call REPEAT,$(STAR),$(2))
 #
 # Output upper portion of Bakalava
 #
-# Arg 1: Starting number of spaces
-# Arg 2: Starting number of stars
+# Arg 1: Starting number of spaces encoded as x's
+# Arg 2: Starting number of stars encoded as x's
 define UPPER_BAKLAVA_LOOP
 $(info $(call BAKLAVA_LINE,$(1),$(2)))
 $(if $(1),$(call UPPER_BAKLAVA_LOOP,$(call DEC,$(1)),$(call ADD,$(2),$(TWO))))
@@ -58,8 +58,8 @@ endef
 # Baklava lower loop
 #
 # Output lower portion of Baklava
-# Arg 1: Starting number of spaces
-# Arg 2: Starting number of stars
+# Arg 1: Starting number of spaces encoded as x's
+# Arg 2: Starting number of stars encoded as x's
 define LOWER_BAKLAVA_LOOP
 $(if $(2),\
     $(info $(call BAKLAVA_LINE,$(1),$(2)))\
