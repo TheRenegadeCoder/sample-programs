f20cb7859560a3a4ad9b3edf514997d9a359bbab
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-08-20 10:29:53 -0500

    Add Zig Tests

--- archive/z/zig/hello-world.zig
+++ archive/z/zig/hello-world.zig
@@ -2,5 +2,5 @@ const std = @import("std");
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("Hello, {}!\n", .{"world"});
+    try stdout.writeAll("Hello, World!\n");
 }
