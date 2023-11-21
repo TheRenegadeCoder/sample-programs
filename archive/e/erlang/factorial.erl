013898cb9a22caa6df8ab0a4ef1847657517088d
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-11-20 18:26:14 -0600

    Add Erlang Tests and fix samples

--- archive/e/erlang/factorial.erl
+++ archive/e/erlang/factorial.erl
@@ -1,15 +1,35 @@
 -module(factorial).
--export([start/1]).
+-export([main/1]).
 
 -spec start(Number :: integer()) -> integer().
 start(N) 
-    when N<0;
-         not is_integer(N) ->
-    io:format("Usage: please input a non-negative integer~n");
+    when N<0 ->
+        usage();
 start(0) ->
     factorial(1,1);
 start(N) ->
     factorial(N,N).
+
+convert_to_integer(Str) ->
+    Result = catch string:to_integer(string:strip(Str)),
+    case Result of
+        {Int, Rest} when Rest == "" ->
+            Int;
+        _ ->
+            usage()
+    end.
+
+main(Args) ->
+    if
+        length(Args) < 1 ->
+            usage();
+        true ->
+            ok
+    end,
+
+    StrValue = lists:nth(1, Args),
+    Value = convert_to_integer(StrValue),
+    start(Value).
 
 %%--------------------------------------------------------------------
 %% Recursively multiply N times N-1 until N-1=1. Output Accumulator
@@ -18,3 +38,7 @@ factorial(1,Acc) ->
     io:format("~w~n", [Acc]);
 factorial(N,Acc) ->
     factorial(N-1, (N-1)*Acc).
+
+usage() ->
+    io:format("Usage: please input a non-negative integer~n"),
+    erlang:halt().
--- archive/e/erlang/reverse_string.erl
+++ archive/e/erlang/reverse_string.erl
@@ -1,16 +1,20 @@
 -module(reverse_string).
--export([start/1]).
+-export([main/1]).
 
--spec start(StringOrAtom :: list() | atom() | integer()) -> 
-   list() | atom() | integer().
+-spec start(String :: string()) -> string().
 %%--------------------------------------------------------------------
-%% Reverse a given string, atom, or integer
+%% Reverse a given string
 %%--------------------------------------------------------------------
-start(String) when is_list(String)  ->
-   lists:reverse(String);
+start(String) ->
+   lists:reverse(String).
 
-start(Atom) when is_atom(Atom) ->
-   list_to_atom(lists:reverse(atom_to_list(Atom)));
+main(Args) ->
+    if
+        length(Args) >= 1 ->
+            Str = lists:nth(1, Args);
+        true ->
+            Str = ""
+    end,
 
-start(Number) when is_integer(Number) ->
-   list_to_integer(lists:reverse(integer_to_list(Number))).
+   ReverseStr = start(Str),
+   io:format("~s~n", [ReverseStr]).
