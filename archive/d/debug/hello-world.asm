ce99855d175f5dc35c15c117a190ad131decc900
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-11-24 15:26:48 -0600

    Add Debug Tests

--- archive/d/debug/hello-world.asm
+++ archive/d/debug/hello-world.asm
@@ -1,25 +1,35 @@
 ; ------------------------------------------------------------------
-; helloworld.asm
+; hello-world.asm
 ;
-; This is a Win64 console program that writes "Hello World"
+; This is a console program that writes "Hello, World!"
 ; on a single line and then exits.
 ;
-; To assemble to .obj: nasm -f Win64 helloworld.asm
-; To compile to .exe:  gcc helloworld.obj -o helloworld.exe
+; To assemble: nasm -f elf64 -o hello-world.o hello-world.asm
+; To compile:  gcc -o hello-world hello-world.o
+; To run: ./hello-world
 ; ------------------------------------------------------------------
 
-        global    _main                ; declare main() method
-        extern    _printf              ; link to external library
-
-        segment  .data
-        message: db   'Hello world', 0xA, 0  ; text message
-                    ; 0xA (10) is hex for (NL), carriage return
-                    ; 0 terminates the line
+; Source: https://www.mourtada.se/calling-printf-from-the-c-standard-library-in-assembly/
 
         ; code is put in the .text section
-        section .text
-_main:                            ; the entry point! void main()
-        push    message           ; save message to the stack
-        call    _printf           ; display the first value on the stack
-        add     esp, 4            ; clear the stack
+        section   .text
+        default   rel
+        global    main                 ; declare main() method
+        extern    printf               ; link to external library
+
+main:                             ; the entry point! void main()
+        push    rbp               ; create stack frame
+        mov     rdi, fmt          ; set format string
+        mov     rsi, message      ; set message string
+        mov	rax, 0            ; not sure why this is needed
+        call    printf wrt ..plt  ; call printf
+        pop     rbp               ; destroy stack frame
+        mov     rax, 0            ; set return code
         ret                       ; return
+
+        segment   .data
+        default   rel
+        fmt:      db   '%s', 0                  ; format string
+        message:  db   'Hello, World!', 0xA, 0  ; text message
+                       ; 0xA (10) is hex for (NL), carriage return
+                       ; 0 terminates the line
