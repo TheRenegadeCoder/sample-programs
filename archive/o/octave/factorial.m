47b6bae49b5d3623f00f89bf55df3a21a61a128b
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-12-06 19:40:28 -0600

    Migrate Prime Number and fix Factorial

--- archive/o/octave/factorial.m
+++ archive/o/octave/factorial.m
@@ -1,19 +1,21 @@
-arg_list = argv();
-nargin = length(arg_list);
-if nargin == 0
-    disp('Usage: please input a non-negative integer')
-    return
-end
+function factorial()
+    arg_list = argv();
+    nargin = length(arg_list);
+    if nargin == 0
+        disp('Usage: please input a non-negative integer')
+        return
+    end
 
-n = str2num(arg_list{1});
-if length(n) ~= 1 || mod(n, 1) ~= 0 || n < 0
-    disp('Usage: please input a non-negative integer')
-    return
-end
+    n = str2num(arg_list{1});
+    if length(n) ~= 1 || mod(n, 1) ~= 0 || n < 0
+        disp('Usage: please input a non-negative integer')
+        return
+    end
 
-x = 1;
-for i = 1:n
-    x = x * i;
-end
+    x = 1;
+    for i = 1:n
+        x = x * i;
+    end
 
-disp(x);
+    disp(x);
+end
---
+++ archive/o/octave/prime_number.m
@@ -0,0 +1,38 @@
+function prime_number()
+    arg_list = argv();
+    nargin = length(arg_list);
+    if nargin == 0
+        disp('Usage: please input a non-negative integer')
+        return
+    end
+
+    n = str2num(arg_list{1});
+    if length(n) ~= 1 || mod(n, 1) ~= 0 || n < 0
+        disp('Usage: please input a non-negative integer')
+        return
+    end
+
+    isprime = 0;
+    if n == 2
+        isprime = 1;
+    elseif n < 2 || rem(n, 2) == 0
+        isprime = 0;
+    else
+        isprime = 1;
+        q = sqrt(n);
+        m = 3;
+        while m <= q
+            if rem(n, m) == 0
+                isprime = 0;
+                break;
+            end
+            m = m + 2;
+        end
+    end
+
+    if isprime == 1
+        disp('prime')
+    else
+        disp('composite')
+    end
+end
