df4af034d996723c5709e240ffd4dbd6a503cdbe
Ron Zuckerman <ron.zuckerman@adtran.com>
2023-12-13 15:09:58 -0600

    Fix Even Odd

--- archive/o/objective-c/even-odd.m
+++ archive/o/objective-c/even-odd.m
@@ -1,19 +1,48 @@
 #import <Foundation/Foundation.h>
+
+// Function to convert and validate the input string
+// Source: ChatGPT
+// Function to convert and validate the input string
+NSInteger convertAndValidateInput(NSString *inputString) {
+    NSScanner *scanner = [NSScanner scannerWithString:inputString];
+    NSInteger integerValue = 0;
+
+    // Check if the scanner successfully scanned an integer
+    if ([scanner scanInteger:&integerValue] && [scanner isAtEnd]) {
+        return integerValue;
+    } else {
+        // Raise an exception for invalid input
+        @throw [NSException exceptionWithName:@"InvalidInputException"
+            reason:@"Input is not a valid integer"
+            userInfo:nil];
+    }
+}
+
 int main (int argc, char *argv[])
 {
- NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
- 
- int numbertotest, remainder;
- NSLog(@"Enter your number to be tested: ");
- scanf("%i", &numbertotest);
- 
- remainder = numbertotest % 2;
- 
- if (remainder == 0)
- NSLog(@"The number is even.");
- else
- NSLog(@"The number is odd.");
- 
- [pool drain];
- return 0;
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    NSString *usage = @"Usage: please input a number";
+
+    if (argc < 2) {
+        printf("%s\n", [usage UTF8String]);
+    }
+    else {
+        NSString* inputStr = [NSString stringWithUTF8String:argv[1]];
+        @try {
+            int input = (int)convertAndValidateInput(inputStr);
+            int remainder = input % 2;
+            if (remainder == 0) {
+                printf("Even\n");
+            }
+            else {
+                printf("Odd\n");
+            }
+        }
+        @catch (NSException *) {
+            printf("%s\n", [usage UTF8String]);
+        }
+    }
+
+    [pool drain];
+    return 0;
 }
