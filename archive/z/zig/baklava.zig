efa34e8eacb8bb879b2eabb8c5e494f6fb810539
rzuckerm <ron.zuckerman@adtran.com>
2024-09-21 08:35:41 -0500

    Use stdout.print instead of debug.print

--- archive/z/zig/baklava.zig
+++ archive/z/zig/baklava.zig
@@ -1,13 +1,14 @@
 const std = @import("std");
 
 pub fn main() !void {
+    const stdout = std.io.getStdOut().writer();
     const spaces = " " ** 10;
     const stars = "*" ** 21;
     var i: i32 = -10;
     while (i <= 10) {
         const numSpaces: usize = @intCast(@abs(i));
         const numStars: usize = 21 - numSpaces * 2;
-        std.debug.print("{s}{s}\n", .{spaces[0..numSpaces], stars[0..numStars]});
+        try stdout.print("{s}{s}\n", .{spaces[0..numSpaces], stars[0..numStars]});
         i += 1;
     }
 }
