3f153d2be7ab69647834cd1e03ca1c03398784d5
rzuckerm <ron.zuckerman@adtran.com>
2024-09-21 08:56:36 -0500

    Can increment in while statement

--- archive/z/zig/baklava.zig
+++ archive/z/zig/baklava.zig
@@ -5,10 +5,9 @@ pub fn main() !void {
     const spaces = " " ** 10;
     const stars = "*" ** 21;
     var i: i32 = -10;
-    while (i <= 10) {
+    while (i <= 10): (i += 1) {
         const numSpaces: usize = @intCast(@abs(i));
         const numStars: usize = 21 - numSpaces * 2;
         try stdout.print("{s}{s}\n", .{spaces[0..numSpaces], stars[0..numStars]});
-        i += 1;
     }
 }
