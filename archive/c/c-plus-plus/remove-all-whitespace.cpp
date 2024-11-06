8fbeaac5ccd707d21be632d1dfcfd89821144f5d
LauraV-702 <Villaraza.laura@student.greenriver.edu>
2024-11-06 22:11:23 +0000

    Added small instruction comments inside main

--- archive/c/c-plus-plus/remove-all-whitespace.cpp
+++ archive/c/c-plus-plus/remove-all-whitespace.cpp
@@ -2,6 +2,14 @@
 #include <string>
 
 int main() {
+    //added example string
     std::string str = "This is a string with spaces.";
     std::string result;
+
+    //add loop and iterate through the strings 
+        //add characters that aren't spaces
+            //print/update result
+    
+    //print out the result
+    //return 0;
 }
