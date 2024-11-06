b9832c4f4cc9571641455b0e3809d9eb5891f5d0
LauraV-702 <Villaraza.laura@student.greenriver.edu>
2024-11-06 22:18:41 +0000

    Changed string variable 'str' to 'sentence' for clarity

--- archive/c/c-plus-plus/remove-all-whitespace.cpp
+++ archive/c/c-plus-plus/remove-all-whitespace.cpp
@@ -3,13 +3,13 @@
 
 int main() {
     //added example string
-    std::string str = "This is a string with spaces.";
+    std::string sentence = "This is a sentence with spaces.";
     std::string result;
 
     //add loop and iterate through the strings 
-    for(char c : str) {
+    for(char c : sentence) {
     //add characters that aren't spaces
-        
+        if (c != ' ')
     }
         
             //print/update result
