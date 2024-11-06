5adbd5b917d11c85ebe08148361e1dbd948029f1
LauraV-702 <Villaraza.laura@student.greenriver.edu>
2024-11-06 22:56:06 +0000

    Changed variable name 'sentence' to 'noSpace' to match test casing.

--- archive/c/c-plus-plus/remove-all-whitespace.cpp
+++ archive/c/c-plus-plus/remove-all-whitespace.cpp
@@ -3,11 +3,11 @@
 
 int main() {
     //added example string
-    std::string sentence = "RemoveAllWhiteSpace";
+    std::string noSpace = "RemoveAllWhiteSpace";
     std::string result;
 
     //add loop and iterate through the strings 
-    for(char c : sentence) {
+    for(char c : noSpace) {
     //add characters that aren't spaces
         if (c != ' ') {
     //print/update result
