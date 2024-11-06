5689971104628d0e07f6ff316bc8c6c58be39cad
LauraV-702 <Villaraza.laura@student.greenriver.edu>
2024-11-06 23:36:08 +0000

    Debugged and fixed helper method to pass in test cases

--- archive/c/c-plus-plus/remove-all-whitespace.cpp
+++ archive/c/c-plus-plus/remove-all-whitespace.cpp
@@ -5,22 +5,22 @@ int main() {
     //added example string
     std::string noSpace = "RemoveAllWhiteSpace";
     std::string leadSpace = " RemoveAllWhiteSpace";
+
+    return 0;
 } 
 
-void removeWhiteSpace() {
+void removeWhiteSpace(const std::string& input) {
     
     std::string result;
     //add loop and iterate through the strings 
-    for(char c : noSpace + leadSpace) {
+    for(char c : input) {
     //add characters that aren't spaces
         if (c != ' ') {
     //print/update result
             result += c;
         }
     }
-
     //print out the result
     //return 0;
     std::cout << result << std::endl;
-    return 0;
 }
