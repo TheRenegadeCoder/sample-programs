205d941ad908adac498a049fe2ffccb2c96fd790
LauraV-702 <Villaraza.laura@student.greenriver.edu>
2024-11-07 08:12:41 +0000

    Removed helper method, removed test cases, moved all function into main method

--- archive/c/c-plus-plus/remove-all-whitespace.cpp
+++ archive/c/c-plus-plus/remove-all-whitespace.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
 #include <string>
+#include <cctype>  // for std::isspace
 
-void removeWhitespace(const std::string& input) {
+
+int main(const std::int& input) {
+
     
     if (input.empty()) {
         std::cout << "Usage: please provide a string" << std::endl;
@@ -14,6 +17,7 @@ void removeWhitespace(const std::string& input) {
     //add characters that aren't spaces
     // If the character is not a whitespace character, add it to result 
     // std::isspace -> if given character is a whitespace chara
+    
         if (!std::isspace(static_cast<unsigned char>(c))) {
             result += c;
         }
@@ -21,25 +25,6 @@ void removeWhitespace(const std::string& input) {
     //print out the result
     //return 0;
     std::cout << result << std::endl;
-}
-
-int main() {
-    //added example string
-    std::string noSpace = "RemoveAllWhitespace";
-    std::string leadSpace = " RemoveAllWhitespace";
-    std::string trailSpace = "RemoveAllWhitespace ";
-    std::string innerSpace = "Remove All Whitespace";
-    std::string tabs = "\tRemove\tAll\tWhitespace\t";
-    std::string newsLines = "\nRemove\nAll\nWhitespace\n";
-    std::string carriageReturns = "\rRemove\rAll\rWhitespace";
-
-    removeWhitespace(noSpace); 
-    removeWhitespace(leadSpace); 
-    removeWhitespace(trailSpace);
-    removeWhitespace(innerSpace);
-    removeWhitespace(tabs);
-    removeWhitespace(newsLines);
-    removeWhitespace(carriageReturns);
-    
+   
     return 0;
 } 
