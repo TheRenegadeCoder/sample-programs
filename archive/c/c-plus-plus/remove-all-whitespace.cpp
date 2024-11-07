7c57213b99c54b6c39ee0ffe6e520142788e8860
LauraV-702 <Villaraza.laura@student.greenriver.edu>
2024-11-07 08:44:09 +0000

    Cleaned up comments, made it clear and concise

--- archive/c/c-plus-plus/remove-all-whitespace.cpp
+++ archive/c/c-plus-plus/remove-all-whitespace.cpp
@@ -5,34 +5,29 @@
 
 int main(int argc, char* argv[]) {
 
-    
+    // Check if given string passed
     if (argc < 2) {
         std::cout << "Usage: please provide a string" << std::endl;
-        return 1; // Return error code 
+        return 1; // Return error code if no string is given
     }
 
-    std::string input = argv[1]; // Get the input string from the command-line argument
-                                // Argument Vector. An array of character pointers, where each points points to a string
+    // Get the string passed 
+    std::string input = argv[1]; 
     std::string result;
 
-    //check if input string is empty
+    // Check if input string is empty
     if (input.empty()) {
         std::cout << "Usage: please provide a string" << std::endl;
-        return 1; // Exit error code
+        return 1; // Exit error code if the string is empty
     } 
 
-    //add loop and iterate through the strings 
     for(char c : input) {
-    //add characters that aren't spaces
-    // If the character is not a whitespace character, add it to result 
-    // std::isspace -> if given character is a whitespace chara
-    
+        // If the character is not a whitespace character, add it to result     
         if (!std::isspace(static_cast<unsigned char>(c))) {
             result += c;
         }
     }
-    //print out the result
-    //return 0;
+    //print out the result (string should have no spaces)
     std::cout << result << std::endl;
    
     return 0;
