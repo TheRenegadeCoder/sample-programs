b064d628f9ab264e3a70330cfceadeebc2a7ad60
f3liz <125415218+f3liz@users.noreply.github.com>
2024-11-08 21:21:44 +0000

    fixed typo in Main, fixed if statement logic, added RemoveEmptyEntries, added code to store string from args

--- archive/c/c-sharp/LongestWord.cs
+++ archive/c/c-sharp/LongestWord.cs
@@ -1,18 +1,25 @@
 using System;
+using System.Linq;
 
 public class LongestWord
 {
-    public static void main(string sentence)
+    public static void Main(string[] args)
     {
-        if (sentence.IsNullOrEmpty) {
-            console.WriteLine("Usage: please provide a string");
+        // check for empty string or empty input
+        if (args.Length == 0 || args[0] == "") {
+            Console.WriteLine("Usage: please provide a string");
         } else {
-            // split string by whitespace (these four special characters)
-            string[] words = sentence.Split(new[] {' ', '\t', '\n', '\r'});
+            // stores string from args
+            string sentence = args[0];
+
+            // split string by whitespace (these four special characters), removes empty entries
+            string[] words = sentence.Split(new[] {' ', '\t', '\n', '\r'}, StringSplitOptions.RemoveEmptyEntries);
 
             // sort array by length in descending order so longest string is first and returns is to array
             words = words.OrderByDescending(word => word.Length).ToArray();
-            console.WriteLine(words[0].Length);
+
+            // log the length of longest word
+            Console.WriteLine(words[0].Length);
         }
     }
 
