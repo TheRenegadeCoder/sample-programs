150d8c0e2dd2bbe7e4db4dbf3e2a46c503045255
rzuckerm <ron.zuckerman@adtran.com>
2024-11-09 06:55:40 -0600

    Try to get C/C++ to work

--- scripts/get_codeql_languages.py
+++ scripts/get_codeql_languages.py
@@ -2,20 +2,20 @@ import argparse
 import json
 from fnmatch import fnmatch
 
-# Can't get C/C++, C#, Java/Kotlin, and Swift to work. Comment out for now
+# Can't get C#, Java, Kotlin, and Swift to work. Comment out for now
 CODEQL_LANGUAGES = {
     "scripts/*.py": "python",
-    # "archive/c/c/*.c": "c-cpp",
-    # "archive/c/c-plus-plus/*.cpp": "c-cpp",
+    "archive/c/c/*.c": "c",
+    "archive/c/c-plus-plus/*.cpp": "cpp",
     #"archive/c/c-sharp/*.cs": "c#",
     "archive/g/go/*.go": "go",
-    # "archive/j/java/*.java": "java-kotlin",
-    "archive/j/javascript/*.js": "javascript-typescript",
-    # "archive/k/kotlin/*.kt": "java-kotlin",
+    # "archive/j/java/*.java": "java",
+    "archive/j/javascript/*.js": "javascript",
+    # "archive/k/kotlin/*.kt": "kotlin",
     "archive/p/python/*.py": "python",
     "archive/r/ruby/*.rb": "ruby",
     # "archive/s/swift/*.swift": "swift",
-    "archive/t/typescript/*.ts": "javascript-typescript",
+    "archive/t/typescript/*.ts": "typescript",
 }
 ALL_CODEQL_LANGUAGES = set(CODEQL_LANGUAGES.values())
 ALL_CODEQL_LANGUAGES_FILES = {
