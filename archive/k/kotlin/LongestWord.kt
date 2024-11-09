b30a0a2bb14b1b827573c8e3554f2c2e734f17a3
mechance782 <147088054+mechance782@users.noreply.github.com>
2024-11-09 04:15:41 +0000

    set up function logic

--- archive/k/kotlin/LongestWord.kt
+++ archive/k/kotlin/LongestWord.kt
@@ -1,14 +1,16 @@
 fun main(args: Array<String>) {
-
-    fun longestWord(sentence: String) {
-        var words = sentence.split(" ")
-        var longest = 0
-        for (word in words){
-            when {
-                word.length > longest -> longest = word.length
+    fun longestWord(sentence: Any): Any {
+        if (sentence is String){
+            var words = sentence.split(" ");
+            var longest = 0;
+            for (word in words){
+                when {
+                    word.length > longest -> longest = word.length;
+                }
             }
+            return longest; 
+        } else {
+            return "Please Enter A String";
         }
-        return longest
     }
-
 }
