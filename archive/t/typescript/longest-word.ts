5233d96dcb54a043ffd4105bb2df6847c9795ad6
TiaMarieG <38619182+TiaMarieG@users.noreply.github.com>
2024-11-09 19:36:14 +0000

    New attempt - passed 2/5 tests

--- archive/t/typescript/longest-word.ts
+++ archive/t/typescript/longest-word.ts
@@ -1,21 +1,25 @@
-const error_message: string = "Usage: please provide a string";
-
-function longestWord(input: string): number {
-
-    const separatedWords: string[] = input.split(' ');
-
-    let longestWord: string = "";
-    let wordLength: number = 0;
-
-    for (let word of separatedWords) {
-        if (word.length > longestWord.length) {
-            longestWord = word;
-            wordLength = word.length;
-        }
-        else if (word == "") {
-            console.log(error_message);
-        }
-    }
-
-    return wordLength;
+if (process.argv.length < 3 || process.argv[2].length < 1) {
+    console.log("Usage: please provide a string")
+    process.exit(0)
 }
+
+let testString: string = process.argv[2];
+
+//     const separatedWords: string[] = input.split(' ');
+
+//     let longestWord: string = "";
+//     let wordLength: number = 0;
+
+//     for (let word of separatedWords) {
+//         if (word.length > longestWord.length) {
+//             longestWord = word;
+//             wordLength = word.length;
+//         }
+//         else if (word == "") {
+//             console.log(error_message);
+//             process.exit(1);
+//         }
+//     }
+
+//     return wordLength;
+// }
