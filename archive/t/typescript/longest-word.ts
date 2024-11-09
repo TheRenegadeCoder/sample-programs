d90dae4d692638827edd7a845d694c660f67d4f8
TiaMarieG <38619182+TiaMarieG@users.noreply.github.com>
2024-11-09 18:33:06 +0000

    First attempt - Wrote code based off experience with JS

--- archive/t/typescript/longest-word.ts
+++ archive/t/typescript/longest-word.ts
@@ -1,1 +1,21 @@
 const error_message: string = "Usage: please provide a string";
+
+function longestWord(input: string): number {
+
+    const separatedWords: string[] = input.split(' ');
+
+    let longestWord: string = "";
+    let wordLength: number = 0;
+
+    for (let word of separatedWords) {
+        if (word.length > longestWord.length) {
+            longestWord = word;
+            wordLength = word.length;
+        }
+        else if (word == "") {
+            console.log(error_message);
+        }
+    }
+
+    return wordLength;
+}
