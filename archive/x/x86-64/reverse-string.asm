75583a5331c735288dc20dea26bf46f871ab904f
RMarx1456 <102569357+RMarx1456@users.noreply.github.com>
2024-11-10 04:00:59 +0000

    Finally fixed this. Had to run it through GDB on my own computer.

--- archive/x/x86-64/reverse-string.asm
+++ archive/x/x86-64/reverse-string.asm
@@ -1,3 +1,12 @@
+%MACRO TESTING 0
+MOV RAX, SYS_WRITE
+MOV RDI, STDOUT
+MOV RSI, testing.txt
+MOV RDX, testing.len
+SYSCALL
+%ENDMACRO
+
+
 ;I/0
 %DEFINE SYS_WRITE 1
 %DEFINE STDOUT 1
@@ -23,9 +32,13 @@ section .rodata
 
     empty:
         .txt db '\'', 0xA
-        .len equ $- empty.txt
+        .len equ $- .txt
 
     quote db '\''
+
+    testing:
+        .txt db 'Test', 0xA
+        .len equ $- .txt
     
     
 section .text
@@ -75,40 +88,39 @@ reverseString:
 ; Returns:
 ;   RAX - (char*)         Pointer to reversed string.
 ; ----------------------------------------------------------------------------  
-    %DEFINE reverseString.STACK_INIT 8
+    %DEFINE reverseString.STACK_INIT 16
     PUSH RBP
     MOV RBP, RSP
     SUB RSP, reverseString.STACK_INIT
     
-    MOV [RBP-8], RDI ;Move the string pointer to the stack.    
+    MOV [RBP-8], RDI ;Move the string pointer to the stack.  
+    MOV [RBP-16], RSI ;Move the string length to the stack.  
     
     MOV RAX, SYS_MMAP
     MOV RDI, 0 ;Let the OS decide which address to use.
-    MOV RSI, RSI ;Redundant, but RSI will hold how many bytes to allocate.
+    ;RSI will hold how many bytes to allocate, but that's already initialized so we won't add it.
     MOV RDX, PROT_WRITE | PROT_READ ;We'll allow the memory to be read and written to.
     MOV R10, MAP_PRIVATE | MAP_ANONYMOUS ;We won't allow other processes to touch this memory, and it will not be associated with a file.
     MOV R8, 0 ;No file descriptor.
     MOV R9, 0 ;No offset.
     SYSCALL
-    
+    MOV RBX, RAX ;RBX (base register) will hold the start of our new string for later.
     MOV RDI, RAX ;RDI (destination register) will hold our new string pointer.
-    MOV RBX, RSI ;RBX (base register) will hold the end address.
-    DEC RBX
+    ;Add string length to RDI and then subrtract by one so we can start at end of address.
+    ADD RDI, [RBP-16]
+    DEC RDI
     MOV RSI, [RBP-8] ;RSI (source register) will hold the old string address.
     MOV RCX, 0 ;RCX (counter register) will hold the loop counter.
-    ;Address calculations
-    ADD RSI, RBX
     .loop:
-        MOV AL, [RSI] ;Move char from location RSI to AL
-        MOV [RDI+RCX], AL ;Move char from AL to location pointer to at RDI+RCX
-        ;Adding to counter and shifting pointers.
-        INC RSI
-        DEC RSI
-        CMP RCX, RBX
-        JBE .loop
-        
-    POP RAX ;Equivalent to MOV RAX, [RBP-8] (dependant on stack size) ADD RSP, 8
-    MOV RAX, RDI ;Move the new string pointer into RAX for returning.
+        MOV AL, [RSI+RCX] ;Move char from location RSI+RCX to AL.
+        MOV [RDI], AL ;Move char from AL to location pointer to at RDI+RCX.
+        DEC RDI ;Move pointer foward by one byte.
+        INC RCX ;Move counter forward by one.
+        CMP RCX, [RBP-16] ;Check if RCX hasn't hit the pointer at RSI yet.
+        JL .loop
+   
+    MOV RAX, RBX ;Move our new string into RAX
+    ADD RSP, reverseString.STACK_INIT
     MOV RSP, RBP
     POP RBP
     RET
@@ -116,7 +128,8 @@ reverseString:
 global _start
 _start:
     %DEFINE _start.STACK_INIT 16 ;Defining this so we aren't placing a literal every time we need to empty the stack at the end.
-    ;Setting up stack frame. Prolog.
+    ;Setting up stack frame. Prolog. Also move argc into RAX
+    MOV RAX, [RSP]
     PUSH RBP
     MOV RBP, RSP
     ;Allocating space on the stack for variables
@@ -124,22 +137,12 @@ _start:
     MOV QWORD [RBP-8], 0 ;Length of text, 8 bytes.
     MOV QWORD [RBP-16], 0 ;New String PTR, 8 bytes.
 
-    MOV RAX, [RBP+8] ;Move argc from stack to RAX.
-    JMP [argcDispatch+RAX*8]
-    
-    argcDispatch:
-    ;Jump table
-        dq noInput ;argcDispatch+0*8
-        dq input ;argcDispatch+1*8
+    CMP RAX, 1
+    JE noInput
+    CMP RAX, 2
+    JE input
     
     noInput:
-        ;Print empty string
-        MOV RAX, SYS_WRITE
-        MOV RDI, STDOUT
-        MOV RSI, empty.txt
-        MOV RDX, empty.len
-        SYSCALL
-
         ;Epilog
         ADD RSP, _start.STACK_INIT
         MOV RSP, RBP
@@ -151,23 +154,27 @@ _start:
         SYSCALL
         
     input:
-        MOV RAX, [RBP+16] ;MOV arg1 to RAX
-        CALL strlen ;Call our string length function
+        ;Checking if input is empty; if it is empty, jump to noInput.
+        ;MOV AL, 0
+        ;MOV RBX, [RSP+24]
+        ;CMP [RBX], AL
+        ;JE noInput
+        MOV RDI, [RBP+24] ;MOV arg1 to RDI.
+        CALL strlen ;Call our string length function.
+        MOV [RBP-8], RAX ;Move string length result into RSP-8.
 
-        MOV [RBP-8], RAX ;Move string length result into RSP-8
-
-        MOV RDI, [RBP+16]
+        MOV RDI, [RBP+24]
         MOV RSI, RAX
         CALL reverseString
-        MOV [RBP-16], RAX ;Moving return pointer into RBP-16
+        MOV [RBP-16], RAX ;Moving return pointer into RBP-16.
+        MOV RSI, RAX ;Moving new string pointer to RSI for SYS_WRITE.
 
         MOV RAX, SYS_WRITE
         MOV RDI, STDOUT
-        MOV RSI, [RBP-16] ;Moving string pointer into RSI
-        MOV RDX, [RBP-8] ;Moving string length into RDX
+        ;RSI already handled earlier.
+        MOV RDX, [RBP-8] ;Moving string length into RDX.
         SYSCALL
-
-        ;Equivalent to *reversedString = NULL; in C
+        ;Deallocating memory.
         MOV RAX, SYS_MUNMAP
         MOV RDI, [RBP-16] ;Move string pointer into RDI to be unmapped.
         MOV RSI, [RBP-8] ;Move amount of memory to be unmapped in bytes.
@@ -182,5 +189,6 @@ _start:
         MOV RAX, SYS_EXIT
         XOR RDI, RDI
         SYSCALL
+
     
     
