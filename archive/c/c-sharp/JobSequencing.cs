3ad66999c17df568474cf002b581230c472c679f
alstondsouza1 <161771038+alstondsouza1@users.noreply.github.com>
2024-11-10 01:42:58 +0000

    Initializing job and jobSequencing classes

---
+++ archive/c/c-sharp/JobSequencing.cs
@@ -0,0 +1,17 @@
+using System;
+using Systems.Collections.Generic;
+
+class Job 
+{
+    public char Id { get; set; }
+    public int Deadline { get; set; }
+    public int Profit { get; set;}
+}
+
+class JobSequencing
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Job Sequencing Program");
+    }
+}
