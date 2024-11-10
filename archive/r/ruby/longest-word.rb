2465e1c100be5b1b16b763735e3555b1667b9e5d
arcjun01 <arciaga.juno@student.greenriver.edu>
2024-11-10 20:06:26 +0000

    Refactored the code: focused steps to make code more understandable.

--- archive/r/ruby/longest-word.rb
+++ archive/r/ruby/longest-word.rb
@@ -1,10 +1,21 @@
 def longest_word(sentence)
     # Converts string into an array of words
     words = sentence.split(" ")
-    # Will find the word the has the most length
-    words.max_by { |word| word.length }
+
+    # Stores the longest word
+    longest_word = ""
+
+    # Iterate over each word
+    words.each do |word|
+        if word.length > longest_word.length
+            # Update the longest word
+            longest_word = word
+        end
+    end
+    return longest_word
 end
 
 # Test and Output
-sentence = ("Swiftly running foxes")
-puts longest_word(sentence)
+sentence = "Swiftly running foxes"
+output = longest_word(sentence)
+puts "Longest word: #{output}" 
