118b5ece48991150ef1b3eb6b7ae018dbf4aa340
alstondsouza1 <161771038+alstondsouza1@users.noreply.github.com>
2024-11-10 01:47:18 +0000

    added constructor to job class

--- archive/c/c-sharp/JobSequencing.cs
+++ archive/c/c-sharp/JobSequencing.cs
@@ -6,6 +6,13 @@ class Job
     public char Id { get; set; }
     public int Deadline { get; set; }
     public int Profit { get; set;}
+
+    public Job(char id, int deadline, int profit)
+    {
+        Id = id;
+        Deadline = deadline;
+        Profit = profit;
+    }
 }
 
 class JobSequencing
