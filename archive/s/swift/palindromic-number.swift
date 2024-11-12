8f8f85cfe466827d59f158b3e555d9ab73da2f7a
Kaalid <87632617+KaalidArare@users.noreply.github.com>
2024-11-12 10:32:30 +0000

    making aditional changes in swift

--- archive/s/swift/palindromic-number.swift
+++ archive/s/swift/palindromic-number.swift
@@ -1,7 +1,18 @@
-func isPalindromeNumber(number : Int) -> Bool {
+func isPalindromeNumber(input : Int) -> Bool {
+    if let number = Int(input) {
+
+    if number < 0 {
+        return false
+    }
+
     var numStr = String(number)
     var reversedStr = String(numStr.reversed())
     return numStr == reversedStr
+    
+    } else {
+        print("Error: Input is not a valid Number!")
+        return false;
+    }
 }
 
 print(isPalindromeNumber(number: 434))
