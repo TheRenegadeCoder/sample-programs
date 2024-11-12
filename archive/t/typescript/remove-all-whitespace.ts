cb60dfc50ac6b322f363165256cd0793c6fc8ee0
alstondsouza1 <161771038+alstondsouza1@users.noreply.github.com>
2024-11-12 18:14:19 +0000

    added comments

--- archive/t/typescript/remove-all-whitespace.ts
+++ archive/t/typescript/remove-all-whitespace.ts
@@ -1,16 +1,20 @@
 function removeAllWhitespace(input: string): string {
+    // remove all whitespace from the string
     return input.replace(/\s+/g, "");
 }
 
 function main() {
+    // checking ig there is an input provided
     if (process.argv.length < 3 || process.argv[2].trim() === "") {
         console.log("Usage: please provide a non-empty string");
         return;
     }
 
+    // retrieve input and remove whitespace
     const input = process.argv[2];
     const result = removeAllWhitespace(input);
     console.log(result);
 }
 
+// run the program
 main();
