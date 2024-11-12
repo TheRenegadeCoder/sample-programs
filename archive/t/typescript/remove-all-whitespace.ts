dd2b19a5caa0dde7b70cd6f7fc4ddd563ffa0abe
alstondsouza1 <161771038+alstondsouza1@users.noreply.github.com>
2024-11-12 18:34:02 +0000

    added comments to the code

--- archive/t/typescript/remove-all-whitespace.ts
+++ archive/t/typescript/remove-all-whitespace.ts
@@ -1,18 +1,27 @@
 function removeAllWhitespace(input: string): string {
-    // remove all whitespace from the string
+    // regu;ar expression to replace all whitespace character with an empty string
     return input.replace(/\s+/g, "");
 }
 
 function main() {
-    // checking ig there is an input provided
+    // check if there is an input provided, and if its not an empty string
     if (process.argv.length < 3 || process.argv[2].trim() === "") {
         console.log("Usage: please provide a non-empty string");
         return;
     }
 
-    // retrieve input and remove whitespace
     const input = process.argv[2];
+    
+    // check if the input is only whitespace
+    if (!/\S/.test(input)) {
+        console.log("Result: The input is whitespace only.");
+        return;
+    }
+
+    // remove all whitespace from the input
     const result = removeAllWhitespace(input);
+
+    // output for result.
     console.log(`Result without whitespace: "${result}"`);
 }
 
