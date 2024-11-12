bd1fd8ee73bab3a20b0fe5bf25711b80d2c411cc
Tav Singh <147795944+TavSingh@users.noreply.github.com>
2024-11-12 05:39:58 +0000

    Found a simplier way, all tests passed.

--- archive/r/r/longest-word.R
+++ archive/r/r/longest-word.R
@@ -1,8 +1,12 @@
 longest_word_length <- function(input_string) {
   # Checking input
   args <- commandArgs(trailingOnly = TRUE)
+  
   if (length(args) == 0 || args == "") {
-  cat("Usage: please provide a string\n")
+    cat("Usage: please provide a string\n")
+  } else {
+    # Split the string by whitespace and find the maximum word length in one line
+    cat(max(nchar(unlist(strsplit(args[1], "[ \t\n\r]+")))), "\n")
   }
 }
 
