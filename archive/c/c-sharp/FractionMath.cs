e714cf33cb855ab9f013aec11e34ad1540035e16
Alexander Ruban <131919965+AlexanderORuban@users.noreply.github.com>
2024-11-12 17:07:15 +0000

    Added operator methods.

--- archive/c/c-sharp/FractionMath.cs
+++ archive/c/c-sharp/FractionMath.cs
@@ -48,21 +48,128 @@ namespace SamplePrograms
             }
         }
 
-        public FractionMath StringToFraction(string fractionString) {
+        public FractionMath StringToFraction(string fractionString)
+        {
             string[] numbers = fractionString.Split('/');
 
-            if (numbers.Length != 2) {
+            if (numbers.Length != 2)
+            {
                 throw new FormatException("Invalid fraction. A format of 'numerator/denominator' is expected.");
-            } 
+            }
 
             int numerator = int.Parse(numbers[0]);
             int denominator = int.Parse(numbers[1]);
-            
+
             if (denominator == 0)
             {
                 throw new ArgumentException("Denominator cannot be zero.");
             }
 
-            return new FractionMath(numerator, denominator);
+            return FractionMath(numerator, denominator);
+        }
+
+        public FractionMath Add(FractionMath f1, FractionMath f2)
+        {
+            int newNumerator = f1.numerator * f2.denominator + f2.numerator * f1.denominator;
+            int newDenominator = f1.denominator * f2.denominator;
+            return FractionMath(newNumerator, newDenominator);
+        }
+
+        public FractionMath Subtract(FractionMath f1, FractionMath f2)
+        {
+            int newNumerator = f1.numerator * f2.denominator - f2.numerator * f1.denominator;
+            int newDenominator = f1.denominator * f2.denominator;
+            return FractionMath(newNumerator, newDenominator);
+        }
+
+        public FractionMath Multiply(FractionMath f1, FractionMath f2)
+        {
+            int newNumerator = f1.numerator * f2.numerator;
+            int newDenominator = f1.denominator * f2.denominator;
+            return FractionMath(newNumerator, newDenominator);
+        }
+
+        public FractionMath Divide(FractionMath f1, FractionMath f2)
+        {
+            int newNumerator = f1.numerator * f2.denominator;
+            int newDenominator = f1.denominator * f2.numerator;
+            return FractionMath(newNumerator, newDenominator);
+        }
+
+        public int EqualsTo(FractionMath f1, FractionMath f2)
+        {
+            if (f1.numerator * f2.denominator == f1.denominator * f2.numerator)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int GreaterThan(FractionMath f1, FractionMath f2)
+        {
+            if (f1.numerator * f2.denominator > f1.denominator * f2.numerator)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int LessThan(FractionMath f1, FractionMath f2)
+        {
+            if (f1.numerator * f2.denominator < f1.denominator * f2.numerator)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int GreaterThanEquals(FractionMath f1, FractionMath f2)
+        {
+            if (f1.numerator * f2.denominator >= f1.denominator * f2.numerator)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int LessThanEquals(FractionMath f1, FractionMath f2)
+        {
+            if (f1.numerator * f2.denominator <= f1.denominator * f2.numerator)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public int NotEquals(FractionMath f1, FractionMath f2)
+        {
+            if (f1.numerator * f2.denominator != f1.denominator * f2.numerator)
+            {
+                return 1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            
         }
     }
