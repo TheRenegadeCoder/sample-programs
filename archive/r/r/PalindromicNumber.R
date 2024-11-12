6756c2c921be3b12df32fb4918d25865e9a2c3e3
Kaalid <87632617+KaalidArare@users.noreply.github.com>
2024-11-12 10:00:12 +0000

    Adding A palindromi-number to swift

--- archive/r/r/PalindromicNumber.R
+++
@@ -1,6 +0,0 @@
-def palindromic?(number)
-  number.to_s == number.to_s.reverse
-end 
-
-puts palindromic?(326)  
-puts palindromic?(545)  
---
+++ archive/s/swift/palindromic-number.swift
@@ -0,0 +1,4 @@
+func isPalindromeNumber(number : Int) -> bool {
+    var numStr = String(number)
+    
+}
