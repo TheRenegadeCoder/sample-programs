cf699c31d36a0c9082831160ad48b8798d9bbbb4
Kaalid <87632617+KaalidArare@users.noreply.github.com>
2024-11-12 10:05:22 +0000

    Finalized Palindromic-number in swift

--- archive/s/swift/palindromic-number.swift
+++ archive/s/swift/palindromic-number.swift
@@ -1,4 +1,8 @@
 func isPalindromeNumber(number : Int) -> bool {
     var numStr = String(number)
-    
+    var reversedStr = String(numStr.reversed())
+    return numStr == reversedStr
 }
+
+print(isPalindromeNumber(number: 434))
+print(isPalindromeNumber(number: 322))
