f17591ce7e6fa18dc13cc7247771a569f1880549
Tav Singh <147795944+TavSingh@users.noreply.github.com>
2024-11-12 05:34:49 +0000

    All tests successful, finding shorter way.

--- archive/r/r/longest-word.R
+++ archive/r/r/longest-word.R
@@ -2,21 +2,7 @@ longest_word_length <- function(input_string) {
   # Checking input
   args <- commandArgs(trailingOnly = TRUE)
   if (length(args) == 0 || args == "") {
-    cat("Usage: please provide a string\n")
-  } else {
-    # Get the input string
-    input_string <- args[1]
-    
-    # Split the input string by spaces, tabs, newlines, and carriage returns
-    words <- unlist(strsplit(input_string, "[ \t\n\r]+"))
-    
-    # Find the length of the longest word
-    if (length(words) == 0) {
-      cat("Usage: please provide a string\n")
-    } else {
-      longest_word_length <- max(nchar(words))
-      cat(longest_word_length, "\n")
-    }
+  cat("Usage: please provide a string\n")
   }
 }
 
