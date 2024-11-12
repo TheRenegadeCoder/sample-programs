e73e888ea3addd13786198b7a2ab1d696d8a0ebb
Tav Singh <147795944+TavSingh@users.noreply.github.com>
2024-11-12 01:59:54 +0000

    Adding function

--- archive/r/r/longest-word.R
+++ archive/r/r/longest-word.R
@@ -1,2 +1,13 @@
 #Given a string, this program should break it up into words and determine the length of the longest word.
 #In this case, a word is defined as anything surrounded by whitespace.
+
+find_longest_word <- function(sentence) {
+  # Split the sentence into words
+  words <- unlist(strsplit(sentence, "\\s+"))
+  
+  # Find the longest word
+  longest_word <- words[which.max(nchar(words))]
+  
+  # Return the longest word
+  return(longest_word)
+}
