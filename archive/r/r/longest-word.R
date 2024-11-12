34888f27ee275450498f83b15714a81cb7f45ec7
Tav Singh <147795944+TavSingh@users.noreply.github.com>
2024-11-12 03:05:45 +0000

    Continuing program

--- archive/r/r/longest-word.R
+++ archive/r/r/longest-word.R
@@ -1,13 +1,20 @@
 #Given a string, this program should break it up into words and determine the length of the longest word.
 #In this case, a word is defined as anything surrounded by whitespace.
 
-find_longest_word <- function(sentence) {
-  # Split the sentence into words
-  words <- unlist(strsplit(sentence, "\\s+"))
+find_longest <- function(string) {
+    words <- strsplit(string, "\\s+")
+    
+    longest_word <- ""
+    for (word in words) {
+        if (nchar(word) > nchar(longest_word)) {
+            longest_word <- word
+        }
+    }
   
-  # Find the longest word
-  longest_word <- words[which.max(nchar(words))]
-  
-  # Return the longest word
   return(longest_word)
 }
+
+# Example usage
+sentence <- "This is a sample sentence to find the longest word."
+longest_word <- find_longest(sentence)
+print(longest_word)
