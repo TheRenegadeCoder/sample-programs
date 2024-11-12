bbd5f4453af85eddf15ffa280d4b726df6471aaf
Alexander Ruban <131919965+AlexanderORuban@users.noreply.github.com>
2024-11-12 16:35:04 +0000

    Added exception handling to FractionMath, Finished GCD, and created both Simplify and StringToFraction.

--- archive/c/c-sharp/FractionMath.cs
+++ archive/c/c-sharp/FractionMath.cs
@@ -4,21 +4,65 @@ namespace SamplePrograms
     {
         private int numerator;
         private int denominator;
-        
-        public FractionMath(int numerator, int denominator) {
-            if (denominator == 0) {
-                System.Console.WriteLine("Denominator cannot be zero"); // Look into different types of exceptions
+
+        public FractionMath(int numerator, int denominator)
+        {
+            if (denominator == 0)
+            {
+                throw new ArgumentException("Denominator cannot be zero.");
             }
-            
+
             this.numerator = numerator;
             this.denominator = denominator;
 
-            // Make sure to simplify the fraction using GCD
+            Simplify();
         }
-        
-        private int GCD(int x, int y) {
-            // look into creating the GCD method
+
+        // GCD method using the Euclidean algorithm in an iterative approach
+        // Orginal algorithm was found on GeeksforGeeks, modified for clarity:
+        // https://www.geeksforgeeks.org/program-to-find-gcd-or-hcf-of-two-numbers/#
+        private int GCD(int x, int y)
+        {
+
+            while (y != 0)
+            {
+                int z = x;
+                x = y;
+                y %= z;
+            }
+
+            return x;
         }
-        
+
+        private void Simplify()
+        {
+            int gcd = GCD(numerator, denominator);
+
+            numerator /= gcd;
+            denominator /= gcd;
+
+            if (denominator < 0)
+            {
+                numerator = -numerator;
+                denominator = -denominator;
+            }
+        }
+
+        public FractionMath StringToFraction(string fractionString) {
+            string[] numbers = fractionString.Split('/');
+
+            if (numbers.Length != 2) {
+                throw new FormatException("Invalid fraction. A format of 'numerator/denominator' is expected.");
+            } 
+
+            int numerator = int.Parse(numbers[0]);
+            int denominator = int.Parse(numbers[1]);
+            
+            if (denominator == 0)
+            {
+                throw new ArgumentException("Denominator cannot be zero.");
+            }
+
+            return new FractionMath(numerator, denominator);
+        }
     }
-}
