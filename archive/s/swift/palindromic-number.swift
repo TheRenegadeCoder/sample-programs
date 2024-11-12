2b4c83764073a197589d4c6c29e82254e934bc7f
Kaalid <87632617+KaalidArare@users.noreply.github.com>
2024-11-12 20:44:46 +0000

    corrections 5

--- archive/s/swift/palindromic-number.swift
+++ archive/s/swift/palindromic-number.swift
@@ -1,36 +1,32 @@
-func isPalindrome(input: String) -> Bool {
-    if input.isEmpty {
-        return false  // No output for empty input
-    }
+import Foundation
 
-    if let number = Int(input) {
-        if number < 0 {
-            print("Usage: please input a non-negative integer")
-            return false
-        }
-        
-        if input.count == 1 {
-            return true;
-        }
-    } else {
-        // Handles floats, letters, and other non-integer inputs
-        print("Usage: please input a non-negative integer")
-        return false
-    }
-
-    let numStr = input
-    let reversedStr = String(numStr.reversed())
-
-    return numStr == reversedStr
+func isPalindrome(_ input: String) -> Bool {
+    return input == String(input.reversed())
 }
 
-// Test cases
-print(isPalindrome(input: "5"))      // returns true
-// print(isPalindrome(input: "2442"))   // Returns true
-// print(isPalindrome(input: "232"))    // Returns true
-print(isPalindrome(input: "5215"))   // Returns false
-print(isPalindrome(input: "521"))    // Returns false
-print(isPalindrome(input: ""))       // No output
-print(isPalindrome(input: "a"))      // Outputs usage message
-// print(isPalindrome(input: "-7"))     // Outputs usage message
-// print(isPalindrome(input: "5.41"))   // Outputs usage message
+func main() {
+    let arguments = CommandLine.arguments
+
+    guard arguments.count == 2 else {
+        print("Usage: please input a non-negative integer")
+        return
+    }
+
+    let input = arguments[1]
+    
+    if let number = Int(input), number >= 0 {
+        
+    
+        // Check if the input is a palindrome
+        if isPalindrome(input) {
+            print("true")
+        } else {
+            print("false")
+        }
+    } else {
+        // If the input is not a valid positive integer
+        print("Usage: please input a non-negative integer")
+    }
+}
+
+main()
