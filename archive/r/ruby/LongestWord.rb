6599f77e9d2b58779454f18bb99e6c10183281d4
BahramT1 <142144715+BahramT1@users.noreply.github.com>
2024-11-13 06:46:24 +0000

    Starting index with and then going through each word

--- archive/r/ruby/LongestWord.rb
+++ archive/r/ruby/LongestWord.rb
@@ -3,4 +3,18 @@ def longest_word_length(input)
     # Split the input by spaces
     words = input.split(" ")  
 
+    # Start with 0 as the longest length
+    max_length = 0        
+
+    # Go through each word
+    words.each do |word|       
+      if word.length > max_length
+        max_length = word.length
+      end
+    end
+    
+    # Return the length of the longest word
+    max_length                 
+  end
+  
   
