40cf4ff6a46132fe06aa227d8efeec25fb54226d
JesseChum <142645297+JesseChum@users.noreply.github.com>
2024-11-13 06:41:53 +0000

    solved most errors

--- archive/f/f-sharp/RemoveAllWhitespace.fs
+++ archive/f/f-sharp/RemoveAllWhitespace.fs
@@ -4,15 +4,18 @@ let removeAllWhitespace (input: string) : string =
     input |> Seq.filter (fun c -> not (Char.IsWhiteSpace(c))) |> String.Concat
 
 [<EntryPoint>]
-let main argv =
+let main argv : int = 
+    let ret : ref = 0
     if argv.Length > 0 then
         let input = argv.[0]
-        
+        printfn "Input: %s" input
         if  input = "" then
-            printfn "Usage: please provide a string"
+            printfn "Usage: please provide a string " 
+            ret
         else
             let result = removeAllWhitespace input      
-            printfn "%s" result
+            printfn "Result: %s" result
+            ret
     else
-        printfn "Usage: please provide a string"
-    
+          printfn "Usage: please provide a string " 
+          ret 
