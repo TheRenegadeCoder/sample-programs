514b8f774c79c7068982e3084ce1b5fc0268fade
BahramT1 <142144715+BahramT1@users.noreply.github.com>
2024-11-13 06:53:50 +0000

    Check if the program has received input from the command line

--- archive/r/ruby/LongestWord.rb
+++ archive/r/ruby/LongestWord.rb
@@ -16,5 +16,13 @@ def longest_word_length(input)
     # Return the length of the longest word
     max_length                 
   end
+
+  # Check if the program has received input from the command line
+  if ARGV.length > 0
+    input_string = ARGV.join(" ")
+    puts longest_word_length(input_string)
+  else
+    puts "Please provide an input string."
+  end
   
   
