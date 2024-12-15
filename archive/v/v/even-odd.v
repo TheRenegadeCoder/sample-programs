5f86bd41913eb7afa5c6a5de526dade3da72fda9
Kaamkiya <ziam3.1415@gmail.com>
2024-12-15 15:38:08 -0500

    fix: Parse integers correctly

--- archive/v/v/even-odd.v
+++ archive/v/v/even-odd.v
@@ -1,16 +1,22 @@
 module main
 
 import os
+import strconv
 
 fn main() {
 	if os.args.len < 2 {
-		eprintln('Usage: please input a number')
+		println('Usage: please input a number')
 		exit(1)
 	}
 
-	print(match os.args[1].i64() % 2 {
-		i64(0) { 'Even' }
-		i64(1) { 'Odd' }
+	num := strconv.atoi(os.args[1]) or {
+		println('Usage: please input a number')
+		exit(1)
+	}
+
+	print(match num % 2 {
+		0 { 'Even' }
+		1 { 'Odd' }
 		else { 'Usage: please input a number' }
 	})
 }
