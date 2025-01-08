66bec06d866eb83cc40c20cd0fc44750bd8605f4
Kaamkiya <ziam3.1415@gmail.com>
2025-01-08 10:14:23 -0500

    fix: Fix length

--- archive/z/zig/fizz-buzz.zig
+++ archive/z/zig/fizz-buzz.zig
@@ -3,7 +3,7 @@ const std = @import("std");
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
     var i: usize = 1;
-    while (i <= 16) : (i += 1) {
+    while (i <= 100) : (i += 1) {
         if (i % 15 == 0) {
             try stdout.writeAll("FizzBuzz\n");
         } else if (i % 3 == 0) {
