ac09f2fb964a67a42332281c51d1baa0525091ba
rzuckerm <ron.zuckerman@adtran.com>
2025-01-11 18:58:01 -0600

    Clean up formatting

--- archive/c/cobol/baklava.cbl
+++ archive/c/cobol/baklava.cbl
@@ -2,18 +2,18 @@
         PROGRAM-ID. BAKLAVA.
         DATA DIVISION.
         WORKING-STORAGE SECTION.
-          01 BAKLAVA-VARIABLES.
-            03 NUM            PIC S9(2).
-            03 NUM-SPACES     PIC S9(2).
-            03 NUM-STARS      PIC S9(2).
-          01 REPEAT-VARIABLES.
-            03 NUM-REPEATS    PIC S9(2).
-            03 REPEAT-CHAR    PIC X(1).
+            01  BAKLAVA-VARIABLES.
+                03  NUM             PIC S9(2).
+                03  NUM-SPACES      PIC S9(2).
+                03  NUM-STARS       PIC S9(2).
+            01  REPEAT-VARIABLES.
+                03  NUM-REPEATS     PIC S9(2).
+                03  REPEAT-CHAR     PIC X(1).
 
         PROCEDURE DIVISION.
             PERFORM VARYING NUM FROM 0 BY 1 UNTIL NUM > 20
                 COMPUTE NUM-SPACES = FUNCTION ABS(NUM - 10)
-                COMPUTE NUM-STARS = 20 - 2 * NUM-SPACES
+                COMPUTE NUM-STARS = 21 - 2 * NUM-SPACES
 
       * Display NUM-SPACES " "
                 MOVE " " TO REPEAT-CHAR
@@ -26,7 +26,7 @@
                 PERFORM DISPLAY-REPEAT-STRING
 
       * Display newline
-                DISPLAY "*"
+                DISPLAY ""
             END-PERFORM
             STOP RUN.
 
