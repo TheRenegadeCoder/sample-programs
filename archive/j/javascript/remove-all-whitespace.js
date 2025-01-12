1aba2fe85f20b8626d2ff08ebe3f08f205a3e09c
Kaamkiya <ziam3.1415@gmail.com>
2025-01-12 15:31:18 -0500

    feat: Use ternary operator instead of if statement

--- archive/j/javascript/remove-all-whitespace.js
+++ archive/j/javascript/remove-all-whitespace.js
@@ -7,5 +7,6 @@ if (process.argv.length != 3) {
   die();
 }
 
-if (process.argv[2].length > 0) console.log(process.argv[2].replaceAll(/\s/g, ''));
-else die();
+process.argv[2].length > 0
+  ? console.log(process.argv[2].replaceAll(/\s/g, ''))
+  : die();
