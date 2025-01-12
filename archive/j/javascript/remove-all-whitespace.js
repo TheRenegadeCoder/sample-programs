4a7416fc2a2b8a050339f9efd73b4b9f65837122
Kaamkiya <ziam3.1415@gmail.com>
2025-01-12 15:03:06 -0500

    feat: Add Remove All Whitespace in JavaScript

---
+++ archive/j/javascript/remove-all-whitespace.js
@@ -0,0 +1,11 @@
+const die = () => {
+  console.log("Usage: please provide a string");
+  process.exit(1);
+}
+
+if (process.argv.length != 3) {
+  die();
+}
+
+if (process.argv[2].length > 0) console.log(process.argv[2].replaceAll(/\s+/g, ''));
+else die();
