f294f2115ca0ba64de547d04e0983706fddc951f
Matthew Larsen <174657206+mlarsen-source@users.noreply.github.com>
2025-02-12 09:06:55 +0000

    initial draft, all tests passed

--- archive/k/kotlin/LinearSearch.kt
+++ archive/k/kotlin/LinearSearch.kt
@@ -1,6 +1,9 @@
 /*
 
-Linear search is quite intuitive, it is basically searching an element in an array by traversing the array from the beginning to the end and comparing each item in the array with the key. If a particular array entry matches with the key the position is recorded and the loop is stopped. The algorithm for this is:
+Linear search is quite intuitive, it is basically searching an element in an array by traversing 
+the array from the beginning to the end and comparing each item in the array with the key. If a 
+particular array entry matches with the key the position is recorded and the loop is stopped. 
+The algorithm for this is:
 
 Define a flag (set it's value to 0) for checking if key is present in array or notation.
 Iterate through every element in array.
@@ -10,3 +13,39 @@ If entire loop is traversed and the element is not found the value of flag will 
 
 */
 
+
+fun main(args: Array<String>)  
+{
+    // store usage message in variable
+    val message = "Usage: please provide a list of integers (\"1, 4, 5, 11, 12\") and the integer to find (\"11\")"
+       
+    // validate input array is correct size and does not contain empty Strings 
+    if(args.size !=2 || args[0].isBlank() || args[1].isBlank())
+    {
+        println(message)
+        return
+    } 
+    
+    // convert list of numbers string input into an int array, invalid entries converted to null
+    val intArray = args[0].split(",").map { it.trim().toIntOrNull() }
+
+    // check if array contains null (invalid) elements
+    if(null in intArray)
+    {
+        println(message)
+        return
+    }
+    
+    // convert key string input into an int, invalid entries converted to null
+    val key = args[1].toIntOrNull()
+ 
+    // check if key is null (invalid)
+    if(key == null)
+    {
+        println(message)
+        return
+    }
+
+    // check if key is in the array and print returned boolean   
+    println(key in intArray)
+}
