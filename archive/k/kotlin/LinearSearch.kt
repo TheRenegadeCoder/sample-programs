6c32ba9a5e8e3d4680aefc6a1efa7ab9ce9c2e15
Matthew Larsen <174657206+mlarsen-source@users.noreply.github.com>
2025-02-12 06:37:50 +0000

    initial commit

---
+++ archive/k/kotlin/LinearSearch.kt
@@ -0,0 +1,12 @@
+/*
+
+Linear search is quite intuitive, it is basically searching an element in an array by traversing the array from the beginning to the end and comparing each item in the array with the key. If a particular array entry matches with the key the position is recorded and the loop is stopped. The algorithm for this is:
+
+Define a flag (set it's value to 0) for checking if key is present in array or notation.
+Iterate through every element in array.
+In each iteration compare the key and the current element.
+If they match set the flag to 1, position to the current iteration and break from the loop.
+If entire loop is traversed and the element is not found the value of flag will be 0 and user can notified that key is not in array.
+
+*/
+
