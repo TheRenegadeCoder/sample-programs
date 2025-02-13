9fbd9050a73b7291d08ce0cf34c8718ab25ba8bb
Tim W <142520824+timmyd4@users.noreply.github.com>
2025-02-13 01:05:32 +0000

    Fixed to pass tests

--- archive/p/perl/linear-search.pl
+++ archive/p/perl/linear-search.pl
@@ -3,7 +3,7 @@ my $arguments_used = scalar @ARGV;
 
 if($arguments_used < 2)
 {
-    die "Usage: please provide a target and a list e.g (\"1,2,3,4,5\" 5)";
+    die "Usage: please provide a list of integers (\"1, 4, 5, 11, 12\") and the integer to find (\"11\")";
 }
 
 my @input_array = @ARGV;
@@ -11,9 +11,9 @@ my $input_string = pop @ARGV;
 my @trimmed_string = split(',', @input_array[0]);
 my $found_it = 0;
 
-if(scalar@trimmed_string < 2)
+if(scalar@trimmed_string < 1)
 {
-    die "Usage: please provide atleast 2 elements to the lis e.g (\"1,2\" 2)"
+    die "Usage: please provide a list of integers (\"1, 4, 5, 11, 12\") and the integer to find (\"11\")";
 }
 
 for(my $i = 0; $i < scalar(@trimmed_string); $i++)
