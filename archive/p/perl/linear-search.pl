0b28953aaa9ba36d9656b7b7c9ca5b8c85669443
Tim W <142520824+timmyd4@users.noreply.github.com>
2025-02-13 00:25:18 +0000

    Added logic for correct input

---
+++ archive/p/perl/linear-search.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl
+my $arguments_used = scalar @ARGV;
+
+if($arguments_used < 2)
+{
+    die "Usage: please provide a target and a list e.g (\"1,2,3,4,5\" 5)";
+}
+
+my $input_string = shift @ARGV;
+my @input_array = @ARGV;
+my @trimmed_string = split(',', @input_array[0]);
