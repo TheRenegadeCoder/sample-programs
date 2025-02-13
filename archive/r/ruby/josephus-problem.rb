0429fe03e7589563f0091ab7277062fbaee71d73
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-02-13 01:37:04 +0000

    Josephus function attempt WIP

--- archive/r/ruby/josephus-problem.rb
+++ archive/r/ruby/josephus-problem.rb
@@ -1,25 +1,39 @@
-def josephus_problem(array, k, index)
+def josephus_problem(array, k, start)
     
+    if (array.length == 1)
+      # if the array is one item left, return the survivor value
+      return array[0]
+    else
+        # set the index position (start + (k - 1)) and set wrap with %
+        index = (start + k - 1)
+        wrap = index % array.length
+        array.delete_at(wrap)
+        return josephus_problem(array, k, wrap)
+    end
 end
 
 def error()
-  puts "Usage: please input the total number of people and number of people to skip."
+    puts "Usage: please input the total number of people and number of people to skip."
 end
 
+# check for incorrect number of arguments
 if ARGV.length != 2
-  error()
+    error
+else
+    # check for letters or empty strings in each argument
+    ARGV.each do |arg|
+        if arg.match(/[a-zA-Z]/) || arg.empty?
+            error
+        else
+            # convert the arguments to integers
+            n = ARGV[0].to_i
+            k = ARGV[1].to_i
+
+            # create an array with a length of argument n
+            array = [*1..n]
+
+            # pass our array to the function with k and our start index (0)
+            josephus_problem(array, k, 0)
+        end
+    end
 end
-
-ARGV.each do |arg|
-  if arg.match(/[a-zA-Z]/) || arg.empty? || 
-    error()
-  else
-    num1 = ARGV[0].to_i
-    num2 = ARGV[1].to_i
-
-    array = [*1..num1]
-
-    josephus_problem(array, num2, 0)
-  end
-end
-
