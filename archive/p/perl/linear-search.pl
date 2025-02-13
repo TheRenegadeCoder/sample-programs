c85469f304c374be924538f07a6bc9603f0f956f
Tim W <142520824+timmyd4@users.noreply.github.com>
2025-02-13 00:57:36 +0000

    Fixed input orientation, added more detail to error output

--- archive/p/perl/linear-search.pl
+++ archive/p/perl/linear-search.pl
@@ -6,14 +6,14 @@ if($arguments_used < 2)
     die "Usage: please provide a target and a list e.g (\"1,2,3,4,5\" 5)";
 }
 
-my $input_string = shift @ARGV;
 my @input_array = @ARGV;
+my $input_string = pop @ARGV;
 my @trimmed_string = split(',', @input_array[0]);
 my $found_it = 0;
 
 if(scalar@trimmed_string < 2)
 {
-    die "Usage: please provide atleast 2 elements to the lis e.g (\"1,2\")"
+    die "Usage: please provide atleast 2 elements to the lis e.g (\"1,2\" 2)"
 }
 
 for(my $i = 0; $i < scalar(@trimmed_string); $i++)
