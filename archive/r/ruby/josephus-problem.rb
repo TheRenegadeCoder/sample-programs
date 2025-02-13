87b740b6f37ad1f62b250a3304b2883a599312f4
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-02-13 16:53:31 +0000

    corrected bad loop and adjusted verification section

--- archive/r/ruby/josephus-problem.rb
+++ archive/r/ruby/josephus-problem.rb
@@ -1,8 +1,7 @@
 def josephus_problem(array, k, start)
-    
     if (array.length == 1)
       # if the array is one item left, return the survivor value
-      return array[0]
+      puts array[0]
     else
         # set the index position (start + (k - 1)) and set wrap with %
         index = (start + k - 1)
@@ -17,23 +16,17 @@ def error()
 end
 
 # check for incorrect number of arguments
-if ARGV.length != 2
-    error
+# .any? loops over the elements of ARGV to check for letters or empty strings in each argument
+if ARGV.length != 2 || ARGV.any? { |arg| arg.match(/[a-zA-Z]/) || arg.empty? }
+    error()
 else
-    # check for letters or empty strings in each argument
-    ARGV.each do |arg|
-        if arg.match(/[a-zA-Z]/) || arg.empty?
-            error
-        else
-            # convert the arguments to integers
-            n = ARGV[0].to_i
-            k = ARGV[1].to_i
+    # convert the arguments to integers
+    n = ARGV[0].to_i
+    k = ARGV[1].to_i
 
-            # create an array with a length of argument n
-            array = [*1..n]
+    # create an array with a length of argument n
+    array = [*1..n]
 
-            # pass our array to the function with k and our start index (0)
-            josephus_problem(array, k, 0)
-        end
-    end
+    # pass our array to the function with k and our start index (0)
+    josephus_problem(array, k, 0)
 end
