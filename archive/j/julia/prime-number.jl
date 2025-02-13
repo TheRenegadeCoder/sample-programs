785bd70cd5f47bbf1c66b2900872836da5b2d279
Jacob Woodbury <166659344+JacobWoodbury@users.noreply.github.com>
2025-02-13 01:42:32 +0000

    Added prime-number in Julia

--- archive/j/julia/prime-number.jl
+++ archive/j/julia/prime-number.jl
@@ -2,11 +2,14 @@ function err()
     return "Usage: please input a non-negative integer"
  end
  
+
 function isPrime(num)
+    # handle 0 and 1
     if num <= 1 
         return "composite"
     end 
-    for i in 2:sqrt(num)
+    # default handler
+    for i in 2:sqrt(num) #loop starting at 2, ending at the squareroot of the input
         if (num % i == 0)
             return "composite"
         end
@@ -14,6 +17,7 @@ function isPrime(num)
     return "prime"             
 end
 
+#check for a valid input
 try
     n = parse(Int, ARGS[1])
     if n < 0
