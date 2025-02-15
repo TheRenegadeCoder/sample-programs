653a11f600ea3fe273201e2e031be5cf4ea79667
unknown <174657090+Butenkite@users.noreply.github.com>
2025-02-15 15:25:12 -0800

    Tests written - require testing

--- archive/f/fortran/fibonacci.f95
+++ archive/f/fortran/fibonacci.f95
@@ -3,15 +3,12 @@
 program fibonacci
 
 ! Create the variables
-Character(26) :: low = 'abcdefghijklmnopqrstuvwxyz'                 ! Defines all lowercase letters. This is used to later scan for letters in the input as a test.
-Character(26) :: cap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'                 ! Defines all uppercase letters. This is used to later scan for letters in the input as a test.
-integer(kind = 16) :: previousnumber, currentnumber, addednumber, uppers, lowers
+character(26) :: low = 'abcdefghijklmnopqrstuvwxyz'                 ! Defines all lowercase letters. This is used to later scan for letters in the input as a test.
+character(26) :: cap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'                 ! Defines all uppercase letters. This is used to later scan for letters in the input as a test.
+character(10) :: numbers = '0123456789'                             ! Defomes all numbers.
+integer(kind = 16) :: previousnumber, currentnumber, addednumber, uppers, lowers, nums
 character(len = 60) :: message                                      ! Defines the length of a message. This is used exclusively for the prompt.
 character(len=10) :: argument                                       ! Defines the object we will be recieving. It will arrive as a charcters as the expectation is keyboard input.
-
-! Tests to make sure we have recieved NUMBERS and not anything else. These act as flags that will later end the program.
-uppers = scan(argument, cap)    ! This will look for uppercase letters in the recieved string
-lowers = scan(argument, low)    ! This will look for lowercase letters in the recieved string
 
 ! Define the variables.
 previousnumber = 0
@@ -19,8 +16,33 @@ currentnumber = 1
 addednumber = 0
 message = "Usage: please input the count of fibonacci numbers to output"
 print *, message   ! Request from user
-read *, arguement  ! Input from user      
+read *, arguement  ! Input from user   
 
+! Tests to make sure we have recieved NUMBERS and not anything else. These act as flags that will later end the program.
+uppers = scan(argument, cap)    ! This will look for uppercase letters in the recieved string. If any letter is uppercase, uppers will be updated.
+lowers = scan(argument, low)    ! This will look for lowercase letters in the recieved string. If any letter is lowercase, lowers will be updated.
+nums = scan(arguement, numbers) ! This will look for numbers in the recieved string. If any letter is a number, the nums be updated.
+
+! Will see if there are any uppercase, if so, print an error.
+if (uppers > 0) then            
+  print *, message
+  STOP
+endif
+
+! Will see if there are any lowercase, if so, print an error.
+if (lowers > 0) then            
+  print *, message
+  STOP
+endif
+
+! Will see if there are any numbers, if there are not, print a error.
+if (nums > 0) then            
+  print *, message
+  STOP
+endif
+
+
+! If all checks pass, we will enter this section.  If non pass, we will not reach this point
 ! Enter a loop
 do i = 1, arguement
     print *, i, ": ", currentnumber                 ! Print current iteration, and current number
