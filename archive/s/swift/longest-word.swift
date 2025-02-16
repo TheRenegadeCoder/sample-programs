cca40c584c95062cb64590b4f9c5129d0948ba9b
ShawnN003 <167806527+ShawnN003@users.noreply.github.com>
2025-02-16 21:13:55 +0000

    Planning and Invalid Testing added

---
+++ archive/s/swift/longest-word.swift
@@ -0,0 +1,16 @@
+//First: split the string by spaces
+//Second: insert the string into an data structure
+//third: use the max method to find the longest length of the string
+//inside the for loop
+
+import foundation
+
+let input = CommandLine.arguments
+
+func longestWord(input) -> any
+{
+    if(input == "")
+    {
+        return "Usage: please provide a string"
+    }
+}
