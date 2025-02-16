afab484acdaedd9e4e6a0a7271da5c2c4889a09c
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-02-16 05:09:33 +0000

    working on invalidating test

--- archive/s/scala/SleepSort.scala
+++ archive/s/scala/SleepSort.scala
@@ -1,5 +1,20 @@
 object SleepSort {
-    def main(userInput: Array[String]): Unit = {
-        
+    def main(args: Array[String]): Unit = {
+        // test()
+        var tArgs: Array[String] = Array("1 2")
+      
+        println(invalidChecker(tArgs))
+    }
+    
+    def invalidChecker(args: Array[String]): Unit = {
+      if(args == null || args.isEmpty){
+        println("No Input")
+      }else if(args.length == 0){
+        println("Empty Input")
+      }else if(args.length == 1){
+        println("Invalid Input: Not A List")
+      }else if(!args.contains(",")){
+        println("Invalid Input: Wrong Format")
+      }
     }
 }
