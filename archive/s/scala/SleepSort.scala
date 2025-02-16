1c00cd3a5f7b4333c5b1cbfe2c6583c69568e7c4
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-02-16 19:50:27 +0000

    finish invalid checker

--- archive/s/scala/SleepSort.scala
+++ archive/s/scala/SleepSort.scala
@@ -1,20 +1,16 @@
 object SleepSort {
     def main(args: Array[String]): Unit = {
-        // test()
-        var tArgs: Array[String] = Array("1 2")
-      
-        println(invalidChecker(tArgs))
+      // test()
+      var tArgs: Array[String] = Array("4 5 3")
+
+      println(invalidChecker(tArgs))
     }
     
-    def invalidChecker(args: Array[String]): Unit = {
-      if(args == null || args.isEmpty){
-        println("No Input")
-      }else if(args.length == 0){
-        println("Empty Input")
-      }else if(args.length == 1){
-        println("Invalid Input: Not A List")
-      }else if(!args.contains(",")){
-        println("Invalid Input: Wrong Format")
-      }
+    def invalidChecker(args: Array[String]): String = args match{
+      case null | Array() => "No Input"
+      case arr if arr.forall(_.isEmpty) =>  "Empty Input"
+      case arr if arr.forall(_.length == 1) => "Invalid Input: Not A List"
+      case arr if !arr.exists(_.contains(",")) => "Invalid Input: Wrong Format"
+      case _ => "Valid Input" //holder for testing
     }
 }
