d0020174e666a1fdba5992f3024054194523a1d5
Tim W <142520824+timmyd4@users.noreply.github.com>
2025-02-16 09:22:03 +0000

    finished logic, now testing

--- archive/p/perl/longest-word.pl
+++ archive/p/perl/longest-word.pl
@@ -4,4 +4,13 @@ if($args eq "" || $args eq " ")
 {
     die "Usage: please provide a string\n"
 }
-
+my @storage = split(" ", $args);
+my $total = 0;
+foreach my $loop (@storage)
+{
+    if(length($loop) > $total)
+    {
+        $total = length($loop);
+    }
+}
+print $total;
