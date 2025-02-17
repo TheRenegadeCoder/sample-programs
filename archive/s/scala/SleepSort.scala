d18a7541a8af205b21e8bf63af8174d95bcde5b8
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-02-17 01:41:00 +0000

    added a non-numeric check

--- archive/s/scala/SleepSort.scala
+++ archive/s/scala/SleepSort.scala
@@ -9,8 +9,16 @@ object SleepSort {
     def invalidChecker(args: Array[String]): String = args match{
       case null | Array() => "No Input"
       case arr if arr.forall(_.isEmpty) =>  "Empty Input"
-      case arr if arr.forall(_.length == 1) => "Invalid Input: Not A List"
+      case arr if arr.forall(_.length == 1) && arr.length == 1 => "Invalid Input: Not A List"
       case arr if !arr.exists(_.contains(",")) => "Invalid Input: Wrong Format"
-      case _ => "Valid Input" //holder for testing
+      case _ => {
+        val numbers = args.flatMap(_.split(",")).map(_.trim).filter(_.nonEmpty)
+        
+          if (numbers.forall(n => n.forall(_.isDigit))) {
+            // timeRecorder(numbers.map(_.toInt))
+          } else {
+            "Invalid Input: Non-numeric Values"
+          }
+      }
     }
 }
