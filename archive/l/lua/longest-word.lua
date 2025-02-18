7362891287a0868ca9c5720b2aec3fad543642cd
alexborovets11 <Borovale002@gmail.com>
2025-02-18 07:33:54 +0000

    added code to iterate over each word and finished it

--- archive/l/lua/longest-word.lua
+++ archive/l/lua/longest-word.lua
@@ -7,3 +7,16 @@ local input = arg[1]
 
 function longest_word_length(str)
     local max_length = 0
+
+    for word in string.gmatch(str, "%S+") do
+        local length = #word  
+        if length > max_length then
+            max_length = length  
+        end
+    end
+
+    return max_length  
+end
+
+
+print(longest_word_length(input))
