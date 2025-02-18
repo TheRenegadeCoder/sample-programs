62b9e55d58074250cdf0efefb7bf0f305c97fcd3
alexborovets11 <Borovale002@gmail.com>
2025-02-18 07:40:49 +0000

    fixed a failed empty input test

--- archive/l/lua/longest-word.lua
+++ archive/l/lua/longest-word.lua
@@ -7,12 +7,19 @@ local input = arg[1]
 
 function longest_word_length(str)
     local max_length = 0
+    local found_word = false
 
     for word in string.gmatch(str, "%S+") do
+        found_word = true
         local length = #word  
         if length > max_length then
             max_length = length  
         end
+    end
+
+    if not found_word then
+        print("Usage: please provide a string")
+        os.exit(1)
     end
 
     return max_length  
