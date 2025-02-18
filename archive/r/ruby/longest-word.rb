0bf8feef2bd8e9c4a53536cd29b531db465cc61a
reetansingh <reetan15@hotmail.com>
2025-02-18 03:57:22 +0000

    added apporiarte fix for line 4 didnt have puts last 2 tests remaining are now passing

--- archive/r/ruby/longest-word.rb
+++ archive/r/ruby/longest-word.rb
@@ -1,9 +1,17 @@
 input_str = ARGV.join(" ").strip 
 
 if input_str.empty?
-    puts "please provide a string"
+   puts "Usage: please provide a string"
     exit
   end
 
   words = input_str.split
         max_length = 0
+
+        words.each do |word|
+            if word.length > max_length
+              max_length = word.length
+            end
+          end
+          
+          puts max_length
