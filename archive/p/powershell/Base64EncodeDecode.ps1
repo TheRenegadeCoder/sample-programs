31198db883fabfc71392c33c5a74efb7dd0b302b
rzuckerm <ron.zuckerman@adtran.com>
2025-05-05 19:44:37 -0500

    Fix function names to follow PowerShell naming convention

--- archive/p/powershell/Base64EncodeDecode.ps1
+++ archive/p/powershell/Base64EncodeDecode.ps1
@@ -1,37 +1,37 @@
-function Usage() {
+function Show-Usage() {
     Write-Host "Usage: please provide a mode and a string to encode/decode"
     Exit 1
 }
 
-function Encode-Base64([string]$Str) {
+function Get-EncodeBase64([string]$Str) {
     $Bytes = [Text.Encoding]::Ascii.GetBytes($Str)
     [Convert]::ToBase64String($Bytes)
 }
 
-function Decode-Base64([string]$Str) {
+function Get-DecodeBase64([string]$Str) {
     $Bytes = [Convert]::FromBase64String($Str)
     [Text.Encoding]::Ascii.GetString($Bytes)
 }
 
 if ($args.Length -lt 2 -or -not $args[1]) {
-    Usage
+    Show-Usage
 }
 
 $Mode = $args[0]
 $Str = $args[1]
 switch ($Mode) {
     "encode" {
-        $Result = Encode-Base64($Str)
+        $Result = Get-EncodeBase64($Str)
     }
     "decode" {
         try {
-            $Result = Decode-Base64($Str)
+            $Result = Get-DecodeBase64($Str)
         } catch [Management.Automation.MethodInvocationException] {
-            Usage
+            Show-Usage
         }
     }
     default {
-        Usage 
+        Show-Usage 
     }
 }
 
