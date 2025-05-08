2cd4bd7c2f252af06d8d56c2f902ff94090712ba
rzuckerm <rzuckerm@users.noreply.github.com>
2025-05-08 11:00:48 -0500

    Use single-line comment

--- archive/p/powershell/Rot13.ps1
+++ archive/p/powershell/Rot13.ps1
@@ -5,9 +5,9 @@ function Show-Usage() {
 
 function Get-Rot13([string]$Str) {
     $Result = switch -regex ($Str.ToCharArray()) {
-        "[A-Ma-m]" { [char]([byte]$_ + 13) } <# A-M, a-m -> N-Z, n-z #>
-        "[N-Zn-z]" { [char]([byte]$_ - 13) } <# N-Z, n-z -> A-M, a-m #>
-        default { $_ } <# Don't change #>
+        "[A-Ma-m]" { [char]([byte]$_ + 13) } # A-M, a-m -> N-Z, n-z
+        "[N-Zn-z]" { [char]([byte]$_ - 13) } # N-Z, n-z -> A-M, a-m
+        default { $_ } # Don't change
     }
     -join $Result
 }
