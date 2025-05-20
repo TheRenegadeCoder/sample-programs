18f4c849cd33f2ad2d9b086d49b1ff2599565f24
rzuckerm <rzuckerm@users.noreply.github.com>
2025-05-20 18:40:13 -0500

    Add Josephus Problem in PowerShell (#4741)

---
+++ archive/p/powershell/JosephusProblem.ps1
@@ -0,0 +1,36 @@
+function Show-Usage() {
+    Write-Host "Usage: please input the total number of people and number of people to skip."
+    Exit 1
+}
+
+<#
+Reference: https://en.wikipedia.org/wiki/Josephus_problem#The_general_case
+
+Use zero-based index algorithm:
+
+    g(1, k) = 0
+    g(m, k) = [g(m - 1, k) + k] MOD m, for m = 2, 3, ..., n
+
+Final answer is g(n, k) + 1 to get back to one-based index
+#>
+function Get-JosephusProblem([int]$N, [int]$K) {
+    $G = 0
+    for ($M = 2; $M -le $N; $M++) {
+        $G = ($G + $K) % $M
+    }
+
+    $G + 1
+}
+
+if ($args.Length -lt 2) {
+    Show-Usage
+}
+
+try {
+    $N = [int]::Parse($args[0])
+    $K = [int]::Parse($args[1])
+} catch {
+    Show-Usage
+}
+
+Write-Host (Get-JosephusProblem $N $K)
