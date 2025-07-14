b6090397587d9b61856bb38b5c2bcf056324cf6a
rzuckerm <rzuckerm@users.noreply.github.com>
2025-07-14 10:07:08 -0500

    Add Bubble Sort in PowerShell (#4769)

---
+++ archive/p/powershell/BubbleSort.ps1
@@ -0,0 +1,40 @@
+function Show-Usage() {
+    Write-Host 'Usage: please provide a list of at least two integers to sort in the format "1, 2, 3, 4, 5"'
+    Exit 1
+}
+
+function Parse-IntList([string]$Str) {
+    @($Str.Split(",") | ForEach-Object { [int]::Parse($_) })
+}
+
+# Source: https://en.wikipedia.org/wiki/Bubble_sort#Optimizing_bubble_sort
+function Invoke-BubbleSort([array]$Values) {
+    $n = $Values.Length
+    do {
+        $newN = 0
+        for ($i = 1; $i -lt $n; $i++) {
+            if ($Values[$i - 1] -gt $Values[$i]) {
+                $Values[$i - 1], $Values[$i] = $Values[$i], $Values[$i - 1]
+                $newN = $i
+            }
+        }
+
+        $n = $newN
+    } while ($n -gt 1)
+}
+
+if ($args.Length -lt 1 -or -not $args[0]) {
+    Show-Usage
+}
+
+try {
+    $values = Parse-IntList $args[0]
+    if ($values.Length -lt 2) {
+        Show-Usage
+    }
+} catch {
+    Show-Usage
+}
+
+Invoke-BubbleSort $values
+Write-Output ($values -join ', ')
