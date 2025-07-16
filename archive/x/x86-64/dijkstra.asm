067a78b2bffdd4b0496658c8fb97a0dc9529ba98
= <hellohi12465@gmail.com>
2025-07-16 12:04:54 -0700

    Got a little bit of work done. I am going for an array-style minheap to implement the priority queue, as every data structure is forced to be implemented bare metal, and so an array is a lot nicer to work with and requires less SYS_MMAP.

--- archive/x/x86-64/dijkstra.asm
+++ archive/x/x86-64/dijkstra.asm
@@ -1,7 +1,16 @@
 section .rodata
 
-section .data
+invalid:
+    .msg db 'Usage: please provide three inputs: a serialized matrix, a source node and a destination node'
+    .len equ $- .msg
 
+
+section .data
+    
+minheap:
+    .ptr dq 0
+    .size dd 0
+    .max_size dd 0
 section .bss
 
 section .text
