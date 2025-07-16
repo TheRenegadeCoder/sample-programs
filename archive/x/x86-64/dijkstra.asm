a8d0784e6852ff884c31d65d274f0e9441f542cc
= <hellohi12465@gmail.com>
2025-07-16 14:51:41 -0700

    Added one function that doesn't have code yet, but does have an explanation. Also added some data up top.

--- archive/x/x86-64/dijkstra.asm
+++ archive/x/x86-64/dijkstra.asm
@@ -6,11 +6,15 @@ invalid:
 
 
 section .data
+
+src_dst:
+    .src dq 0
+    .dst dq 0
     
 minheap:
     .ptr dq 0
     .size dd 0
-    .max_size dd 0
+    .max_size dd 0 ;SYS_MMAP will be based on this value.
 section .bss
 
 section .text
@@ -18,4 +22,26 @@ section .text
 global _start
 
 _start:
+    
+    
+    
+    
+parse_SRC_DST:
+; ----------------------------------------------------------------------------
+; Function: parse_SRC_DST
+; Description:
+;   Separated from the vertice parser for organization/readability.
+;   Takes ptr to stack source and destination arguments, addressed through argv[2] and argv[3] respectively.
+;   Addressing argv[1] will cause an error.
+;   Parsed through a finite state machine.
+; Parameters:
+;   RDI - (char*)         Pointer to stack location of src/dst.
+;   RSI - (long*)         Pointer to src/dst storage in .data
+;   RDX - ()              Unused.
+;   R10 - ()              Unused.
+;   R8  - ()              Unused.
+;   R9  - ()              Unused.
+; Returns:
+;   RAX - -1 for invalid input.
+; ---------------------------------------------------------------------------    
 
