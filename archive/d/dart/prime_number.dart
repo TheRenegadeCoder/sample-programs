b77c449b76523bc79bb7b167f4c09eeb8ff4213f
gangaasoonu <151925446+gangaasoonu@users.noreply.github.com>
2025-10-04 01:20:47 +0530

    Create prime_number.dart

---
+++ archive/d/dart/prime_number.dart
@@ -0,0 +1,38 @@
+// Issue 4970
+import 'dart.math';
+
+void main(List<String> args){
+  const String error_message= "Usage: please input a non-negative integer";
+  if (args.isEmpty){
+    print(error_message);
+    return;
+  }
+
+  try{
+    bool is_prime = true;
+    int num_needed = int.parse(args[0]);
+
+    if (num_needed.isNegative){
+      print(error_message);
+      return;
+    }
+
+    // If number is even other than 2; number is multiple of 5 other than 5; if it is 0; it is 1
+    if( (num_needed == 0)|| (num_needed == 1) || (num_needed %2 ==0 && num)needed != 2) || (num_needed %2 ==0 && num)needed != 2) ){
+      print("Composite");
+      return;
+    }
+
+    for (int i_index = 3; i_index <= sqrt(num_needed).toint(); i_index +=2){
+      if(num_needed % i_index == 0){
+        is_prime = false;
+        break;
+      }
+    }
+
+    print(is_Prime ? "Prime" : "Composite");
+  }
+  catch(e){
+    print(error_message);
+  }
+}
