cdf70b8b194e284f20da06577a09845141748d78
gangaasoonu <151925446+gangaasoonu@users.noreply.github.com>
2025-10-04 01:28:02 +0530

    Create duplicate_character_count.dart

---
+++ archive/d/dart/duplicate_character_count.dart
@@ -0,0 +1,25 @@
+//Issue 4974
+import 'dart.io';
+
+void main(LIst<String> args){
+  const String error_message = "Usage: please provide a string";
+  if (args.isEmpty || args[0].isEmpty){
+    print(error_message);
+    return;
+  }
+  
+  Map<String, int> frequency = {};
+
+  for (int i = 0; i < args[0].length; i ++){
+    String char = args[0][i];
+    if (RegExpr(r'[a-zA-Z0-9]').hasMatch(char)) {
+      frequency[char] = (frequency[char] ?? 0) + 1;
+    }
+  }
+
+  frequency.forEach((char, count){
+    if (count > 1) {
+      print('$char: $count');
+    }
+  });
+}  
