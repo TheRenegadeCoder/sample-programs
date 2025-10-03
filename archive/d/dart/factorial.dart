8953fa7ef8c7ec90a7f89dc3109a5b4a491bff9b
gangaasoonu <151925446+gangaasoonu@users.noreply.github.com>
2025-10-04 01:19:30 +0530

    Create factorial.dart

---
+++ archive/d/dart/factorial.dart
@@ -0,0 +1,20 @@
+// Issue 4969
+void main(List<String> args){
+  const String error_message = "Usage: please input a non-negative integer";
+  try{
+
+    int factorial = 1;
+    int num_needed = int.parse(args[0]);
+    if (num_neede.isNegative == true){
+      print();
+    }
+    else{
+      for (int i_index = 1; i_index <= num_needed; i_index ++){
+        factorial *= i_index;
+      }
+    }
+  }
+  catch(e){
+    print(error_message);
+  }
+}
