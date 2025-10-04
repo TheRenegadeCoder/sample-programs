27a2b6eed45381da5ae48b7c6a67a69d4056ea0b
gangaasoonu <151925446+gangaasoonu@users.noreply.github.com>
2025-10-04 11:22:55 +0530

    Update remove-all-whitespace.dart

--- archive/d/dart/remove-all-whitespace.dart
+++ archive/d/dart/remove-all-whitespace.dart
@@ -1,15 +1,15 @@
-//Issue 4973
-import 'dart.io';
-
-void main(LIst<String> args){
+// Issue 4973
+void main(List<String> argv){
   const String error_message = "Usage: please provide a string";
-  if (args.isEmpty || args[0].isEmpty){
+  if (argv.isEmpty || argv[0].isEmpty){
     print(error_message);
     return;
   }
+  String? sentence = argv[0];
 
-  String sentence = argv[0];
-  String sentence_no_spaces = sentence.replaceAll(RegExpr(r'\s+'), "");
-  int max_length= 0;
-  print(max_length);
+  //Convert  \r, ]n and \t explicitly into carriage return, newline and tab
+  String raw = sentence.replaceAll(r'\t', '\t').replaceAll(r'\r', '\r').replaceAll(r'\n', '\n');
+  // Replace carriage return, newline and tab with empty string
+  String sentence_no_spaces = raw.replaceAll(RegExp(r'[\t\r\n ]'), '');
+  print(sentence_no_spaces);
 }
