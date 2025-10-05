f1ed0ce5cf3d888b234df8bad74aa0aac58e5275
gangaasoonu <151925446+gangaasoonu@users.noreply.github.com>
2025-10-05 05:48:58 +0530

    Update remove-all-whitespace.dart
    removed the line, which checks the command line param for additional check

--- archive/d/dart/remove-all-whitespace.dart
+++ archive/d/dart/remove-all-whitespace.dart
@@ -5,10 +5,8 @@ void main(List<String> argv){
     print(error_message);
     return;
   }
-  String? sentence = argv[0];
-
   //Convert  \r, ]n and \t explicitly into carriage return, newline and tab
-  String raw = sentence.replaceAll(r'\t', '\t').replaceAll(r'\r', '\r').replaceAll(r'\n', '\n');
+  String raw = argv[0].replaceAll(r'\t', '\t').replaceAll(r'\r', '\r').replaceAll(r'\n', '\n');
   // Replace carriage return, newline and tab with empty string
   String sentence_no_spaces = raw.replaceAll(RegExp(r'[\t\r\n ]'), '');
   print(sentence_no_spaces);
