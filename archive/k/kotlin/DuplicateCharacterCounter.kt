d5baa3634aec7e8ae17496dc717ac2af4f1ff3ed
Jeremy Grifski <11050412+jrg94@users.noreply.github.com>
2025-10-07 18:30:54 -0400

    Implemented duplicate character counter

---
+++ archive/k/kotlin/DuplicateCharacterCounter.kt
@@ -0,0 +1,30 @@
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    val phrase: String = errorChecking(args)
+    val counts = duplicateCharacterCount(phrase)
+    outputMap(counts)
+}
+
+fun usageError() {
+    println("Usage: please provide a string")
+}
+
+fun errorChecking(args: Array<String>): String {
+    if (args.size == 0 || args[0] == "") {
+       usageError()
+       exitProcess(1)
+    }
+    return args[0]
+}
+
+fun duplicateCharacterCount(phrase: String): Map<Char, Int> {
+    val counts: Map<Char, Int> = phrase.groupingBy { it }.eachCount()
+    return counts.filter { it.value > 1 }
+}
+
+fun outputMap(counts: Map<Char, Int>) {
+    for (pair in counts) {
+        println("${pair.key}: ${pair.value}")
+    }
+}
