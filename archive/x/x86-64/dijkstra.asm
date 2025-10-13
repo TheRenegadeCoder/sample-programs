7438cc825b9955d6f5c5d20757e75ddf2e9a9c02
= <hellohi12465@gmail.com>
2025-10-12 17:02:39 -0700

    Finished getting the functions down I wanted. Next time I should really put down all of the functions/methods associated with the data structures I'll be using. Not having everything down ahead of time caused a lot of confusion.

--- archive/x/x86-64/dijkstra.asm
+++ archive/x/x86-64/dijkstra.asm
@@ -3,11 +3,11 @@ section .rodata
 section .data
 
 struc priority_queue:
-    .virtualTable
-    .nodes
-    .nodePriorities
-    .size
-    .maxSize
+    .virtualTable resq 1
+    .nodes resq 1
+    .nodePriorities resq 1
+    .size resq 1
+    .maxSize resq 1
 endstruc
 
 struc VT_priority_queue:
@@ -27,6 +27,10 @@ global _start
 _start:
 
 
+parseSRCDST:
+
+parseVertices:
+
 dijkstra:
 
 
@@ -39,6 +43,10 @@ priority_queue@isEmpty:
 priority_queue@pop:
 
 priority_queue@decreasePriority:
+
+ezsqrt:
+
+atoi:
 
     
 
