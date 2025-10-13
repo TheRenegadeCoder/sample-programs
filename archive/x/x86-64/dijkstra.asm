564ec2ce0af6148ecbc8445e8c6ad527729fe591
= <hellohi12465@gmail.com>
2025-10-12 17:12:01 -0700

    Took and reworked some of the functions from the original (ezsqrt, atol -> atoi).

--- archive/x/x86-64/dijkstra.asm
+++ archive/x/x86-64/dijkstra.asm
@@ -1,3 +1,16 @@
+%DEFINE atol.STACK_INIT 8
+%DEFINE atol.ret 8
+
+
+
+
+
+
+
+
+
+
+
 section .rodata
 
 section .data
@@ -45,8 +58,77 @@ priority_queue@pop:
 priority_queue@decreasePriority:
 
 ezsqrt:
+; ----------------------------------------------------------------------------
+; Function: Easy Square Root
+; Description:
+;   Checks if number is perfect square; returns square root into RAX.
+; Parameters:
+;   RDI - (long)          Input value to sqrt.
+;   RSI - ()              Unused.
+;   RDX - ()              Unused.
+;   R10 - ()              Unused.
+;   R8  - ()              Unused.
+;   R9  - ()              Unused.
+; Returns:
+;   RAX - (long)          RAX == -1 (Input not square); RAX > 0 (Input IS perfect square).
+;   Clobbers - RCX
+; ---------------------------------------------------------------------------
+    PUSH RBP
+    MOV RBP, RSP
+
+    MOV RDX, -1
+    MOV RCX, 1
+    .sqrt_loop:
+        MOV RAX, RCX
+        MUL RCX
+        CMP RAX, RDI
+        INC RCX
+        CMOVA RAX, RDX
+        JB .sqrt_loop
+    MOV RAX, RCX
+    
+    MOV RSP, RBP
+    POP RBP
+    RET
 
 atoi:
+; ----------------------------------------------------------------------------
+; Function: atoi
+; Description:
+;   Converts ascii string to integer.
+; Parameters:
+;   RDI - (char*)         Pointer to string to convert to integer.
+;   RSI - (long)          Strlen.
+;   RDX - ()              Unused.
+;   R10 - ()              Unused.
+;   R8  - ()              Unused.
+;   R9  - ()              Unused.
+; Returns:
+;   EAX - (int)          Integer value of string.
+; ---------------------------------------------------------------------------
 
+    PUSH RBP
+    MOV RBP, RSP
+    SUB RSP, atoi.STACK_INIT
+    MOV QWORD [RBP - atoi.ret]
+    
+    MOV RBX, 10 ;Multiplier
+    MOV RCX, 0
+    MOV R8B, BYTE [RDI+RCX]
+    .operation:
+        MOV RAX, QWORD [RBP - atoi.ret]
+        MUL RBX
+        INC RCX
+        SUB R8B, '0'
+        ADD RAX, R8
+        MOV QWORD [RBP - atoi.ret], RAX
+        CMP RCX, RSI ;Compare counter to Strlen
+        JNE .operation
+    
+    MOV RAX, QWORD [RBP - atoi.ret]
+    ADD RSP, atoi.STACK_INIT
+    MOV RSP, RBP
+    POP RBP
+    RET
     
 
