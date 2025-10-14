4ac2422a27c44fda03146a13173062cbd6737d45
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2025-10-15 01:50:21 +0300

    Improve Factorial Fortran code snippet

--- archive/f/fortran/factorial.f95
+++ archive/f/fortran/factorial.f95
@@ -1,51 +1,27 @@
-! In program name, - is not allowed
-!works till 77
-! 78 and above is out of the reasonable bounds for calculation
-program factorial_generate
-  character(len=10) :: argument
-  Character(26) :: low = 'abcdefghijklmnopqrstuvwxyz'
-  Character(26) :: cap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-  integer :: number, check_capital_letters, check_small_letters, i
-  integer(kind = 16):: factorial
-  ! Anything not equal to single argument, Print Error
-  IF(COMMAND_ARGUMENT_COUNT().NE.1)THEN
-    write(*,'(g0.8)')"Usage: please input a non-negative integer"
-    STOP
-  ENDIF
-  
-  CALL GET_COMMAND_ARGUMENT(1,argument)
-  if (argument == "") then
-    write(*,'(g0.8)')"Usage: please input a non-negative integer"
-    STOP
-  endif
-  ! Scan for letters
-  check_capital_letters = scan(argument, cap)
-  check_small_letters = scan(argument, low)
-  ! If capital letters exist, print error
-  if (check_capital_letters > 0) then
-    write(*,'(g0.8)')"Usage: please input a non-negative integer"
-    STOP
-  endif
-  ! If small letters exist, print error
-  if (check_small_letters > 0) then
-    write(*,'(g0.8)')"Usage: please input a non-negative integer"
-    STOP
-  endif
-  ! read the cmd line arg into number
-  read (argument, '(I10)') number
+program factorial
+   use, intrinsic :: iso_fortran_env, only: int64
+   implicit none
 
-  if (number < 0) then
-    write(*,'(g0.8)')"Usage: please input a non-negative integer"
-    STOP
-  endif
-  if (number > 77) then
-    write(*,'(g0.8)')"Input is out of the reasonable bounds for calculation"
-    STOP
-  endif
+   character(len=64) :: arg
+   integer :: ios
+   integer(kind=int64) :: n, i
 
-  factorial = 1
-  do i = 1, number
-    factorial = factorial * i
-  end do
-  write(*,'(g0.8)') factorial
-end program
+   if (command_argument_count() /= 1) call usage()
+
+   call get_command_argument(1, arg)
+   arg = adjustl(trim(arg))
+   if (len_trim(arg) == 0) call usage()
+
+   read(arg, *, iostat=ios) n
+   if (ios /= 0 .or. n < 0_int64) call usage()
+
+   write(*,'(i0)') product((/(i, i = 1_int64, n)/))
+
+contains
+
+   subroutine usage()
+      write(*,'(a)') "Usage: please input a non-negative integer"
+      stop
+   end subroutine usage
+
+end program factorial
