8a96f63ba9d2a3f3831553850557f3b749a09a87
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2025-10-15 15:34:25 +0300

    Modify Even Odd in Fortran (#5117)
    Improve Even Odd Fortran code snippet

--- archive/f/fortran/even-odd.f95
+++ archive/f/fortran/even-odd.f95
@@ -1,40 +1,30 @@
-! In program name, - is not allowed
 program evenodd
-character(len=10) :: argument
-Character(26) :: low = 'abcdefghijklmnopqrstuvwxyz'
-Character(26) :: cap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-integer :: number, check_capital_letters, check_small_letters, remainder 
-! Anything not equal to single argument, Print Error
-IF(COMMAND_ARGUMENT_COUNT().NE.1)THEN
-  write(*,'(g0.8)')"Usage: please input a number"
-  STOP
-ENDIF
+   implicit none
+   character(len=12) :: arg
+   character(len=52) :: letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+   integer :: number, ios, argc
 
-CALL GET_COMMAND_ARGUMENT(1,argument)
-if (argument == "") then
-  write(*,'(g0.8)')"Usage: please input a number"
-  STOP
-endif
-! Scan for letters
-check_capital_letters = scan(argument, cap)
-check_small_letters = scan(argument, low)
-! If capital letters exist, print error
-if (check_capital_letters > 0) then
-  write(*,'(g0.8)')"Usage: please input a number"
-  STOP
-endif
-! If small letters exist, print error
-if (check_small_letters > 0) then
-  write(*,'(g0.8)')"Usage: please input a number"
-  STOP
-endif
-! read the cmd line arg into number
-read (argument, '(I10)') number
-! get the remainder
-remainder = modulo(number, 2)
-if (remainder == 0) then
-  write(*,'(g0.8)') "Even"
-else
-  write(*,'(g0.8)') "Odd"
-end if
-end program
+   argc = command_argument_count()
+   if (argc /= 1) call usage()
+
+   call get_command_argument(1, arg)
+   if (len_trim(arg) == 0) call usage()
+   if (scan(arg, letters) > 0) call usage()
+
+   read(arg, *, iostat=ios) number
+   if (ios /= 0) call usage()
+
+   if (mod(number,2) == 0) then
+      write(*,*) "Even"
+   else
+      write(*,*) "Odd"
+   end if
+
+contains
+
+   subroutine usage()
+      write(*,*) "Usage: please input a number"
+      stop
+   end subroutine
+
+end program evenodd
