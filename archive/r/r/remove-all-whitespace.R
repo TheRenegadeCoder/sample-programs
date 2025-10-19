0017c9348297e5593888c7c3a670f0bffd5cce46
manasmithamn <73154097+manasmithamn@users.noreply.github.com>
2025-10-19 10:47:05 +0530

    Update remove-all-whitespace.R
    fixed the program, I was either dozing or hallucinating .. I was typing fibonacci check code under remove all white space, need to get good sleep after 6 commits

--- archive/r/r/remove-all-whitespace.R
+++ archive/r/r/remove-all-whitespace.R
@@ -2,26 +2,9 @@
 args <- commandArgs(trailingOnly = TRUE)
 
 if (length(args) == 0 || args[1] == "" || is.null(args[1]) ){
-  cat("Error: Please provide a positive integer.")
+  cat("Usage: please provide a string")
 }
 
-n <- as.numeric(args[1])
+cleaned_sentence <- gsub("\\s+", "", args[1])
 
-if(is.na(n) || n < 1){
-  cat("Error: Please provide a positive integer.")
-  q(status=1)
-} else if (n == 0){
-  q(status = 1)
-} else {
-  num1 = 0
-  num2 = 1
-  num3 = 0
-  i = 1
-  while (i <= n) {
-    num3 = num1 + num2
-    num1 = num2
-    num2 = num3
-    cat(i, ":", num1,"\n")
-    i = i +1
-  }
-}
+cat (cleaned_sentence)
