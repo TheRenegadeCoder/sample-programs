76b9dc5522492aadfd4ec484b41dc5f5ebdcbc34
Apurva Vats <vatsapurva31@gmail.com>
2025-10-25 22:30:28 +0530

    Usage message for Josephus Problem to match test requirements

--- archive/c/c-plus-plus/josephus-problem.cpp
+++ archive/c/c-plus-plus/josephus-problem.cpp
@@ -1,47 +1,38 @@
 #include <iostream>
+#include <string>
 #include <stdexcept>
-#include <vector>
 using namespace std;
 
-void josephus(int n, int k) {
-    vector<int> people;
-    for (int i = 1; i <= n; ++i) {
-        people.push_back(i);
-    }
-
-    int index = 0;
-    while (people.size() > 1) {
-        cout << "[" << people[0];
-        for (size_t i = 1; i < people.size(); ++i) {
-            cout << ", " << people[i];
-        }
-        cout << "] " << (k - 1) << " " << index << endl;
-        index = (index + k - 1) % people.size();
-        people.erase(people.begin() + index);
-        if (index == people.size()) index = 0;
-    }
-    cout << people[0] << endl;
+int josephus(int n, int k) {
+    if (n == 1)
+        return 1;
+    else
+        return (josephus(n - 1, k) + k - 1) % n + 1;
 }
 
 int main(int argc, char* argv[]) {
+    const string usage_msg = "Usage: please input the total number of people and number of people to skip.\n";
+
     if (argc != 3) {
-        cerr << "Usage: please input the total number of people and number of people to skip.\n";
+        cerr << usage_msg;
         return 1;
     }
+
     try {
         int n = stoi(argv[1]);
         int k = stoi(argv[2]);
+
         if (n <= 0 || k <= 0) {
-            cerr << "Usage: please input the total number of people and number of people to skip.\n";
+            cerr << usage_msg;
             return 1;
         }
-        josephus(n, k);
+
+        int result = josephus(n, k);
+        cout << result << endl;
         return 0;
-    } catch (const invalid_argument&) {
-        cerr << "Usage: please input the total number of people and number of people to skip.\n";
-        return 1;
-    } catch (const out_of_range&) {
-        cerr << "Usage: please input the total number of people and number of people to skip.\n";
+
+    } catch (...) {
+        cerr << usage_msg;
         return 1;
     }
 }
