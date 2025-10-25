6cecbdafd45857276698d4a719a2545e9473e838
Apurva Vats <vatsapurva31@gmail.com>
2025-10-25 20:35:48 +0530

    Fix: Output for Josephus Problem to match test requirements

--- archive/c/c-plus-plus/josephus-problem.cpp
+++ archive/c/c-plus-plus/josephus-problem.cpp
@@ -1,38 +1,47 @@
 #include <iostream>
 #include <stdexcept>
+#include <vector>
 using namespace std;
 
-int josephus(int n, int k) {
-    int res = 0;
+void josephus(int n, int k) {
+    vector<int> people;
     for (int i = 1; i <= n; ++i) {
-        res = (res + k) % i;
+        people.push_back(i);
     }
-    return res + 1;
+
+    int index = 0;
+    while (people.size() > 1) {
+        cout << "[" << people[0];
+        for (size_t i = 1; i < people.size(); ++i) {
+            cout << ", " << people[i];
+        }
+        cout << "] " << (k - 1) << " " << index << endl;
+        index = (index + k - 1) % people.size();
+        people.erase(people.begin() + index);
+        if (index == people.size()) index = 0;
+    }
+    cout << people[0] << endl;
 }
 
 int main(int argc, char* argv[]) {
     if (argc != 3) {
-        cerr << "Usage: <n> <k>" << endl;
+        cerr << "Usage: please input the total number of people and number of people to skip.\n";
         return 1;
     }
-
     try {
         int n = stoi(argv[1]);
         int k = stoi(argv[2]);
-
         if (n <= 0 || k <= 0) {
-            cerr << "Invalid input" << endl;
+            cerr << "Usage: please input the total number of people and number of people to skip.\n";
             return 1;
         }
-
-        cout << josephus(n, k) << endl;
+        josephus(n, k);
         return 0;
-
     } catch (const invalid_argument&) {
-        cerr << "Invalid input" << endl;
+        cerr << "Usage: please input the total number of people and number of people to skip.\n";
         return 1;
     } catch (const out_of_range&) {
-        cerr << "Invalid input" << endl;
+        cerr << "Usage: please input the total number of people and number of people to skip.\n";
         return 1;
     }
 }
