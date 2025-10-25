7d544daa312e4bf63febd89bb570be7302bf4949
Apurva Vats <vatsapurva31@gmail.com>
2025-10-25 18:09:58 +0530

    Add Josephus Problem in C++ (Fixes #3332)

---
+++ archive/c/c-plus-plus/josephus-problem.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int josephus(int n, int k) {
+    int res = 0;
+    for (int i = 1; i <= n; i++)
+        res = (res + k) % i;
+    return res + 1;
+}
+
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        cerr << "Usage: <n> <k>" << endl;
+        return 1;
+    }
+
+    int n = stoi(argv[1]);
+    int k = stoi(argv[2]);
+    if (n <= 0 || k <= 0) {
+        cerr << "Invalid input" << endl;
+        return 1;
+    }
+
+    cout << josephus(n, k) << endl;
+    return 0;
+}
