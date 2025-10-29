18e61a66b4a3dd41190a7f067743483bd53dd90b
konradkelly <konradky@gmail.com>
2025-10-29 22:05:12 +0000

    implement LinearSearch search logic and parse input

--- archive/s/scala/LinearSearch.scala
+++ archive/s/scala/LinearSearch.scala
@@ -4,3 +4,21 @@ object LinearSearch {
             println("Usage: please provide a list of integers (\"1, 4, 5, 11, 12\") and the integer to find (\"11\")")
             return
         }
+        
+        val list = args(0);
+        val key = args(1);
+
+        try {
+            val arr = list.split(",").map(_.trim.toInt)
+            val target = key.trim.toInt
+           
+            var flag = 0
+            var pos = -1
+            for (i <- arr.indices) {
+                if (arr(i) == target) {
+                    flag = 1
+                    pos = i
+                    println("true")
+                    return
+                }
+            }
