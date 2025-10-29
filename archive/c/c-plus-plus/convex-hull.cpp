21927e7819d89d25547395dfad19a4b336183712
Matthew Vargas <matthewvargas2013@gmail.com>
2025-10-29 13:24:07 -0700

    First changes

---
+++ archive/c/c-plus-plus/convex-hull.cpp
@@ -0,0 +1,12 @@
+// Starting out changes, base template courtesy of w3Schools
+#include <iostream>
+using namespace std;
+
+void myFunction() {
+  cout << "I just got executed!";
+}
+
+int main() {
+  myFunction();
+  return 0;
+}
