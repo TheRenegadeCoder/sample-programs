c71e884aecd529b287bf3e1ab4c94ecb9945fee9
Fredrick Karau <30944677+Karau1218@users.noreply.github.com>
2025-10-30 05:18:28 +0000

    still working on the file

--- archive/h/haxe/merge-sort.hx
+++ archive/h/haxe/merge-sort.hx
@@ -1,3 +1,78 @@
-class Main {
-    static function main()
+package;
+class MergeSort {
+    static function main() {
+        var args = sys.args();
+
+        if(args.length != 1) {
+            usage();
+            return;
+        }
+
+
+        var input = args[0];
+
+        var parts = input.split(",");
+        var numbers = [];
+
+        
+        try {
+            for (p in parts) {
+                var trimmed = StringTools.trim(p);
+                if (trimmed == "") throw "Invalid";
+                numbers.push(Std.parseInt(trimmed));
+            }
+        } catch (e.Dynamic) {
+            usage();
+            return;
+        }
+        if(numbers.length < 2) {
+            usage();
+            return;
+        }
+
+        var sorted = mergeSort(numbers);
+        trace(sorted.join(", "));
+    }
+    static function mergeSort(arr:Array<Int>):Array<Int> {
+        if (arr.length <= 1) return arr;
+
+        var mid = Std.int(arr.length / 2);
+        var left = mergeSort(arr.slice(0, mid));
+        var right = mergeSort(arr.slice(mid, arr.length));
+
+        return merge(left, right);
+}
+
+
+    static function merge(left:Array<Int>, right:Array<Int>):Array<Int> {
+        var result:Array<Int> = [];
+        var i = 0;
+        var j = 0;
+
+        while (i < left.length && j < right.length) {
+            if (left[i] <= right[j]) {
+                result.push(left[i]);
+                i++;
+            } else {
+                result.push(right[j]);
+                j++;
+            }
+             }
+
+        while (i < left.length) {
+            result.push(left[i]);
+            i++;
+        }
+
+        while (j < right.length) {
+            result.push(right[j]);
+            j++;
+        }
+
+        return result;
+    }
+
+    static function usage() {
+        trace('Usage: please provide a list of at least two integers to sort in the format "1, 2, 3, 4, 5"');
+    }
 }
