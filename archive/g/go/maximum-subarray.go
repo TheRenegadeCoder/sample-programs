39f464ebc5da8badcf9772498f915062469a5703
Jessica Hebert <90229469+hebert87@users.noreply.github.com>
2025-10-31 01:17:09 +0000

    Add input parsing and Kadane's algorithm setup for Maximum Subarray - Split comma-separated string into integers - Initialize slice for numeric values - Set up maxSum and blockSum for future calculation

--- archive/g/go/maximum-subarray.go
+++ archive/g/go/maximum-subarray.go
@@ -3,13 +3,41 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
 func main() {
-	if len(os.Args) < 1 {
-		fmt.Println("Please enter a list of comma-separated numbers", )
+	if len(os.Args) < 2 {
+		fmt.Println("Please enter a list of comma-separated numbers")
 	}
 	input := os.Args[1]
-	fmt.Println(input)
-	for i := 
+	parts := strings.Split(input, ",")
+	nums := make([]int, len(parts))
+
+	for i, p := range parts {
+		n, _ := strconv.Atoi(p)
+		nums[i] = n
+
+	}
+
+	maxSum := nums[0]
+	blockSum := nums[0]
+
+	for i := 0; i < len(nums); i++ {
+		blockSum = max(nums[i], blockSum+nums[i])
+		if maxSum < blockSum {
+			maxSum = blockSum
+
+		}
+		fmt.Println(blockSum, maxSum)
+	}
+
 }
