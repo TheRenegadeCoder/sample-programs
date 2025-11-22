10f13ecedeefb130dfa6aabfa1dd36c6e6c9c075
rzuckerm <rzdulc@gmail.com>
2025-11-22 08:22:50 -0600

    Use pyyaml to parse testinfo.yml file

--- scripts/build_codeql_language.py
+++ scripts/build_codeql_language.py
@@ -1,10 +1,11 @@
 import argparse
 import re
-import shlex
 import subprocess
 from dataclasses import dataclass
 from pathlib import Path
-from typing import Dict, List
+from typing import Any, Dict, List
+
+import yaml
 
 TEST_INFO_DIR: Dict[str, str] = {
     "c": "c",
@@ -14,7 +15,6 @@ TEST_INFO_DIR: Dict[str, str] = {
     "swift": "swift",
 }
 
-BUILD_PATTERN = re.compile(r"""^\s+build:\s*['"]([^'"]+)""", re.M)
 SOURCE_NAME_PATTERN = re.compile(r"\{\{\s*source\.name\s*\}\}")
 SOURCE_EXTENSION_PATTERN = re.compile(r"\{\{\s*source\.extension\s*\}\}")
 
@@ -23,7 +23,7 @@ SOURCE_EXTENSION_PATTERN = re.compile(r"\{\{\s*source\.extension\s*\}\}")
 class TestInfoStruct:
     path: Path
     language: str
-    test_info_str: str
+    build: str
 
 
 def main():
@@ -36,22 +36,21 @@ def main():
         path = Path(changed_file)
         print(f"Building {path}")
         command = get_build_command(testinfo_struct, path)
-        subprocess.run(command, cwd=path.parent, check=True)
+        subprocess.run(command, cwd=path.parent, check=True, shell=True)
 
 
 def get_test_info_struct(language: str) -> TestInfoStruct:
     path = Path("archive") / language[0] / TEST_INFO_DIR[language] / "testinfo.yml"
-    testinfo_str = path.read_text(encoding="utf-8")
-    return TestInfoStruct(path=path, language=language, test_info_str=testinfo_str)
+    with path.open() as f:
+        testinfo = yaml.safe_load(f)
+
+    return TestInfoStruct(path=path, language=language, build=testinfo["container"]["build"])
 
 
-def get_build_command(testinfo_struct: TestInfoStruct, path: Path) -> List[str]:
-    match_str = BUILD_PATTERN.search(testinfo_struct.test_info_str)
-    assert match_str
-
-    build = SOURCE_NAME_PATTERN.sub(path.stem, match_str.group(1))
+def get_build_command(testinfo_struct: TestInfoStruct, path: Path) -> str:
+    build = SOURCE_NAME_PATTERN.sub(path.stem, testinfo_struct.build)
     build = SOURCE_EXTENSION_PATTERN.sub(path.suffix, build)
-    return shlex.split(build)
+    return build
 
 
 if __name__ == "__main__":
