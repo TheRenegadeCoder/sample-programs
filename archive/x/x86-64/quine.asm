467fb4e23b8a2434a75cdb62fe827e3160fd49a0
= <hellohi12465@gmail.com>
2026-01-05 08:59:19 -0800

    Got the program done; now I just need to fix the stat struct since it isn't pulling the right data for filesize.

---
+++ archive/x/x86-64/quine.asm
@@ -0,0 +1,134 @@
+; SYSCALLS
+%DEFINE SYS_READ 0
+
+%DEFINE SYS_WRITE 1
+%DEFINE STDOUT 1
+
+
+%DEFINE SYS_OPEN 2
+%DEFINE RDONLY 0x0
+%DEFINE NO_MODE 0
+%DEFINE SYS_CLOSE 3
+
+%DEFINE SYS_FSTAT 5
+
+%DEFINE SYS_MMAP 9
+%DEFINE NO_ADDR 0
+%DEFINE NO_FD -1
+%DEFINE NO_OFFSET 0
+;PROTS (RDX)
+%DEFINE PROT_READ 0x01
+%DEFINE PROT_WRITE 0x02
+;FLAGS (R10)
+%DEFINE MAP_SHARED 0x01
+%DEFINE MAP_ANONYMOUS 0x20
+
+%DEFINE SYS_MUNMAP 11
+
+%DEFINE SYS_EXIT 60
+
+; CONSTS
+%DEFINE TERM_STRING 0
+%DEFINE EXIT_OK 0
+
+; STACKS
+
+%DEFINE _start.STACK_INIT 32
+%DEFINE _start.QUINE_FD 8
+%DEFINE _start.FSTAT_PTR 16
+%DEFINE _start.QUINE_BUF 24
+
+section .rodata
+struc stat
+    .st_dev resd 1
+    .st_ino resd 1
+    .st_mode resd 1
+    .st_nlink resd 1
+    .st_uid resd 1
+    .st_gid resd 1
+    .st_rdev resd 1
+    .st_size resd 1
+    .st_atim resq 2
+    .st_mtim resq 2
+    .st_ctim resq 2
+    .st_blksize resd 1
+    .st_stblocks resd 1
+endstruc
+
+filename db 'quine.asm', TERM_STRING
+section .data
+
+section .bss
+
+section .text
+
+global _start
+
+_start:
+PUSH RBP
+MOV RBP, RSP
+SUB RSP, _start.STACK_INIT
+
+MOV RAX, SYS_OPEN
+MOV RDI, filename
+MOV RSI, RDONLY
+MOV RDX, NO_MODE
+SYSCALL
+MOV [RBP - _start.QUINE_FD], RAX
+
+MOV RAX, SYS_MMAP
+MOV RDI, NO_ADDR
+MOV RSI, stat_size
+MOV RDX, PROT_READ | PROT_WRITE
+MOV R10, MAP_SHARED | MAP_ANONYMOUS
+MOV R8, NO_FD
+MOV R9, NO_OFFSET
+SYSCALL
+MOV [RBP - _start.FSTAT_PTR], RAX
+
+MOV RAX, SYS_FSTAT
+MOV RDI, [RBP - _start.QUINE_FD]
+MOV RSI, [RBP - _start.FSTAT_PTR]
+SYSCALL
+
+MOV R12, [RBP - _start.FSTAT_PTR]
+
+MOV RAX, SYS_MMAP
+MOV RDI, NO_ADDR
+MOV ESI, [R12 + stat.st_size]
+MOV RDX, PROT_READ | PROT_WRITE
+MOV R10, MAP_SHARED | MAP_ANONYMOUS
+MOV R8, NO_FD
+MOV R9, NO_OFFSET
+SYSCALL
+MOV [RBP - _start.QUINE_BUF], RAX
+
+MOV RAX, SYS_READ
+MOV RDI, [RBP - _start.QUINE_FD]
+MOV RSI, [RBP - _start.QUINE_BUF]
+MOV EDX, [R12 + stat.st_size]
+SYSCALL
+
+MOV RAX, SYS_WRITE
+MOV RDI, STDOUT
+MOV RSI, [RBP - _start.QUINE_BUF]
+MOV EDX, [R12 + stat.st_size]
+SYSCALL
+
+MOV RAX, SYS_CLOSE
+MOV RDI, [RBP - _start.QUINE_FD]
+SYSCALL
+
+MOV RAX, SYS_MUNMAP
+MOV RDI, [RBP - _start.QUINE_BUF]
+MOV ESI, [R12 + stat.st_size]
+SYSCALL
+
+MOV RAX, SYS_MUNMAP
+MOV RDI, [RBP - _start.FSTAT_PTR]
+MOV RSI, stat_size
+SYSCALL
+
+MOV RAX, SYS_EXIT
+MOV RDI, EXIT_OK
+SYSCALL
