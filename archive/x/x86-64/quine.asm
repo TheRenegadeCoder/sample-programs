51f22ca46d3d2f43640b404fcf7076ee5e0eb0e4
= <hellohi12465@gmail.com>
2026-01-05 09:30:02 -0800

    Working!!!

--- archive/x/x86-64/quine.asm
+++ archive/x/x86-64/quine.asm
@@ -40,19 +40,24 @@
 
 section .rodata
 struc stat
-    .st_dev resd 1
-    .st_ino resd 1
+    .st_dev resq 1
+    .st_ino resq 1
+    .st_nlink resq 1
     .st_mode resd 1
-    .st_nlink resd 1
     .st_uid resd 1
     .st_gid resd 1
-    .st_rdev resd 1
-    .st_size resd 1
-    .st_atim resq 2
-    .st_mtim resq 2
-    .st_ctim resq 2
-    .st_blksize resd 1
-    .st_stblocks resd 1
+    .padding0 resd 1
+    .st_rdev resq 1
+    .st_size resq 1
+    .st_blksize resq 1
+    .st_blocks resq 1
+    .st_atime resq 1
+    .st_atime_nsec resq 1
+    .st_mtim resq 1
+    .st_mtim_nsec resq 1
+    .st_ctim resq 1
+    .st_ctim_nsec resq 1
+    .unused resq 1
 endstruc
 
 filename db 'quine.asm', TERM_STRING
@@ -90,12 +95,11 @@ MOV RAX, SYS_FSTAT
 MOV RDI, [RBP - _start.QUINE_FD]
 MOV RSI, [RBP - _start.FSTAT_PTR]
 SYSCALL
-
 MOV R12, [RBP - _start.FSTAT_PTR]
 
 MOV RAX, SYS_MMAP
 MOV RDI, NO_ADDR
-MOV ESI, [R12 + stat.st_size]
+MOV RSI, [R12 + stat.st_size]
 MOV RDX, PROT_READ | PROT_WRITE
 MOV R10, MAP_SHARED | MAP_ANONYMOUS
 MOV R8, NO_FD
@@ -106,13 +110,13 @@ MOV [RBP - _start.QUINE_BUF], RAX
 MOV RAX, SYS_READ
 MOV RDI, [RBP - _start.QUINE_FD]
 MOV RSI, [RBP - _start.QUINE_BUF]
-MOV EDX, [R12 + stat.st_size]
+MOV RDX, [R12 + stat.st_size]
 SYSCALL
 
 MOV RAX, SYS_WRITE
 MOV RDI, STDOUT
 MOV RSI, [RBP - _start.QUINE_BUF]
-MOV EDX, [R12 + stat.st_size]
+MOV RDX, [R12 + stat.st_size]
 SYSCALL
 
 MOV RAX, SYS_CLOSE
@@ -121,7 +125,7 @@ SYSCALL
 
 MOV RAX, SYS_MUNMAP
 MOV RDI, [RBP - _start.QUINE_BUF]
-MOV ESI, [R12 + stat.st_size]
+MOV RSI, [R12 + stat.st_size]
 SYSCALL
 
 MOV RAX, SYS_MUNMAP
