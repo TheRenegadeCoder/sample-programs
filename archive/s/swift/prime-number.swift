02bef35e4196db309bb2fd4e087db557452c5289
Vidish Kumar <vidishkumar890@gmail.com>
2026-03-09 22:42:05 +0530

    Fix output format to match project requirements

--- archive/s/swift/prime-number.swift
+++ archive/s/swift/prime-number.swift
@@ -1,27 +1,25 @@
 import Foundation
 
-func isPrime(_ n: Int) -> Bool {
-    if n <= 1 {
-        return false
-    }
-
-    for i in 2...Int(Double(n).squareRoot()) {
-        if n % i == 0 {
-            return false
-        }
-    }
-
-    return true
-}
-
 if CommandLine.arguments.count != 2 {
-    print("Usage: please input a number")
-} else if let number = Int(CommandLine.arguments[1]) {
-    if isPrime(number) {
-        print("Prime")
-    } else {
-        print("Composite")
-    }
-} else {
-    print("Invalid input")
+    print("Usage: please input a non-negative integer")
+    exit(0)
 }
+
+guard let number = Int(CommandLine.arguments[1]), number >= 0 else {
+    print("Usage: please input a non-negative integer")
+    exit(0)
+}
+
+if number < 2 {
+    print("composite")
+    exit(0)
+}
+
+for i in 2...Int(Double(number).squareRoot()) {
+    if number % i == 0 {
+        print("composite")
+        exit(0)
+    }
+}
+
+print("prime")
