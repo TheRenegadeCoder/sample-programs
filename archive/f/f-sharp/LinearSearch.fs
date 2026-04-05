36391ba378694bccc64e6206c97f22310920b9c2
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-04-06 01:26:46 +0300

    Add Linear Search in F#

--- archive/f/f-sharp/LinearSearch.fs
+++ archive/f/f-sharp/LinearSearch.fs
@@ -1,45 +1,50 @@
 open System
 
-let (|Int|_|) (s: string) =
-    match Int32.TryParse(s.Trim()) with
-    | true, n -> Some n
-    | _ -> None
-
-let (|IntList|_|) (s: string) =
-    if String.IsNullOrWhiteSpace(s) then
-        None
-    else
-        let parts = s.Split([| ',' |], StringSplitOptions.RemoveEmptyEntries)
-
-        let parsed =
-            parts
-            |> Array.choose (function
-                | Int n -> Some n
-                | _ -> None)
-
-        if parsed.Length = parts.Length && parsed.Length > 0 then
-            Some parsed
-        else
-            None
-
 module LinearSearch =
+    let run (numbers: 'a list, target: 'a) =
+        numbers
+        |> List.exists ((=) target)
+        |> Ok
+
+
+module Helpers =
     let usage =
         "Usage: please provide a list of integers (\"1, 4, 5, 11, 12\") and the integer to find (\"11\")"
 
-    let search (target: 'a) (arr: 'a array) =
-        let rec loop i =
-            if i >= arr.Length then false
-            elif arr.[i] = target then true
-            else loop (i + 1)
+    let private (|Int|_|) (s: string) =
+        match s.Trim() |> Int32.TryParse with
+        | true, n -> Some n
+        | _ -> None
 
-        loop 0
+    let private (|IntList|_|) (s: string) =
+        s.Split(',', StringSplitOptions.RemoveEmptyEntries)
+        |> Array.toList
+        |> List.choose (fun x ->
+            match x.Trim() with
+            | Int n -> Some n
+            | _ -> None)
+        |> function
+            | [] -> None
+            | xs -> Some xs
+
+
+    let parseArgs argv =
+        match argv with
+        | [| IntList numbers; Int target |] -> Ok(numbers, target)
+        | _ -> Error usage
+
+    let handleResult =
+        function
+        | Ok result ->
+            printfn "%b" result
+            0
+        | Error msg ->
+            eprintfn "%s" msg
+            1
 
 [<EntryPoint>]
 let main argv =
-    match argv with
-    | [| IntList numbers; Int target |] ->
-        numbers |> LinearSearch.search target |> string |> _.ToLower() |> printfn "%s"
-        0
-    | _ ->
-        eprintfn "%s" LinearSearch.usage
-        1
+    argv
+    |> Helpers.parseArgs
+    |> Result.bind LinearSearch.run
+    |> Helpers.handleResult
