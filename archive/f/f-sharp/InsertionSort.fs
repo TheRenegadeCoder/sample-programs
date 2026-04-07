228a7229376485962e954a05b4e11734938d6bf8
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-04-08 02:39:37 +0300

    Add Insertion Sort in F# (#5471)

---
+++ archive/f/f-sharp/InsertionSort.fs
@@ -0,0 +1,51 @@
+open System
+
+module InsertionSort =
+    let rec private insert sorted x =
+        match sorted with
+        | [] -> [ x ]                                // Insert at end
+        | head :: tail when x <= head -> x :: sorted // Found spot
+        | head :: tail -> head :: insert tail x      // Keep traversing
+
+    let run numbers =
+        numbers |> List.fold insert [] |> List.map string |> String.concat ", " |> Ok
+
+module Result =
+    let toOption =
+        function
+        | Ok x -> Some x
+        | Error _ -> None
+
+module Helpers =
+    let private (|IntList|_|) (s: string) =
+        let ns =
+            s.Split(',', StringSplitOptions.RemoveEmptyEntries)
+            |> Array.toList
+            |> List.map (fun p ->
+                match Int32.TryParse(p.Trim()) with
+                | true, n -> Ok n
+                | false, _ -> Error $"Invalid integer: '{p}'")
+            |> List.choose Result.toOption
+
+        if ns.Length >= 2 then Some ns else None
+
+    let parseArgs argv =
+        match argv with
+        | [| IntList numbers |] -> Ok numbers
+        | _ -> Error "Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\""
+
+    let handleResult =
+        function
+        | Ok result ->
+            printfn "%s" result
+            0
+        | Error msg ->
+            eprintfn "%s" msg
+            1
+
+[<EntryPoint>]
+let main argv =
+    argv
+    |> Helpers.parseArgs
+    |> Result.bind InsertionSort.run
+    |> Helpers.handleResult
