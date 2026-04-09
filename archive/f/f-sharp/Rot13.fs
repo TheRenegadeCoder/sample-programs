36e46f4a56b182c60e1649f37f349f818596e3e1
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-04-10 01:07:20 +0300

    Add Rot13 in F#

---
+++ archive/f/f-sharp/Rot13.fs
@@ -0,0 +1,31 @@
+open System
+
+module Rot13 =
+    let private rot13Char c =
+        let shift baseLetter =
+            char (((int c - int baseLetter + 13) % 26) + int baseLetter)
+
+        if c >= 'a' && c <= 'z' then shift 'a'
+        elif c >= 'A' && c <= 'Z' then shift 'A'
+        else c
+
+    let run = String.map rot13Char
+
+module Helpers =
+    let parseArgs =
+        function
+        | [| s |] -> Ok s
+        | _ -> Error "Usage: please provide a string to encrypt"
+
+    let handleResult =
+        function
+        | Ok s ->
+            s |> Rot13.run |> printfn "%s"
+            0
+        | Error msg ->
+            printfn "%s" msg
+            1
+
+[<EntryPoint>]
+let main argv =
+    argv |> Helpers.parseArgs |> Helpers.handleResult
