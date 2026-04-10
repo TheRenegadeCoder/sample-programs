a3a4e9f88aa53dd789e2658ed522156aa31bde52
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-04-10 10:13:03 +0300

    Increase time duration to 100ms

--- archive/j/java/SleepSort.java
+++ archive/j/java/SleepSort.java
@@ -37,7 +37,7 @@ public class SleepSort {
 
     private static void sleepUninterruptibly(int value) {
         try {
-            Thread.sleep(Duration.ofMillis(value * 10L));
+            Thread.sleep(Duration.ofMillis(value * 100L));
         } catch (InterruptedException _) {
             Thread.currentThread().interrupt();
         }
