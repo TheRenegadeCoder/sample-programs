7450c4c5a32d6902d19e707799ffbc43c4de0169
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-04-11 05:59:50 +0300

    Update File Input Output to idiomatic Scala 3

--- archive/s/scala/FileInputOutput.scala
+++ archive/s/scala/FileInputOutput.scala
@@ -1,41 +1,30 @@
-import scala.io.Source
-import java.io.{FileNotFoundException, IOException, File, FileOutputStream, PrintWriter}
+import java.nio.charset.StandardCharsets
+import java.nio.file.{Files, Path, Paths}
+import scala.jdk.CollectionConverters.*
+import scala.util.Try
 
-object FileInputOutput {
-  // reading file then write to stdout
-  // write exception when fail
-  def readFromFile(filename: String) {
-    try {
-      val buffer = Source.fromFile(filename)
-      val lines = buffer.getLines
+extension (p: Path)
+  def readLines: Try[List[String]] =
+    Try(Files.readAllLines(p, StandardCharsets.UTF_8).asScala.toList)
 
-      lines.foreach(println)
-      buffer.close
-    } catch {
-      case e: FileNotFoundException => println(s"File ${filename} does not exist.")
-      case e: IOException => println(s"I/O Exception when reading from ${filename}.")
-      case e: Throwable => println(s"Error ${e.getMessage} when reading from ${filename}.")
-    }
-  }
+  def writeString(content: String): Try[Unit] =
+    Try(Files.writeString(p, content, StandardCharsets.UTF_8)).map(_ => ())
 
-  // write to file
-  // stdout exception when fail
-  def writeToFile(filename: String, contents: String) {
-    try {
-      val writer = new PrintWriter(new File(filename))
-      writer.write(contents)
-      writer.close
-    } catch {
-      case e: FileNotFoundException => println(s"Cannot write into file ${filename}.")
-      case e: Throwable => println(s"Error ${e.getMessage} when writing to file ${filename}.")
-    }
-  }
+object FileInputOutput:
+  private val path = Paths.get("output.txt")
+  private val content =
+    """I am a string.
+      |I am also a string.
+      |Scala is fun!""".stripMargin
 
-  def main(args: Array[String]) {
-    // write succesfully
-    writeToFile("output.txt", "I am a string.\nI am also a string.\nScala is fun!\n")
+  @main def run(): Unit =
+    val program =
+      for
+        _ <- path.writeString(content)
+        lines <- path.readLines
+      yield lines
 
-    // read successfully
-    readFromFile("output.txt")
-  }
-}
+    program.fold(
+      err => System.err.println(s"File operation failed: ${err.getMessage}"),
+      res => res.foreach(println)
+    )
