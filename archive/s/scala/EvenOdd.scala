54e0d1f9da463498a994db0dc42fc98d1f0ab9c2
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-04-11 05:24:51 +0300

    Update EvenOdd to Scala 3 and clean it up

--- archive/s/scala/EvenOdd.scala
+++ archive/s/scala/EvenOdd.scala
@@ -1,20 +1,14 @@
-// Scala Program to check if input number is Odd or Even
+import scala.util.Try
 
-import scala.util.{Try, Success, Failure}
+object EvenOdd:
 
-object EvenOdd 
-{
-    def check_even_odd(num: Int): String = { 
-        val result = if (num%2==0) "Even" else "Odd"
-        return result
-    }
+  private val usage = "Usage: please input a number"
 
-    // Driver Code 
-    def main(args: Array[String]) 
-    {
-        Try(args(0).toInt) match {
-            case Failure(_) => println("Usage: please input a number")
-            case Success(m) => println(check_even_odd(m))
-        }
-    }
-} 
+  @main def run(args: String*): Unit =
+    val result =
+      args.headOption
+        .flatMap(a => Try(a.toInt).toOption)
+        .map(n => if n % 2 == 0 then "Even" else "Odd")
+        .getOrElse(usage)
+
+    println(result)
