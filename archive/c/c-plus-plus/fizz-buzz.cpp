65ce12c1b690670e47792ca248b1c3a67a9060e4
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-05 11:39:40 +0300

    Move Fizz Buzz to C++20

--- archive/c/c-plus-plus/fizz-buzz.cpp
+++ archive/c/c-plus-plus/fizz-buzz.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 
-int main()
-{
-    for (int i = 1; i <= 100; i++)
-        if (i % 15 == 0)
+int main() {
+    for (int i = 1; i <= 100; ++i) {
+        const bool fizz = (i % 3 == 0);
+        const bool buzz = (i % 5 == 0);
+
+        if (fizz && buzz) {
             std::cout << "FizzBuzz\n";
-        else if (i % 5 == 0)
-            std::cout << "Buzz\n";
-        else if (i % 3 == 0)
+        } else if (fizz) {
             std::cout << "Fizz\n";
-        else
-            std::cout << i << "\n";
+        } else if (buzz) {
+            std::cout << "Buzz\n";
+        } else {
+            std::cout << i << '\n';
+        }
+    }
+    
     return 0;
 }
