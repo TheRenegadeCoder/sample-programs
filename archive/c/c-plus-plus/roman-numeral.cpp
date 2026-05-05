181d1c99d9ef392f8e2d79bcf5466a69fe4ae668
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-05 12:50:19 +0300

    Move Roman Numeral to C++20

--- archive/c/c-plus-plus/roman-numeral.cpp
+++ archive/c/c-plus-plus/roman-numeral.cpp
@@ -1,62 +1,70 @@
-#include <bits/stdc++.h>
+#include <algorithm>
 #include <iostream>
+#include <ranges>
+#include <string_view>
 
-using namespace std;
-
-bool is_invalid_roman(char x)
-{
-    return !(x == 'I'
-             || x == 'V'
-             || x == 'X'
-             || x == 'L'
-             || x == 'C'
-             || x == 'D'
-             || x == 'M');
+[[noreturn]] void usage() {
+    std::cerr << "Usage: please provide a string of roman numerals\n";
+    std::exit(1);
 }
 
-int main(int argc, char *argv[])
-{
-    if (argv[1] == NULL)
-    {
-        cerr << "Usage: please provide a string of roman numerals" << endl;
-        exit(0);
+[[noreturn]] void error() {
+    std::cerr << "Error: invalid string of roman numerals\n";
+    std::exit(1);
+}
+
+constexpr int value(char c) {
+    switch (c) {
+        case 'I':
+            return 1;
+        case 'V':
+            return 5;
+        case 'X':
+            return 10;
+        case 'L':
+            return 50;
+        case 'C':
+            return 100;
+        case 'D':
+            return 500;
+        case 'M':
+            return 1000;
+        default:
+            return -1;
+    }
+}
+
+constexpr bool is_valid(char c) { return value(c) != -1; }
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) usage();
+
+    std::string_view s = argv[1];
+
+    if (s.empty()) {
+        std::cout << 0 << '\n';
+        return 0;
     }
 
-    string s;
-    s = argv[1];
+    if (!std::ranges::all_of(s, is_valid)) error();
 
-    for (char c : s)
-    {
-        if (is_invalid_roman(c))
-        {
-            cerr << "Error: invalid string of roman numerals" << endl;
-            exit(0);
+    int result = 0;
+
+    for (std::size_t i = 0; i < s.size(); ++i) {
+        int curr = value(s[i]);
+
+        if (i + 1 < s.size()) {
+            int next = value(s[i + 1]);
+
+            if (curr < next) {
+                result += next - curr;
+                ++i;
+                continue;
+            }
         }
+
+        result += curr;
     }
 
-    map<char, int> value;
-    value['I'] = 1;
-    value['V'] = 5;
-    value['X'] = 10;
-    value['L'] = 50;
-    value['C'] = 100;
-    value['D'] = 500;
-    value['M'] = 1000;
-
-    int num = 0;
-
-    for (int i = s.size() - 1; i >= 0; i--)
-    {
-        if (i > 0 && value[s[i]] > value[s[i - 1]])
-        {
-            num += value[s[i]] - value[s[i - 1]];
-            i--;
-        }
-        else
-        {
-            num += value[s[i]];
-        }
-    }
-
-    cout << num << endl;
+    std::cout << result << '\n';
 }
