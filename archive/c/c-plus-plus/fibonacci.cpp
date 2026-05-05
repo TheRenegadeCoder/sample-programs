cc97b3b0c3ef406f553e1cc19357d1d692f8c520
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-05 11:38:52 +0300

    Move Fibonacci to C++20

--- archive/c/c-plus-plus/fibonacci.cpp
+++ archive/c/c-plus-plus/fibonacci.cpp
@@ -1,35 +1,59 @@
-#include <cstdlib>
+#include <charconv>
+#include <format>
 #include <iostream>
-using namespace std;
+#include <optional>
+#include <ranges>
+#include <string_view>
 
-int main(int argc, char *argv[])
-{
-    if (argc < 2 || std::string(argv[1]) == "")
-    {
-        cout
-            << "Usage: please input the count of fibonacci numbers to output"
-            << endl;
-        return 1;
-    }
+namespace views = std::views;
 
-    int n = atoi(argv[1]);
-    if (n == 0 && std::string(argv[1]) != "0")
-    {
-        cout
-            << "Usage: please input the count of fibonacci numbers to output"
-            << endl;
-        return 1;
-    }
-    int first = 0;
-    int second = 1;
-    int result = 0;
+[[noreturn]] void usage() {
+    std::cerr
+        << "Usage: please input the count of fibonacci numbers to output\n";
+    std::exit(1);
+}
 
-    for (int i = 1; i <= n; ++i)
-    {
-        result = first + second;
-        first = second;
-        second = result;
-        cout << i << ": " << first << endl;
+static constexpr std::string_view ws = " \t\n\r\f\v";
+constexpr std::string_view trim(std::string_view s) {
+    const auto start = s.find_first_not_of(ws);
+    if (start == std::string_view::npos) return "";
+    s.remove_prefix(start);
+
+    const auto end = s.find_last_not_of(ws);
+    s.remove_suffix(s.size() - 1 - end);
+    return s;
+}
+
+std::optional<int> to_int(std::string_view s) {
+    s = trim(s);
+    int value{};
+    auto [ptr, ec] = std::from_chars(s.data(), s.data() + s.size(), value);
+    return (ec == std::errc{} && ptr == s.data() + s.size())
+               ? std::make_optional(value)
+               : std::nullopt;
+}
+
+auto fibonacci_sequence() {
+    return views::iota(0) | views::transform([a = 1LL, b = 1LL](auto) mutable {
+               auto out = a;
+               auto next = a + b;
+               a = b;
+               b = next;
+               return out;
+           });
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) usage();
+
+    if (auto n_opt = to_int(argv[1]); !n_opt || *n_opt < 0) {
+        usage();
+    } else {
+        const int n = *n_opt;
+
+        int index = 1;
+        for (const auto value : fibonacci_sequence() | views::take(n)) {
+            std::cout << std::format("{}: {}\n", index++, value);
+        }
     }
-    return 0;
 }
