565d688a7ea49e2b8fe3698b789d3a711d247c75
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-07 18:17:12 +0300

    Migrate Palindromic Number to Ruby 4

--- archive/r/ruby/palindromic-number.rb
+++ archive/r/ruby/palindromic-number.rb
@@ -1,32 +1,9 @@
-# print true, if input is a non-negative palindrome number
-if ARGV.empty?
-  puts "Usage: please input a non-negative integer"
-  exit
-else
-  begin
-    string1 = ARGV[0]
-    num = Integer(string1)
-    rescue ArgumentError
-    puts "Usage: please input a non-negative integer"
-    exit
-  end
-  if num < 0
-    puts "Usage: please input a non-negative integer"
-    exit    
-  end
-# end validations for empty input, non-number and negative number
-  reverse_no = 0
-  save = num 
-  while (save > 0)
-
-      last_digit = save % 10 
-      reverse_no = (reverse_no *10) + last_digit
-      save /=10     
-  end
-  if num == reverse_no
-      print("true")
-  else
-      print("false")
-  end
-
+def usage!
+  abort("Usage: please input a non-negative integer")
 end
+
+number = Integer(ARGV.first, exception: false)
+
+usage! if number.nil? || number.negative?
+
+puts number.digits == number.digits.reverse
