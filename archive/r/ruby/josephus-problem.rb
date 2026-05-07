36b4359d42e55a925e106ac53632ac61c1a70084
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-07 17:37:14 +0300

    Migrate Josephus Problem to Ruby 4

--- archive/r/ruby/josephus-problem.rb
+++ archive/r/ruby/josephus-problem.rb
@@ -1,32 +1,17 @@
-def josephus_problem(array, k, start)
-    if (array.length == 1)
-      # if the array is one item left, return the survivor value
-      puts array[0]
-    else
-        # set the index position (start + (k - 1)) and set wrap with %
-        index = (start + k - 1)
-        wrap = index % array.length
-        array.delete_at(wrap)
-        return josephus_problem(array, k, wrap)
-    end
+def josephus(n, k)
+  (1..n).reduce(0) { |acc, i| (acc + k) % i } + 1
 end
 
-def error()
-    puts "Usage: please input the total number of people and number of people to skip."
+def usage!
+  abort("Usage: please input the total number of people and number of people to skip.")
 end
 
-# check for incorrect number of arguments
-# .any? loops over the elements of ARGV to check if the argument contains non-digit characters or empty strings
-if ARGV.length != 2 || ARGV.any? { |arg| arg.match(/[^0-9]/) || arg.empty? }
-    error()
-else
-    # convert the arguments to integers
-    n = ARGV[0].to_i
-    k = ARGV[1].to_i
+n, k = ARGV
 
-    # create an array with a length of argument n
-    array = [*1..n]
+usage! unless n && k
+usage! unless n.match?(/\A\d+\z/) && k.match?(/\A\d+\z/)
 
-    # pass our array to the function with k and our start index (0)
-    josephus_problem(array, k, 0)
-end
+n = n.to_i
+k = k.to_i
+
+puts josephus(n, k)
