1422243e383763c9dceb0474680c4e10f0bc0ae7
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-10 19:08:17 +0300

    Add Factorial in Ada (#5871)
    * Add Fibonacci in Ada
    
    * Isolate exception block to Natural'Value

---
+++ archive/a/ada/factorial.adb
@@ -0,0 +1,48 @@
+with Ada.Text_IO;      use Ada.Text_IO;
+with Ada.Command_Line; use Ada.Command_Line;
+
+procedure Factorial is
+
+   procedure Print_Usage is
+   begin
+      Put_Line ("Usage: please input a non-negative integer");
+   end Print_Usage;
+
+   function Fact (N : Natural) return Natural is
+      Result : Natural := 1;
+   begin
+      for I in 2 .. N loop
+         Result := Result * I;
+      end loop;
+      return Result;
+   end Fact;
+
+   Max_Allowed : constant Natural := 12; -- Fact (13) > Natural'Last
+
+begin
+   if Argument_Count /= 1 then
+      Print_Usage;
+      return;
+   end if;
+
+   declare
+      N : Natural;
+   begin
+      begin
+         N := Natural'Value (Argument (1));
+      exception
+         when Constraint_Error =>
+            Print_Usage;
+            return;
+      end;
+
+      if N > Max_Allowed then
+         Print_Usage;
+         return;
+      end if;
+
+      Put_Line (Natural'Image (Fact (N)));
+
+   end;
+
+end Factorial;
