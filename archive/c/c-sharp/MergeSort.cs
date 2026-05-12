99e588d78e5aabd57d963fb0175e79fcbef84c17
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-12 22:51:33 +0300

    Migrate Merge Sort to C# 14

--- archive/c/c-sharp/MergeSort.cs
+++ archive/c/c-sharp/MergeSort.cs
@@ -1,62 +1,119 @@
-using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Buffers;
 
-public class MergeSort
+if (args is not [var input] || !TryParseList(input.AsSpan(), out var numbers))
+    return ExitWithUsage();
+
+Span<int> span = CollectionsMarshal.AsSpan(numbers);
+MergeSort(span);
+
+Console.WriteLine(string.Join(", ", numbers));
+return 0;
+
+static bool TryParseList(ReadOnlySpan<char> view, out List<int> numbers)
 {
-    public static List<int> Sort(List<int> xs) => Sort(xs.Select(x => new List<int>() {x}).ToList()).First();
-    public static List<List<int>> Sort(List<List<int>> xs)
+    numbers = null!;
+    if (view.IsWhiteSpace())
+        return false;
+
+    int expectedCount = view.Count(',') + 1;
+    var list = new List<int>(expectedCount);
+
+    while (!view.IsEmpty)
     {
-        if (xs.Count <= 1)
-            return xs;
-        var x0 = xs[0];
-        var x1 = xs[1];
-        xs.RemoveAt(0);
-        xs.RemoveAt(0);
-        return Sort(new List<List<int>>()
-        {
-            Merge(x0, x1)
-        }.Concat(Sort(xs).ToList()).ToList());
+        if (!TryParseNext(ref view, out int val))
+            return false;
+
+        list.Add(val);
     }
 
-    public static List<int> Merge(List<int> xs, List<int> ys)
+    if (list.Count < 2)
+        return false;
+
+    numbers = list;
+    return true;
+
+    static bool TryParseNext(ref ReadOnlySpan<char> span, out int value)
     {
-        if (!xs.Any())
-            return ys;
-        if (!ys.Any())
-            return xs;
-        if (xs[0] < ys[0])
+        int comma = span.IndexOf(',');
+
+        ReadOnlySpan<char> token;
+        if (comma >= 0)
         {
-            var x0 = xs[0];
-            xs.RemoveAt(0);
-            return new List<int>() {x0}.Concat(Merge(xs, ys)).ToList();
+            token = span[..comma];
+            span = span[(comma + 1)..];
         }
-        var y0 = ys[0];
-        ys.RemoveAt(0);
-        return new List<int>() {y0}.Concat(Merge(xs, ys)).ToList();
+        else
+        {
+            token = span;
+            span = default;
+        }
+
+        return int.TryParse(token, out value);
+    }
+}
+
+// Bottom-up merge sort
+static void MergeSort(Span<int> span)
+{
+    int n = span.Length;
+    if (n <= 1)
+        return;
+
+    int[] buffer = new int[n];
+
+    Span<int> src = span;
+    Span<int> dst = buffer;
+
+    for (int width = 1; width < n; width *= 2)
+    {
+        for (int left = 0; left < n; left += width * 2)
+        {
+            int mid = Math.Min(left + width, n);
+            int right = Math.Min(left + width * 2, n);
+
+            Merge(
+                src[left..mid],
+                src[mid..right],
+                dst[left..right]
+            );
+        }
+
+        Span<int> temp = src;
+        src = dst;
+        dst = temp;
     }
 
-    public static void ErrorAndExit()
+    // if final data ended up in buffer, copy back
+    if (!src.Overlaps(span))
+        src.CopyTo(span);
+}
+
+static void Merge(
+    ReadOnlySpan<int> left,
+    ReadOnlySpan<int> right,
+    Span<int> target)
+{
+    int li = 0;
+    int ri = 0;
+    int ti = 0;
+
+    while (li < left.Length && ri < right.Length)
     {
-        Console.WriteLine("Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\"");
-        Environment.Exit(1);
+        target[ti++] = left[li] <= right[ri]
+            ? left[li++]
+            : right[ri++];
     }
 
-    public static void Main(string[] args)
-    {
-        if (args.Length != 1)
-            ErrorAndExit();
-        try
-        {
-            var xs = args[0].Split(',').Select(i => Int32.Parse(i.Trim())).ToList();
-            if (xs.Count() <= 1)
-                ErrorAndExit();
-            var sortedXs = Sort(xs);
-            Console.WriteLine(string.Join(", ", sortedXs));
-        }
-        catch
-        {
-            ErrorAndExit();
-        }
-    }
+    left[li..].CopyTo(target[ti..]);
+    right[ri..].CopyTo(target[ti..]);
+}
+
+static int ExitWithUsage()
+{
+    Console.WriteLine(
+        "Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\""
+    );
+    return 1;
 }
