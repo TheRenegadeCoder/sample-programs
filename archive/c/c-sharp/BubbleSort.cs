2f32d9c793a81abf59770c2575235a3f29142272
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-13 01:10:44 +0300

    Shorten Bubble Sort

--- archive/c/c-sharp/BubbleSort.cs
+++ archive/c/c-sharp/BubbleSort.cs
@@ -1,62 +1,35 @@
-using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
 if (args is not [var input] || !TryParseList(input.AsSpan(), out var numbers))
-    return ExitWithUsage();
+    return Usage();
 
 BubbleSort(numbers);
 
 Console.WriteLine(string.Join(", ", numbers));
 return 0;
 
-static bool TryParseList(ReadOnlySpan<char> view, out List<int> numbers)
+static bool TryParseList(ReadOnlySpan<char> span, out List<int> numbers)
 {
-    numbers = null!;
-    if (view.IsWhiteSpace())
-        return false;
+    numbers = new(span.Count(',') + 1);
 
-    int expectedCount = view.Count(',') + 1;
-    var list = new List<int>(expectedCount);
-
-    while (!view.IsEmpty)
-    {
-        if (!TryParseNext(ref view, out int val))
-            return false;
-
-        list.Add(val);
-    }
-
-    if (list.Count < 2)
-        return false;
-
-    numbers = list;
-    return true;
-
-    static bool TryParseNext(ref ReadOnlySpan<char> span, out int value)
+    while (!span.IsEmpty)
     {
         int comma = span.IndexOf(',');
+        var token = comma >= 0 ? span[..comma] : span;
 
-        ReadOnlySpan<char> token;
-        if (comma >= 0)
-        {
-            token = span[..comma];
-            span = span[(comma + 1)..];
-        }
-        else
-        {
-            token = span;
-            span = default;
-        }
+        span = comma >= 0 ? span[(comma + 1)..] : [];
 
-        return int.TryParse(token, out value);
+        if (!int.TryParse(token, out int n))
+            return false;
+
+        numbers.Add(n);
     }
+
+    return numbers.Count > 1;
 }
 
 static void BubbleSort(List<int> list)
 {
-    if (list.Count < 2) 
-        return;
-
     Span<int> span = CollectionsMarshal.AsSpan(list);
     int n = span.Length;
 
@@ -66,22 +39,24 @@ static void BubbleSort(List<int> list)
 
         for (int j = 0; j < n - i - 1; j++)
         {
-            if (span[j] > span[j + 1])
-            {
-                (span[j], span[j + 1]) = (span[j + 1], span[j]);
-                swapped = true;
-            }
+            if (span[j] <= span[j + 1])
+                continue;
+
+            (span[j], span[j + 1]) = (span[j + 1], span[j]);
+            swapped = true;
         }
 
         if (!swapped)
-            break;
+            return;
     }
 }
 
-static int ExitWithUsage()
+
+static int Usage()
 {
-    Console.WriteLine(
-        "Usage: please provide a list of at least two integers to sort in the format \"1, 2, 3, 4, 5\""
+    Console.Error.WriteLine("""
+Usage: please provide a list of at least two integers to sort in the format "1, 2, 3, 4, 5"
+"""
     );
     return 1;
 }
