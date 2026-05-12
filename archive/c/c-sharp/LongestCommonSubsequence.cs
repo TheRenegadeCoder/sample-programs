78ed3b98ad791ad0564bc72aae26da7000979c85
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-12 21:38:43 +0300

    Migrate Longest Common Subsequence to C# 14

--- archive/c/c-sharp/LongestCommonSubsequence.cs
+++ archive/c/c-sharp/LongestCommonSubsequence.cs
@@ -1,39 +1,110 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
 
-namespace SamplePrograms
+if (args is not [var raw1, var raw2] ||
+    !TryParseList(raw1.AsSpan(), out var a) ||
+    !TryParseList(raw2.AsSpan(), out var b))
 {
-    public class LongestCommonSubsequence
+    Console.WriteLine("""
+Usage: please provide two lists in the format "1, 2, 3, 4, 5"
+""");
+    return;
+}
+
+var lcs = BuildLCS(a, b);
+
+Console.WriteLine(string.Join(", ", lcs));
+
+static List<int> BuildLCS(List<int> a, List<int> b)
+{
+    int n = a.Count;
+    int m = b.Count;
+
+    if (n == 0 || m == 0)
+        return [];
+
+    int[][] dp = new int[n + 1][];
+    for (int i = 0; i <= n; i++)
+        dp[i] = new int[m + 1];
+
+    // Build DP
+    for (int i = 1; i <= n; i++)
     {
-        private static IEnumerable<string> LCS(IEnumerable<string> list1, IEnumerable<string> list2)
+        int ai = a[i - 1];
+        var prevRow = dp[i - 1];
+        var currRow = dp[i];
+
+        for (int j = 1; j <= m; j++)
         {
-            if (list1.Count() == 0 || list2.Count() == 0)
-                return new List<string>();
+            if (ai == b[j - 1])
+                currRow[j] = prevRow[j - 1] + 1;
+            else
+                currRow[j] = Math.Max(prevRow[j], currRow[j - 1]);
+        }
+    }
 
-            if (list1.First().Equals(list2.First()))
-                return LCS(list1.Skip(1), list2.Skip(1)).Concat(new List<string>() { list1.First() });
+    var result = new List<int>(dp[n][m]);
 
-            return Longest(LCS(list1, list2.Skip(1)), LCS(list1.Skip(1), list2));
+    int x = n, y = m;
+
+    while (x > 0 && y > 0)
+    {
+        if (a[x - 1] == b[y - 1])
+        {
+            result.Add(a[x - 1]);
+            x--;
+            y--;
+        }
+        else if (dp[x - 1][y] >= dp[x][y - 1])
+        {
+            x--;
+        }
+        else
+        {
+            y--;
+        }
+    }
+
+    result.Reverse();
+    return result;
+}
+
+static bool TryParseList(ReadOnlySpan<char> view, out List<int> numbers)
+{
+    numbers = null!;
+
+    if (view.IsWhiteSpace())
+        return false;
+
+    int expected = view.Count(',') + 1;
+    var list = new List<int>(expected);
+
+    while (!view.IsEmpty)
+    {
+        if (!TryParseNext(ref view, out int val))
+            return false;
+
+        list.Add(val);
+    }
+
+    numbers = list;
+    return list.Count > 0;
+
+    static bool TryParseNext(ref ReadOnlySpan<char> span, out int value)
+    {
+        int comma = span.IndexOf(',');
+
+        ReadOnlySpan<char> token;
+        if (comma >= 0)
+        {
+            token = span[..comma];
+            span = span[(comma + 1)..];
+        }
+        else
+        {
+            token = span;
+            span = default;
         }
 
-        private static IEnumerable<string> Longest(params IEnumerable<string>[] lists) =>
-            lists.OrderByDescending(l => l.Count()).First();
-
-        public static void Main(string[] args)
-        {
-            try
-            {
-                var list1 = args[0].Split(',').Select(i => i.Trim());
-                var list2 = args[1].Split(',').Select(i => i.Trim());
-                var lcs = LCS(list1, list2).Reverse();
-                Console.WriteLine(string.Join(", ", lcs));
-            }
-            catch
-            {
-                Console.WriteLine("Usage: please provide two lists in the format \"1, 2, 3, 4, 5\"");
-                Environment.Exit(1);
-            }
-        }
+        return int.TryParse(token, out value);
     }
 }
