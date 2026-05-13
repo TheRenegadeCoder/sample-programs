c7b6db65e2614868d70b87ac5378d45b0afeccf6
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-13 17:16:35 +0300

    Fix Maximum Array Rotation

--- archive/c/c-sharp/MaximumArrayRotation.cs
+++ archive/c/c-sharp/MaximumArrayRotation.cs
@@ -21,7 +21,7 @@ static bool TryParseList(ReadOnlySpan<char> span, out List<int> numbers)
         numbers.Add(n);
     }
 
-    return true;
+    return numbers.Count > 0;
 }
 
 static int MaximumRotationSum(List<int> numbers)
