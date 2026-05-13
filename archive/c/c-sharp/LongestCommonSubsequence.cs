ae29c3360b959a3babc1fbeb066c5666a46103b4
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-13 16:08:56 +0300

    Shorten Longest Common Subsequence

--- archive/c/c-sharp/LongestCommonSubsequence.cs
+++ archive/c/c-sharp/LongestCommonSubsequence.cs
@@ -1,110 +1,76 @@
 using System.Collections.Generic;
 
-if (args is not [var raw1, var raw2] ||
-    !TryParseList(raw1.AsSpan(), out var a) ||
-    !TryParseList(raw2.AsSpan(), out var b))
+if (
+    args is not [var raw1, var raw2]
+    || !TryParseList(raw1.AsSpan(), out var a)
+    || !TryParseList(raw2.AsSpan(), out var b)
+)
 {
-    Console.WriteLine("""
+    Console.WriteLine(
+        """
 Usage: please provide two lists in the format "1, 2, 3, 4, 5"
-""");
+"""
+    );
     return;
 }
 
-var lcs = BuildLCS(a, b);
+Console.WriteLine(string.Join(", ", LCS(a, b)));
 
-Console.WriteLine(string.Join(", ", lcs));
-
-static List<int> BuildLCS(List<int> a, List<int> b)
+static bool TryParseList(ReadOnlySpan<char> span, out List<int> numbers)
 {
-    int n = a.Count;
-    int m = b.Count;
+    numbers = new(span.Count(',') + 1);
 
-    if (n == 0 || m == 0)
-        return [];
-
-    int[][] dp = new int[n + 1][];
-    for (int i = 0; i <= n; i++)
-        dp[i] = new int[m + 1];
-
-    // Build DP
-    for (int i = 1; i <= n; i++)
+    while (!span.IsEmpty)
     {
-        int ai = a[i - 1];
-        var prevRow = dp[i - 1];
-        var currRow = dp[i];
+        int comma = span.IndexOf(',');
+        var token = comma >= 0 ? span[..comma] : span;
 
-        for (int j = 1; j <= m; j++)
-        {
-            if (ai == b[j - 1])
-                currRow[j] = prevRow[j - 1] + 1;
-            else
-                currRow[j] = Math.Max(prevRow[j], currRow[j - 1]);
-        }
+        span = comma >= 0 ? span[(comma + 1)..] : [];
+
+        if (!int.TryParse(token, out int n))
+            return false;
+
+        numbers.Add(n);
     }
 
-    var result = new List<int>(dp[n][m]);
+    return true;
+}
 
-    int x = n, y = m;
+static List<int> LCS(List<int> a, List<int> b)
+{
+    int n = a.Count, m = b.Count;
+    if (n == 0 || m == 0) return [];
 
-    while (x > 0 && y > 0)
+    int[,] dp = new int[n + 1, m + 1];
+
+    for (int i = 1; i <= n; i++)
+    for (int j = 1; j <= m; j++)
+        dp[i, j] = a[i - 1] == b[j - 1]
+            ? dp[i - 1, j - 1] + 1
+            : Math.Max(dp[i - 1, j], dp[i, j - 1]);
+
+    var result = new List<int>(dp[n, m]);
+
+    int i2 = n, j2 = m;
+
+    while (i2 > 0 && j2 > 0)
     {
-        if (a[x - 1] == b[y - 1])
+        if (a[i2 - 1] == b[j2 - 1])
         {
-            result.Add(a[x - 1]);
-            x--;
-            y--;
+            result.Add(a[i2 - 1]);
+            i2--;
+            j2--;
         }
-        else if (dp[x - 1][y] >= dp[x][y - 1])
+        else if (dp[i2 - 1, j2] >= dp[i2, j2 - 1])
         {
-            x--;
+            i2--;
         }
         else
         {
-            y--;
+            j2--;
         }
     }
 
     result.Reverse();
     return result;
-}
-
-static bool TryParseList(ReadOnlySpan<char> view, out List<int> numbers)
-{
-    numbers = null!;
-
-    if (view.IsWhiteSpace())
-        return false;
-
-    int expected = view.Count(',') + 1;
-    var list = new List<int>(expected);
-
-    while (!view.IsEmpty)
-    {
-        if (!TryParseNext(ref view, out int val))
-            return false;
-
-        list.Add(val);
-    }
-
-    numbers = list;
-    return list.Count > 0;
-
-    static bool TryParseNext(ref ReadOnlySpan<char> span, out int value)
-    {
-        int comma = span.IndexOf(',');
-
-        ReadOnlySpan<char> token;
-        if (comma >= 0)
-        {
-            token = span[..comma];
-            span = span[(comma + 1)..];
-        }
-        else
-        {
-            token = span;
-            span = default;
-        }
-
-        return int.TryParse(token, out value);
-    }
 }
