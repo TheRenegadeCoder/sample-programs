38533bbeec9b3a5405a15ff15de2ccad810570fb
Ștefan-Iulian Alecu <165364995+pascalecu@users.noreply.github.com>
2026-05-14 14:18:53 +0300

    Migrate Prime Number to Perl 5.42

--- archive/p/perl/prime-number.pl
+++ archive/p/perl/prime-number.pl
@@ -1,65 +1,22 @@
-# Requirement https://sample-programs.therenegadecoder.com/projects/prime-number/
-# Issue  #1834
-# Accept a number on command line and print if it is Prime or Composite
-# Prime Numbers will have only 1 Divisor, itself..  Use that to determine Composite.
-# Note: 0 and 1 are Composite numbers, 2 is a Prime Number
-use warnings;
+#!/usr/bin/env perl
+use v5.42;
 
-my ($prime) = @ARGV;
-
-$num_args = $#ARGV + 1;
-
-# Empty input
-if ( $num_args < 1 ) {
-    print("Usage: please input a non-negative integer");
-    exit(0);
+sub usage {
+    say "Usage: please input a non-negative integer";
+    exit;
 }
 
-# Only Integer
-if ( $prime =~ /^-?\d+$/ ) {
+my ($n) = @ARGV;
+usage() unless defined $n && $n =~ /\A\d+\z/;
 
-    # Negative Number
-    if ( $prime < 0 ) {
-        print("Usage: please input a non-negative integer");
-        exit(0);
-    }
+$n += 0;
 
-    if ( $prime == 2 ) {
-        print("Prime");
-        exit(0);
-    }
+say "Composite" and exit if $n < 2;
+say "Prime"     and exit if $n == 2;
+say "Composite" and exit if $n % 2 == 0;
 
-    # If 1 or the Number is Even
-    elsif ( ( $prime == 1 ) || ( $prime == 0 ) || ( $prime % 2 == 0 ) ) {
-        print("Composite");
-        exit(0);
-    }
-
-    else {
-        #   Check how many divisors for the given number
-        $i               = 0;
-        $num_of_divisors = 0;
-
-        #   Number is guaranteed to be Even
-        for ( $i = $prime ; $i > 1 ; $i = $i - 2 ) {
-            if ( $prime % $i == 0 ) {
-                $num_of_divisors += 1;
-            }
-        }
-
-        # If more than 2 divisors
-        if ( $num_of_divisors > 2 ) {
-            print("Composite");
-        }
-        else {
-            print("Prime");
-        }
-    }
-
+for ( my $i = 3 ; $i * $i <= $n ; $i += 2 ) {
+    say "Composite" and exit if $n % $i == 0;
 }
 
-# If not Integer
-else {
-    print "Usage: please input a non-negative integer";
-    exit(0);
-}
+say "Prime";
