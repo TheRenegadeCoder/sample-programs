3b166ea3f25dcfac3fe1b431e9e11cf8c29bf0c3
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-05-16 17:51:41 -0700

    Refactored argument parsing and reformatted ocaml factorial

--- archive/o/ocaml/factorial.ml
+++ archive/o/ocaml/factorial.ml
@@ -1,15 +1,15 @@
-let factorial n = 
+let factorial n =
   (* Use accumulator so OCaml can do tail call recursion *)
-  let rec helper acc n = 
-    match n with
-    | 0 | 1 -> acc
-    | n -> helper (acc*n) (n-1)
+  let rec helper acc n =
+    match n with 0 | 1 -> acc | n -> helper (acc * n) (n - 1)
   in
   helper 1 n
 
-let () = print_endline (
-  let nth_arg = List.nth_opt (Array.to_list Sys.argv) 1 in
-  match Option.bind nth_arg int_of_string_opt with
-  | Some num when num >= 0 -> num |> factorial |> string_of_int
-  | _ -> "Usage: please input a non-negative integer"
-)
+let parse_args argv =
+  match argv with [| _; n |] -> int_of_string_opt n | _ -> None
+
+let () =
+  print_endline
+    (match parse_args Sys.argv with
+    | Some num when num >= 0 -> num |> factorial |> string_of_int
+    | _ -> "Usage: please input a non-negative integer")
