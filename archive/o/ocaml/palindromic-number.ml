1363f14af90744fe14a889739209f4d6015c9ecd
alope107 <alope107@users.noreply.github.com>
2026-05-17 15:16:28 -0700

    Add Palindromic Number in OCaml (#5970)
    * Add Palindromic Number in OCaml
    
    * refactor ocaml palindromic number to eliminate unneeded match
    
    * added tailcall check in ocaml palindromic number to ensure desired optimization is met
    
    ---------
    
    Co-authored-by: Auberon Lopez <auberon.lopez@greenriver.edu>

---
+++ archive/o/ocaml/palindromic-number.ml
@@ -0,0 +1,23 @@
+let ( let* ) = Option.bind
+
+let palindrome str =
+  let rec aux lo hi =
+    lo >= hi || (str.[lo] = str.[hi] && (aux [@tailcall]) (lo + 1) (hi - 1))
+  in
+  aux 0 (String.length str - 1)
+
+let validate_arg = function
+  | [| _; n |] ->
+      let* num = int_of_string_opt n in
+      if num >= 0 then
+        (* Converting back and forth normalizes the number 
+           - allows for checking things like "+323" based on the actual numerical value *)
+        (* Problem statement is underspecified, so going to follow Postel's law and be permissive :) *)
+        Some (string_of_int num)
+      else None
+  | _ -> None
+
+let () =
+  match validate_arg Sys.argv with
+  | Some n -> Printf.printf "%b\n" (palindrome n)
+  | None -> print_endline "Usage: please input a non-negative integer"
