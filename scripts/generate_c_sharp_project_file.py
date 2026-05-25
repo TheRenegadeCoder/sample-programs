2987117641db10383e8c248240108c02d30432b4
rzuckerm <rzuckerm@users.noreply.github.com>
2026-05-25 16:45:21 -0500

    Cleanup GitHub workflows (#6012)

---
+++ scripts/generate_c_sharp_project_file.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+
+import yaml
+
+C_SHARP_ARCHIVE_DIR = Path("archive/c/c-sharp")
+C_SHARP_PROJECT_CONTENTS = """\
+<Project Sdk="Microsoft.NET.Sdk">
+  <PropertyGroup>
+    <OutputType>Library</OutputType>
+      <TargetFramework>net{version}</TargetFramework>
+      <ImplicitUsings>enable</ImplicitUsings>
+      <Nullable>enable</Nullable>
+      <Deterministic>true</Deterministic>
+  </PropertyGroup>
+
+  <ItemGroup>
+    <!-- Exclude all .cs files; the build process handles compilation individually -->
+    <Compile Remove="**/*.cs" />
+  </ItemGroup>
+</Project>
+"""
+
+
+
+def main():
+    testinfo = yaml.safe_load((C_SHARP_ARCHIVE_DIR / "testinfo.yml").read_text(encoding="utf-8"))
+    tag = testinfo["container"]["tag"]
+    project_contents = C_SHARP_PROJECT_CONTENTS.format(version=tag)
+    (C_SHARP_ARCHIVE_DIR / "MyProj.csproj").write_text(project_contents, encoding="utf-8")
+
+
+if __name__ == "__main__":
+    main()
