c2bd9d4e4b921bb35d78443b0ebcf6429e8e218d
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-05-29 11:50:57 -0700

    OCaml Zeckendorf: refactor out nested conditional

--- archive/o/ocaml/zeckendorf.ml
+++ archive/o/ocaml/zeckendorf.ml
@@ -7,11 +7,9 @@ let max_2_fib_up_to n =
 let zeckendorf n =
   let rec aux high_f low_f rem acc =
     if rem = 0 then List.rev acc
-    else
-      let new_rem, new_acc =
-        if high_f <= rem then (rem - high_f, high_f :: acc) else (rem, acc)
-      in
-      aux low_f (high_f - low_f) new_rem new_acc
+    else if high_f <= rem then
+      aux low_f (high_f - low_f) (rem - high_f) (high_f :: acc)
+    else aux low_f (high_f - low_f) rem acc
   in
   let high_f, low_f = max_2_fib_up_to n in
   aux high_f low_f n []
@@ -22,7 +20,8 @@ let parse_args argv =
   match argv with [| _; n |] -> int_of_string_opt n | _ -> None
 
 let () =
-  print_endline @@
+  print_endline
+  @@
   match parse_args Sys.argv with
   | Some num when num >= 0 -> num_list_str (zeckendorf num)
   | _ -> "Usage: please input a non-negative integer"
