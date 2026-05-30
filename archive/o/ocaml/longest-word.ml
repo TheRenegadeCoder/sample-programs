56a8cb5f7d969ad77ddbde7afa637f019dae4c25
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-05-29 19:27:47 -0700

    OCaml longest word: refactor tuple extraction and names for clarity

--- archive/o/ocaml/longest-word.ml
+++ archive/o/ocaml/longest-word.ml
@@ -3,18 +3,21 @@
    purposes of this problem *)
 let is_whitespace = function ' ' | '\t' | '\n' | '\r' -> true | _ -> false
 
-let longest_word s =
-  s |> String.to_seq
-  |> Seq.fold_left
-       (fun (best, curr) c ->
-         if is_whitespace c then (best, 0)
-         else (Int.max best (curr + 1), curr + 1))
-       (0, 0)
-  |> fst
+let longest_word_len s =
+  let best, _ =
+    String.fold_left
+      (fun (best, curr) c ->
+        if is_whitespace c then (best, 0)
+        else
+          let curr = curr + 1 in
+          (Int.max best curr, curr))
+      (0, 0) s
+  in
+  best
 
 let () =
   print_endline
   @@
   match Sys.argv with
-  | [| _; s |] when s <> "" -> string_of_int (longest_word s)
+  | [| _; s |] when s <> "" -> string_of_int (longest_word_len s)
   | _ -> "Usage: please provide a string"
