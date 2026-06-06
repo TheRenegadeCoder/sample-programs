29e4193936b259cc45616ef861ad1f3342ebde46
Auberon Lopez <auberon.lopez@greenriver.edu>
2026-06-05 19:06:22 -0700

    Ocaml Frac: Switch to modules

--- archive/o/ocaml/fraction-math.ml
+++ archive/o/ocaml/fraction-math.ml
@@ -1,64 +1,75 @@
 let ( let* ) = Option.bind
 
-type rat = { num : int; denom : int }
+module Rational : sig
+  type rat
 
-let gcd a b =
-  let a = abs a in
-  let b = abs b in
-  if b = 0 then a
-  else
-    let rec aux a b =
-      let r = a mod b in
-      if r = 0 then b else aux b r
-    in
-    aux a b
+  val make : int -> int -> rat
+  val string_of_rat : rat -> string
+  val rat_of_string_opt : string -> rat option
+  val recip : rat -> rat
+  val mult : rat -> rat -> rat
+  val div : rat -> rat -> rat
+  val add : rat -> rat -> rat
+  val sub : rat -> rat -> rat
+  val eq : rat -> rat -> bool
+  val neq : rat -> rat -> bool
+  val gt : rat -> rat -> bool
+  val lt : rat -> rat -> bool
+  val gte : rat -> rat -> bool
+  val lte : rat -> rat -> bool
+end = struct
+  type rat = { num : int; denom : int }
 
-let simplify { num; denom } =
-  let div = gcd num denom in
-  let sign_flip = if denom < 0 then -1 else 1 in
-  { num = num / div * sign_flip; denom = denom / div * sign_flip }
+  let gcd a b =
+    let a = abs a in
+    let b = abs b in
+    if b = 0 then a
+    else
+      let rec aux a b =
+        let r = a mod b in
+        if r = 0 then b else aux b r
+      in
+      aux a b
 
-let to_string { num; denom } = string_of_int num ^ "/" ^ string_of_int denom
-let recip { num; denom } = { num = denom; denom = num }
-let mult a b = simplify { num = a.num * b.num; denom = a.denom * b.denom }
-let div a b = mult a (recip b)
+  let make num denom =
+    if denom = 0 then raise Division_by_zero
+    else
+      let divisor = gcd num denom in
+      let sign_flip = if denom < 0 then -1 else 1 in
+      { num = num / divisor * sign_flip; denom = denom / divisor * sign_flip }
 
-let add a b =
-  simplify
-    { num = (a.num * b.denom) + (b.num * a.denom); denom = a.denom * b.denom }
+  let string_of_rat { num; denom } =
+    string_of_int num ^ "/" ^ string_of_int denom
 
-let sub a b = add a { num = -b.num; denom = b.denom }
+  let recip { num; denom } = make denom num
+  let mult a b = make (a.num * b.num) (a.denom * b.denom)
+  let div a b = mult a (recip b)
+  let add a b = make ((a.num * b.denom) + (b.num * a.denom)) (a.denom * b.denom)
+  let sub a b = add a (make (-b.num) b.denom)
+  let eq a b = a = b
+  let neq a b = not (eq a b)
+  let gt a b = a.num * b.denom > b.num * a.denom
+  let gte a b = gt a b || eq a b
+  let lt a b = not (gte a b)
+  let lte a b = lt a b || eq a b
 
-let common_denom a b =
-  let denom = a.denom * b.denom in
-  ({ num = a.num * b.denom; denom }, { num = b.num * a.denom; denom })
-
-let eq a b = simplify a = simplify b
-let neq a b = not (eq a b)
-
-let gt a b =
-  let { num = a_num; _ }, { num = b_num; _ } =
-    common_denom (simplify a) (simplify b)
-  in
-  a_num > b_num
-
-let gte a b = gt a b || eq a b
-let lt a b = not (gte a b)
-let lte a b = lt a b || eq a b
-
-let parse_frac s =
-  match String.split_on_char '/' s with
-  | [ num; denom ] ->
-      let* num_int = int_of_string_opt num in
-      let* denom_int = int_of_string_opt denom in
-      if denom_int <> 0 then Some { num = num_int; denom = denom_int } else None
-  | _ -> None
+  let rat_of_string_opt s =
+    match String.split_on_char '/' s with
+    | [ num; denom ] ->
+        let* num_int = int_of_string_opt num in
+        let* denom_int = int_of_string_opt denom in
+        if denom_int <> 0 then Some (make num_int denom_int) else None
+    | _ -> None
+end
 
 module StringMap = Map.Make (String)
 
-type frac_op = Arith of (rat -> rat -> rat) | Bool of (rat -> rat -> bool)
+type frac_op =
+  | Arith of (Rational.rat -> Rational.rat -> Rational.rat)
+  | Bool of (Rational.rat -> Rational.rat -> bool)
 
 let ops =
+  let open Rational in
   StringMap.of_list
     [
       ("*", Arith mult);
@@ -75,14 +86,14 @@ let ops =
 
 let exec_exp a op b =
   match op with
-  | Arith f -> f a b |> to_string
+  | Arith f -> f a b |> Rational.string_of_rat
   | Bool f -> if f a b then "1" else "0"
 
 let parse_args = function
   | [| _; a; op_s; b |] ->
       let* op_f = StringMap.find_opt op_s ops in
-      let* a_rat = parse_frac a in
-      let* b_rat = parse_frac b in
+      let* a_rat = Rational.rat_of_string_opt a in
+      let* b_rat = Rational.rat_of_string_opt b in
       Some (a_rat, op_f, b_rat)
   | _ -> None
 
